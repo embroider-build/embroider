2f28f1379c8845d6453fb9620c343ee08f970a3a
Edward Faulkner <ef@alum.mit.edu>
2018-07-18 14:01:46 -0400

    start building up complete picture of deps

---
+++ notes.md
@@ -0,0 +1,21 @@
+
+can we construct a shadow node_modules to handle the v1-to-v2 compiled addons or should we do in-place replacement of them?
+
+shadow is annoying to make work in both directions (from shadow to real and real to shadow). It's not too bad when packages are same-level in the filesystem (you can override the real copy by placing symlinks into the nested node_modules of each of the packages that depend on it). But when you're already nested, there's no higher-priority place to put the symlink. Probalby in that case you need to copy the dependee into shadow too.
+
+two-way shadow involves editing inside of the real node_modules, so maybe it's not worth trying to avoid in-place replacement.
+
+we can make it only go one way if we copy a lot more stuff into shadow (anything that depends on a v1-to-v2 compiled package). That's unfortunate because a single v1 addon imposes the cost on everyone.
+
+in-place replacement seems intrusive, but more understandable and performant. Can do atomic replacement, depth-first.
+
+but in-place works best as a one-shot thing you do after npm install, not so good if we are doing incremental rebuilds of legacy addons with dynamic behaviors that haven't been ported to v2. Perhaps we can make it work well by having a well-known place to keep the original, and some explicit relationship between the original and the v2 such that we will notice and rebuild when NPM runs again and undoes our work.
+
+tentative conclusion: go with in-place replacement.
+  1. Not every up-compiled package will need rebuilds. If it doesn't implement any custom hooks, we know it doesn't. So a one-shot atomic replacement is sufficient. The atomic replacment should be all packages at once: we will build everything separately first.
+  2. That is the initial case I'm working with anyway, so go with that.
+  3. Later we can add a dynamically rebuilding strategy that keeps the original v1 around in a nested `.ember-cli-vanilla-v1` folder (nested so it retains access to the same node_modules).
+
+NEXT: keep working on the build step, without worrying about inter-module resolution yet, knowing that we will do an atomic replace step at the end.
+
+
--- ts/index.ts
+++ ts/index.ts
@@ -1,6 +1,7 @@
 import mergeTrees from 'broccoli-merge-trees';
 import Funnel from 'broccoli-funnel';
 import { UnwatchedDir } from 'broccoli-source';
+import { Tree } from 'broccoli-plugin';
 import { existsSync } from 'fs';
 import { join, dirname } from 'path';
 import RewritePackageJSON from './rewrite-package-json';
@@ -19,11 +20,93 @@ import { sync as pkgUpSync }  from 'pkg-up';
 //   'treeForVendor',
 // ]);
 
+// Represents the set of active V2 packages that make up a complete application. Addons
+// that publish as v2 can merely be discovered, addons that publish as v1 get
+// up-compiled. The host application itself also gets compiled into v2 format on
+// the fly, so that we can present a complete set of v2 packages as the public
+// API forward to our bundling phase.
+export default class Packages {
+  private builtPackages: Map<string, Tree> = new Map();
+
+  constructor(project) {
+    // TODO: we need to follow all deps, not just active ones. You can still
+    // directly import things out of non-active packages, because we follow
+    // node_modules resolution rules and those rules don't care about our notion
+    // of active.
+    project.addons.forEach(addonInstance => this.addPackage(addonInstance));
+  }
+
+  private addPackage(addonInstance) {
+    // TODO: check for native v2 and go down a different path
+
+    if (this.builtPackages.has(addonInstance.root)) {
+      // TODO: the same addon may be used by multiple different packages, and
+      // for a v1 package each consumer may cause it to have different build
+      // output, so we could have conflicting needs here. (This doesn't come up
+      // for v2 packages, their contents are constant by design, dynamicism is
+      // handled elsewhere in the build process.)
+    } else {
+      this.builtPackages.set(addonInstance.root, this.upcompileV1Package(addonInstance));
+      addonInstance.addons.forEach(a => this.addPackage(a));
+    }
+  }
+
+  upcompileV1Package(addonInstance) {
+    let trees = [];
+
+    // addonInstance.root gets modified by a customized "main" or
+    // "ember-addon.main" in package.json. We want the real package root here (the
+    // place where package.json lives).
+    let root = dirname(pkgUpSync(addonInstance.root));
+
+    let rootTree = new UnwatchedDir(root);
+    trees.push(new RewritePackageJSON(rootTree));
+
+    let mainModule = require(addonInstance.constructor._meta_.modulePath);
+
+    if (customizes(mainModule, 'treeForAddon', 'treeForAddonTemplates')) {
+      console.log(`TODO: ${addonInstance.name} may have customized the addon tree`);
+    } else {
+      if (existsSync(join(root, 'addon'))) {
+        // TODO: set main in package.json to index.js
+        // and synthesize an index.js if there isn't one
+        trees.push(
+          transpile(addonInstance, new Funnel(rootTree, {
+            srcDir: 'addon'
+          }))
+        );
+      }
+    }
+
+    if (customizes(mainModule, 'treeForAddonTestSupport')) {
+      console.log(`TODO: ${addonInstance.name} may have customized the addon test support tree`);
+    } else {
+      if (existsSync(join(root, 'addon-test-support'))) {
+        trees.push(
+          transpile(addonInstance, new Funnel(rootTree, {
+            srcDir: 'addon-test-support',
+            destDir: 'test-support'
+          }))
+      );
+      }
+    }
+
+    return new Funnel(mergeTrees(trees), {
+      destDir: addonInstance.pkg.name
+    });
+  }
+
+  // TODO: This is a placeholder for development purposes only.
+  dumpTrees() {
+    return [...this.builtPackages.values()].map((tree, index) => new Funnel(tree, { destDir: `out-${index}` });
+  }
+}
+
 function customizes(mainModule, ...treeNames) {
   return mainModule.treeFor || treeNames.find(treeName => mainModule[treeName]);
 }
 
-function transpile(addonInstance, tree) {
+function transpile(_addonInstance, tree) {
   // TODO: for Javascript, this should respect the addon's configured babel
   // plugins but only target ES latest, leaving everything else (especially
   // modules) intact. For templates, this should apply custom AST transforms and
@@ -35,47 +118,3 @@ function transpile(addonInstance, tree) {
   return tree;
 }
 
-export function prebuildV1Package(addonInstance) {
-  let trees = [];
-
-  // addonInstance.root gets modified by a customized "main" or
-  // "ember-addon.main" in package.json. We want the real package root here (the
-  // place where package.json lives).
-  let root = dirname(pkgUpSync(addonInstance.root));
-
-  let rootTree = new UnwatchedDir(root);
-  trees.push(new RewritePackageJSON(rootTree));
-
-  let mainModule = require(addonInstance.constructor._meta_.modulePath);
-
-  if (customizes(mainModule, 'treeForAddon', 'treeForAddonTemplates')) {
-    console.log(`TODO: ${addonInstance.name} may have customized the addon tree`);
-  } else {
-    if (existsSync(join(root, 'addon'))) {
-      // TODO: set main in package.json to index.js
-      // and synthesize an index.js if there isn't one
-      trees.push(
-        transpile(addonInstance, new Funnel(rootTree, {
-          srcDir: 'addon'
-        }))
-      );
-    }
-  }
-
-  if (customizes(mainModule, 'treeForAddonTestSupport')) {
-    console.log(`TODO: ${addonInstance.name} may have customized the addon test support tree`);
-  } else {
-    if (existsSync(join(root, 'addon-test-support'))) {
-      trees.push(
-        transpile(addonInstance, new Funnel(rootTree, {
-          srcDir: 'addon-test-support',
-          destDir: 'test-support'
-        }))
-    );
-    }
-  }
-
-  return new Funnel(mergeTrees(trees), {
-    destDir: addonInstance.pkg.name
-  });
-}

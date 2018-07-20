c124c0f55d5d446f96766db7911b9d29b5815bb6
Edward Faulkner <ef@alum.mit.edu>
2018-07-20 17:55:11 -0400

    more build

--- ts/index.ts
+++ ts/index.ts
@@ -13,11 +13,11 @@ import { sync as pkgUpSync }  from 'pkg-up';
 //   'treeForAddonTemplates',
 //   'treeForAddonTestSupport',
 //   'treeForApp',
-//   'treeForPublic',
-//   'treeForStyles',
+//   'treeForPublic', // TODO
+//   'treeForStyles', // TODO
 //   'treeForTemplates',
-//   'treeForTestSupport',
-//   'treeForVendor',
+//   'treeForTestSupport', // TODO
+//   'treeForVendor', // TODO
 // ]);
 
 // Represents the set of active V2 packages that make up a complete application. Addons
@@ -45,6 +45,7 @@ export default class Packages {
       // output, so we could have conflicting needs here. (This doesn't come up
       // for v2 packages, their contents are constant by design, dynamicism is
       // handled elsewhere in the build process.)
+      console.log(`TODO: multiple instances of same copy of addon ${addonInstance.name}`);
     } else {
       this.builtPackages.set(addonInstance.root, this.upcompileV1Package(addonInstance));
       addonInstance.addons.forEach(a => this.addPackage(a));
@@ -52,17 +53,29 @@ export default class Packages {
   }
 
   upcompileV1Package(addonInstance) {
+    let trees = this.v2Trees(addonInstance);
+    return new Funnel(mergeTrees(trees), {
+      destDir: addonInstance.pkg.name
+    });
+  }
+
+  private v2Trees(addonInstance) {
     let trees = [];
 
     // addonInstance.root gets modified by a customized "main" or
-    // "ember-addon.main" in package.json. We want the real package root here (the
-    // place where package.json lives).
+    // "ember-addon.main" in package.json. We want the real package root here
+    // (the place where package.json lives).
     let root = dirname(pkgUpSync(addonInstance.root));
 
     let rootTree = new UnwatchedDir(root);
     trees.push(new RewritePackageJSON(rootTree));
 
     let mainModule = require(addonInstance.constructor._meta_.modulePath);
+
+    if (customizes(mainModule, 'treeFor')) {
+      console.log(`TODO: ${addonInstance.name} has customized treeFor`);
+      return trees;
+    }
 
     if (customizes(mainModule, 'treeForAddon', 'treeForAddonTemplates')) {
       console.log(`TODO: ${addonInstance.name} may have customized the addon tree`);
@@ -91,9 +104,20 @@ export default class Packages {
       }
     }
 
-    return new Funnel(mergeTrees(trees), {
-      destDir: addonInstance.pkg.name
-    });
+    if (customizes(mainModule, 'treeForApp', 'treeForTemplates')) {
+      console.log(`TODO: ${addonInstance.name} may have customized the app tree`);
+    } else {
+      if (existsSync(join(root, 'app'))) {
+        trees.push(
+          transpile(addonInstance, new Funnel(rootTree, {
+            srcDir: 'app',
+            destDir: '_app_'
+          }))
+        );
+      }
+    }
+
+    return trees;
   }
 
   // TODO: This is a placeholder for development purposes only.
@@ -103,7 +127,7 @@ export default class Packages {
 }
 
 function customizes(mainModule, ...treeNames) {
-  return mainModule.treeFor || treeNames.find(treeName => mainModule[treeName]);
+  return treeNames.find(treeName => mainModule[treeName]);
 }
 
 function transpile(_addonInstance, tree) {

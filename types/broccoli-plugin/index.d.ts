f1ed687996cace7777f6eb93dfd2d8eb2c926443
Edward Faulkner <ef@alum.mit.edu>
2018-08-31 10:08:21 -0400

    better typings for broccoli-plugin

--- types/broccoli-plugin/index.d.ts
+++ types/broccoli-plugin/index.d.ts
@@ -2,10 +2,19 @@ declare module 'broccoli-plugin' {
 
   export interface Tree {}
 
-  export default class Plugin {
-    constructor(inputTrees: Tree[], options: any)
+  export interface Options {
+    name?: string;
+    annotation?: string;
+    persistentOutput?: boolean;
+    needsCache?: boolean;
+  }
+
+
+  export default abstract class Plugin {
+    constructor(inputTrees: Tree[], options: Options)
     inputPaths: string[];
-    outputPath: string
+    outputPath: string;
+    abstract build();
   }
 
 }

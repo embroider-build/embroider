4929b1218b85a55bb1610d239eda27bff554ab51
Edward Faulkner <ef@alum.mit.edu>
2018-09-11 13:48:52 -0400

    starting script tag support

--- ts/ember-webpack.ts
+++ ts/ember-webpack.ts
@@ -1,10 +1,24 @@
 import { Packager } from "ember-cli-vanilla";
 import webpack from 'webpack';
-import { readFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
 import { join, basename } from 'path';
 import { JSDOM } from 'jsdom';
 import isEqual from 'lodash/isEqual';
 import mergeWith from 'lodash/mergeWith';
+
+interface Entrypoint {
+  name: string;
+  scripts: string[];
+  scriptTags: HTMLScriptElement[];
+  dom: any;
+  filename: string;
+}
+
+interface AppInfo {
+  entrypoints: Entrypoint[];
+  externals: string[];
+  templateCompiler: Function;
+}
 
 class Webpack {
   constructor(
@@ -15,25 +29,33 @@ class Webpack {
     ) {
   }
 
-  private examineApp() {
+  private examineApp(): AppInfo {
     let packageJSON = JSON.parse(readFileSync(join(this.pathToVanillaApp, 'package.json'), 'utf8'));
     let entrypoints = packageJSON['ember-addon'].entrypoints.map(entrypoint => {
-      let document = new JSDOM(readFileSync(join(this.pathToVanillaApp, entrypoint), 'utf8')).window.document;
-      let scripts = [...document.querySelectorAll('script')].filter(s => {
+      let dom = new JSDOM(readFileSync(join(this.pathToVanillaApp, entrypoint), 'utf8'));
+      let document = dom.window.document;
+      let scriptTags = [...document.querySelectorAll('script')].filter(s => {
         return !s.hasAttribute('async') && !isAbsoluteURL(s.src);
-      }).map(s => s.src.replace(/^\//, this.pathToVanillaApp + '/'));
-      return { name: entrypoint, scripts };
+      });
+      let scripts = scriptTags.map(s => s.src.replace(/^\//, this.pathToVanillaApp + '/'));
+      return {
+        name: basename(entrypoint, '.html'),
+        scripts,
+        scriptTags,
+        dom,
+        filename: entrypoint
+      };
     });
     let externals = packageJSON['ember-addon'].externals;
     let templateCompiler = require(join(this.pathToVanillaApp, packageJSON['ember-addon']['template-compiler']));
     return { entrypoints, externals, templateCompiler };
   }
 
-  private configureWebpack({ entrypoints, externals, templateCompiler }) {
+  private configureWebpack({ entrypoints, externals, templateCompiler }: AppInfo) {
 
     let entry = {};
     entrypoints.forEach(entrypoint => {
-      entry[basename(entrypoint.name, '.html')] = entrypoint.scripts.slice();
+      entry[entrypoint.name] = entrypoint.scripts.slice();
     });
 
     let amdExternals = {};
@@ -59,7 +81,7 @@ class Webpack {
         ]
       },
       output: {
-        path: this.outputPath,
+        path: join(this.outputPath, 'assets'),
         filename: `chunk.[chunkhash].js`,
         chunkFilename: `chunk.[chunkhash].js`,
       },
@@ -87,8 +109,45 @@ class Webpack {
     let appInfo = this.examineApp();
     let config = this.configureWebpack(appInfo);
     let webpack = this.getWebpack(config);
-    await this.runWebpack(webpack);
+    let stats = this.summarizeStats(await this.runWebpack(webpack));
+    this.writeHTML(stats, appInfo);
   }
+
+  private writeHTML(stats, { entrypoints }: AppInfo) {
+    // we're doing this ourselves because I haven't seen a webpack 4 HTML plugin
+    // that handles multiple entrypoints correctly.
+    for (let entrypoint of entrypoints) {
+      let assets = stats.entrypoints.get(entrypoint.name);
+      let firstTag = entrypoint.scriptTags[0];
+      for (let asset of assets) {
+        let newScript = entrypoint.dom.window.document.createElement('script');
+        newScript.src = `/assets/${asset}`; // todo adjust for rootURL
+        firstTag.parentElement.insertBefore(newScript, firstTag);
+      }
+      entrypoint.scriptTags.forEach(tag => tag.remove());
+      writeFileSync(join(this.outputPath, entrypoint.filename), entrypoint.dom.serialize(), 'utf8');
+    }
+  }
+
+  private summarizeStats(stats) {
+    let output = {
+      entrypoints: new Map(),
+      lazyAssets: [],
+    };
+    let nonLazyAssets = new Set();
+    for (let id of Object.keys(stats.entrypoints)) {
+      let entrypoint = stats.entrypoints[id];
+      output.entrypoints.set(id, entrypoint.assets);
+      entrypoint.assets.forEach(asset => nonLazyAssets.add(asset));
+    }
+    for (let asset of stats.assets) {
+      if (!nonLazyAssets.has(asset.name)) {
+        output.lazyAssets.push(asset.name);
+      }
+    }
+    return output;
+  }
+
   private runWebpack(webpack): Promise<any> {
     return new Promise((resolve, reject) => {
       webpack.run((err, stats) => {

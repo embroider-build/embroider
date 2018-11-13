200b65af143581b8daea355961f8af163f1c9520
Edward Faulkner <ef@alum.mit.edu>
2018-11-13 13:39:16 -0500

    documentation

--- packages/core/src/app.ts
+++ packages/core/src/app.ts
@@ -1,6 +1,11 @@
 import { Tree } from "broccoli-plugin";
 
 export default interface App {
+  // this is the broccoli tree that must get built for the app to be ready. But!
+  // This tree's output path is _not_ necessarily where the final app will be,
+  // for that you must look at `root`.
   readonly tree: Tree;
+
+  // This is the actual directory in which the app will be.
   readonly root: string;
 }

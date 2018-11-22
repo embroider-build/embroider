e0989f7b6be0a24ef65594ac54b151667443f31c
Edward Faulkner <ef@alum.mit.edu>
2018-11-21 23:06:17 -0500

    adding a comment

--- packages/compat/src/compat-adapters/ember-percy.ts
+++ packages/compat/src/compat-adapters/ember-percy.ts
@@ -4,8 +4,11 @@ import { forceIncludeModule } from "../compat-utils";
 export default class extends V1Addon {
   get packageMeta() {
     let meta = super.packageMeta;
+
+    // these get invoked from an inline script tag in content-for('test-body-footer')
     meta = forceIncludeModule(meta, 'native-xhr');
     meta = forceIncludeModule(meta, 'finalize');
+
     return meta;
   }
 }

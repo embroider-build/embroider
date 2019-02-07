afedcc73d08606e779e3152ca22cbb1994d77e22
Edward Faulkner <ef@alum.mit.edu>
2019-02-07 16:58:49 -0500

    handle non-instantiated addons

--- packages/compat/src/build-compat-addon.ts
+++ packages/compat/src/build-compat-addon.ts
@@ -4,6 +4,9 @@ import SmooshPackageJSON from "./smoosh-package-json";
 import broccoliMergeTrees from "broccoli-merge-trees";
 import { Tree } from "broccoli-plugin";
 import OneShot from "./one-shot";
+import Funnel from 'broccoli-funnel';
+import { UnwatchedDir } from 'broccoli-source';
+import EmptyPackageTree from "./empty-package-tree";
 
 export default function cachedBuildCompatAddon(originalPackage: Package, v1Cache: V1InstanceCache): Tree {
   let tree = buildCompatAddon(originalPackage, v1Cache);
@@ -15,17 +18,28 @@ export default function cachedBuildCompatAddon(originalPackage: Package, v1Cache
 
 function buildCompatAddon(originalPackage: Package, v1Cache: V1InstanceCache): Tree {
   if (originalPackage.isV2) {
-    // todo: this case is needed when a native-v2 addon depends on a
+    // this case is needed when a native-v2 addon depends on a
     // non-native-v2 addon. (The non-native one will get rewritten and
     // therefore moved, so to continue depending on it the native one needs to
-    // move too.) It should probably grab the whole package off disk and just
-    // filter out node_modules.
-    throw new Error(`unimplemented`);
+    // move too.)
+    return withoutNodeModules(originalPackage.root);
   }
 
   let oldPackages = v1Cache.getAddons(originalPackage.root);
-  let needsSmooshing = oldPackages.length > 1 && oldPackages[0].hasAnyTrees();
 
+  if (oldPackages.length === 0) {
+    // this happens when the v1 addon wasn't actually getting instantiated at
+    // all, which can happen if the app uses `addons.blacklist` or another addon
+    // uses `shouldIncludeChildAddon`.
+    //
+    // we still keep a place for this addon in the rewritten addon workspace,
+    // because that whole process only depends on looking at all the
+    // package.json files on disk -- it can't know which ones are going to end
+    // up unused at this point.
+    return new EmptyPackageTree();
+  }
+
+  let needsSmooshing = oldPackages[0].hasAnyTrees();
   if (needsSmooshing) {
     let trees = oldPackages.map(pkg => pkg.v2Tree);
     let smoosher = new SmooshPackageJSON(trees);
@@ -33,4 +47,10 @@ function buildCompatAddon(originalPackage: Package, v1Cache: V1InstanceCache): T
   } else {
     return oldPackages[0].v2Tree;
   }
+}
+
+function withoutNodeModules(root: string): Tree {
+  return new Funnel(new UnwatchedDir(root), {
+    exclude: ['node_modules']
+  });
 }
---
+++ packages/compat/src/empty-package-tree.ts
@@ -0,0 +1,19 @@
+import Plugin from "broccoli-plugin";
+import { writeJSONSync } from "fs-extra";
+import { join } from "path";
+
+export default class extends Plugin {
+  private built = false;
+
+  constructor() {
+    super([], {
+      annotation: 'empty-package-tree',
+      persistentOutput: true,
+    });
+  }
+  build() {
+    if (!this.built) {
+      writeJSONSync(join(this.outputPath, 'package.json'), {});
+    }
+  }
+}

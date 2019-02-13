2a90c2936c46cf69c096dcae6736cda0ab387cf3
Edward Faulkner <ef@alum.mit.edu>
2019-02-13 15:12:43 -0500

    expose addon options to default pipeline

--- packages/compat/src/default-pipeline.ts
+++ packages/compat/src/default-pipeline.ts
@@ -1,9 +1,10 @@
-import { App, AppOptions, Addons as CompatAddons } from '.';
+import { App, AppOptions, Addons as CompatAddons, AddonOptions } from '.';
 import { toBroccoliPlugin, PrebuiltAddons, Packager } from '@embroider/core';
 import { Tree } from 'broccoli-plugin';
 
 interface PipelineOptions<PackagerOptions> extends AppOptions {
   packagerOptions?: PackagerOptions;
+  addonOptions?: AddonOptions;
 }
 
 export default function defaultPipeline<PackagerOptions>(
@@ -15,9 +16,9 @@ export default function defaultPipeline<PackagerOptions>(
   if (process.env.REUSE_WORKSPACE) {
     addons = new PrebuiltAddons(__dirname, '/tmp/embroider-workspace');
   } else {
-    addons = new CompatAddons(emberApp, {
+    addons = new CompatAddons(emberApp, Object.assign({
       workspaceDir: '/tmp/embroider-workspace',
-    });
+    }, options && options.addonOptions));
     addons.ready().then(result => {
       console.log(`Building into ${result.outputPath}`);
     });
--- packages/compat/src/index.ts
+++ packages/compat/src/index.ts
@@ -1,4 +1,5 @@
 export { default as App, Options as AppOptions } from './compat-app';
 export { default as Addons } from './compat-addons';
+export { default as AddonOptions } from './options';
 export { default as V1Addon } from './v1-addon';
 export { default as compatBuild } from './default-pipeline';

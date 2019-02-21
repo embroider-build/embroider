558a933372dc8d3b4f05e4d9e2a319efe67ea96a
Edward Faulkner <ef@alum.mit.edu>
2019-02-21 00:24:02 -0500

    it was not correct to try to do identical copy reduction first

--- packages/dedupe/src/index.ts
+++ packages/dedupe/src/index.ts
@@ -1,7 +1,6 @@
 import { Package, PackageCache } from '@embroider/core';
 import { relative } from 'path';
 import assertNever from 'assert-never';
-import partition from 'lodash/partition';
 import { satisfies, compare } from 'semver';
 import { removeSync, symlinkSync } from 'fs-extra';
 
@@ -96,25 +95,6 @@ function optimize(duplicates: Set<Package>[], consumers: Map<Package, Set<Packag
   // packages-to-be-replaced, values are packages-doing-the-replacing.
   let replacements: Map<Package, Package> = new Map();
 
-  // first, all identical copies are trivially replaceable. So we do that first
-  // to reduce the search space.
-  for (let set of duplicates) {
-    let packages = [...set];
-    while (packages.length > 0) {
-      let target = packages.shift()!;
-      let [matched, remaining] = partition(packages, pkg => pkg.version === target.version);
-      for (let m of matched) {
-        replacements.set(m, target);
-        set.delete(m);
-      }
-      packages = remaining;
-    }
-  }
-
-  // the identical copy reduction may have completely solved some packages so
-  // they have no dups remaining.
-  duplicates = duplicates.filter(versions => versions.size > 1);
-
   let candidates = findCandidates(duplicates, consumers, options);
   for (let [replaced, choices] of candidates) {
     // we can't go wrong if we choose to use a package that can't be removed
@@ -131,28 +111,29 @@ function optimize(duplicates: Set<Package>[], consumers: Map<Package, Set<Packag
   return { replacements };
 }
 
+function requiredRange(consumer: Package, dependency: Package, options: Options) {
+  // we already make the correct distinction within Package between when
+  // to care about devDependencies vs dependencies, so it is correct here
+  // to always look in all the sections. Here we're really just trying to
+  // extract the version dependency after having already discovered
+  // elsewhere that the dependency is relevant.
+  for (let section of ['dependencies', 'peerDependencies', 'devDependencies']) {
+    let range = consumer.packageJSON[section] && consumer.packageJSON[section][dependency.name];
+    if (range) {
+      if (!satisfies(dependency.version, range)) {
+        warn(`npm bug detected: ${consumer.name} located at ${relative(options['project-dir'], consumer.root)} started with an invalid version of ${dependency.name}. ${dependency.version} does not satisfy ${range}.`);
+      }
+      return range;
+    }
+  }
+  throw new Error(`bug: didn't find required version of ${dependency.name} in ${consumer.root}`);
+}
+
 function findCandidates(duplicates: Set<Package>[], consumers: Map<Package, Set<Package>>, options: Options) {
   let candidates: Map<Package, Set<Package>> = new Map();
   for (let set of duplicates) {
     for (let pkg of set) {
-      let requirements = [...consumers.get(pkg)!].map(c => {
-        // we already make the correct distinction within Package between when
-        // to care about devDependencies vs dependencies, so it is correct here
-        // to always look in all the sections. Here we're really just trying to
-        // extract the version dependency after having already discovered
-        // elsewhere that the dependency is relevant.
-        for (let section of ['dependencies', 'peerDependencies', 'devDependencies']) {
-          let range = c.packageJSON[section] && c.packageJSON[section][pkg.name];
-          if (range) {
-            if (!satisfies(pkg.version, range)) {
-              warn(`npm bug detected: ${c.name} located at ${relative(options['project-dir'], c.root)} started with an invalid version of ${pkg.name}. ${pkg.version} does not satisfy ${range}.`);
-            }
-            return c.packageJSON[section][pkg.name];
-          }
-        }
-        throw new Error(`bug: didn't find required version of ${pkg.name} in ${c.root}`);
-      });
-
+      let requirements = [...consumers.get(pkg)!].map(c => requiredRange(c, pkg, options));
       let choices = new Set();
 
       for (let otherPkg of set) {
@@ -173,11 +154,16 @@ function findCandidates(duplicates: Set<Package>[], consumers: Map<Package, Set<
 
 export async function inspect(options: Options) {
   let { duplicates, consumers } = await traverse(options);
-  for (let list of duplicates) {
-    for (let pkg of list) {
-      console.log(`${pkg.name} ${pkg.version} ${relative(options['project-dir'], pkg.root)}`);
+  for (let set of duplicates) {
+    let emittedTitle = false;
+    for (let pkg of set) {
+      if (!emittedTitle) {
+        console.log(`=== ${pkg.name} ===`);
+        emittedTitle = true;
+      }
+      console.log(`  ${pkg.version} ${relative(options['project-dir'], pkg.root)}`);
       for (let consumer of consumers.get(pkg)!) {
-        console.log(`    ${consumer.name} ${consumer.version}`);
+        console.log(`    ${consumer.name}-${consumer.version} requires ${requiredRange(consumer, pkg, options)}`);
       }
     }
   }

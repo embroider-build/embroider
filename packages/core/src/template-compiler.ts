3519fdcbe04f723cafe74bf0251a3b32219030e4
Edward Faulkner <ef@alum.mit.edu>
2019-02-24 00:51:49 -0500

    [wip] starting to detect inter-template dependencies

--- packages/core/src/template-compiler.ts
+++ packages/core/src/template-compiler.ts
@@ -10,8 +10,32 @@ export interface Compiler {
   _Ember: any;
 }
 
+const dependencies: Map<string, Set<string>> = new Map();
+
+function buildTimeResolver(env: { moduleName: string }) {
+  let deps = new Set();
+  dependencies.set(env.moduleName, deps);
+  return {
+    name: 'embroider-build-time-resolver',
+    visitor: {
+      MustacheStatement(node: any) {
+        //deps.add(node.path.original);
+        if (node.path.original === 'welcome-page') {
+          console.log(env);
+          debugger;
+        }
+        console.log(`mustache path: ${node.path.original}`);
+      },
+      ElementNode(node: any) {
+        console.log(`element: ${node.tag}`);
+      },
+    }
+  };
+}
+
 export default function(compiler: Compiler, EmberENV: unknown, plugins: Plugins) {
   registerPlugins(compiler, plugins);
+  compiler.registerPlugin('ast', buildTimeResolver);
   initializeEmberENV(compiler, EmberENV);
   return function(moduleName: string, contents: string) {
     let compiled = compiler.precompile(
@@ -20,7 +44,10 @@ export default function(compiler: Compiler, EmberENV: unknown, plugins: Plugins)
         moduleName
       }
     );
-    return 'export default Ember.HTMLBars.template('+compiled+');';
+    let deps = dependencies.get(moduleName);
+    let lines = [...deps!].map(d => `import "${d}";`);
+    lines.push(`export default Ember.HTMLBars.template(${compiled});`);
+    return lines.join("\n");
   };
 }
 

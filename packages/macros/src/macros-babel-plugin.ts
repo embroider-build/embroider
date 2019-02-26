4cdaceea8378026a8cdfd7cf7482b756df3606f3
Edward Faulkner <ef@alum.mit.edu>
2019-02-26 15:32:58 -0500

    modulePresent implementation

--- packages/macros/src/macros-babel-plugin.ts
+++ packages/macros/src/macros-babel-plugin.ts
@@ -1,13 +1,58 @@
+import resolve from 'resolve';
+import { dirname } from 'path';
+
 interface State {
+  removed: any[];
 }
 
 export default function main({ types: t} : { types: any }){
   return {
     visitor: {
+      Program: {
+        enter(_: any, state: State) {
+          state.removed = [];
+        },
+        exit(_: any, state: State) {
+          if (state.removed.length === 0) {
+            return;
+          }
+          let moduleScope = state.removed[0].findParent((path: any) => path.type === 'Program').scope;
+          for (let name of Object.keys(moduleScope.bindings)) {
+            let binding = moduleScope.bindings[name];
+            let bindingPath = binding.path;
+            if (bindingPath.isImportSpecifier() || bindingPath.isImportDefaultSpecifier()) {
+              if (binding.referencePaths.every((path: any) => Boolean(path.findParent((p: any) => state.removed.includes(p))))) {
+                bindingPath.remove();
+                let importPath = bindingPath.parentPath;
+                if (importPath.get('specifiers').length === 0) {
+                  importPath.remove();
+                }
+              }
+            }
+          }
+
+        }
+      },
       ReferencedIdentifier(path: any, state: State) {
         if (path.referencesImport('@embroider/macros', 'modulePresent')) {
-          let r = t.identifier('yup');
-          path.replaceWith(r);
+          if (path.parent.type !== 'CallExpression') {
+            throw new Error(`You can only use modulePresent as a function call`);
+          }
+          if (path.parent.arguments.length !== 1) {
+            throw new Error(`modulePresent takes exactly one argument, you passed ${path.parent.arguments.length}`);
+          }
+          let arg = path.parent.arguments[0];
+          if (arg.type !== 'StringLiteral') {
+            throw new Error(`the argument to modulePresent must be a string literal`);
+          }
+          let sourceFileName = path.hub.file.opts.filename;
+          try {
+            resolve.sync(arg.value, { basedir: dirname(sourceFileName) });
+            path.parentPath.replaceWith(t.booleanLiteral(true));
+          } catch (err) {
+            path.parentPath.replaceWith(t.booleanLiteral(false));
+          }
+          state.removed.push(path.parentPath);
         }
       },
     }
--- packages/macros/tests/module-present-test.ts
+++ packages/macros/tests/module-present-test.ts
@@ -2,11 +2,12 @@ import 'qunit';
 import { transform as transform6, TransformOptions as Options6 } from 'babel-core';
 import { transform as transform7, TransformOptions as Options7 } from '@babel/core';
 import { babelPluginConfig } from '..';
+import { join } from 'path';
 
 const { test } = QUnit;
 
 function createTests(transform: (code: string) => string, babelVersion: number) {
-  QUnit.module(`modulePresent (babel ${babelVersion})`, function(hooks) {
+  QUnit.module(`modulePresent (babel ${babelVersion})`, function() {
 
     test('module is present', function(assert) {
       let code = transform(`
@@ -28,25 +29,79 @@ function createTests(transform: (code: string) => string, babelVersion: number) 
       assert.equal(runDefault(code), false);
     });
 
+    test('multiple uses in one module', function(assert) {
+      let code = transform(`
+      import { modulePresent } from '@embroider/macros';
+      export default function() {
+        return [modulePresent('qunit'), modulePresent('not-a-real-dep')];
+      }
+      `);
+      assert.deepEqual(runDefault(code), [true, false]);
+    });
+
     test('import gets removed', function(assert) {
       let code = transform(`
       import { modulePresent, other } from '@embroider/macros';
       export default function() {
         return modulePresent('not-a-real-dep');
       }
+      export function x() {
+        return other;
+      }
       `);
       assert.ok(!/modulePresent/.test(code), `modulePresent should not be in the output: ${code}`);
       assert.ok(/@embroider\/macros/.test(code), `other import should still be present: ${code}`);
+    });
+
+    test('entire import statement gets removed', function(assert) {
+      let code = transform(`
+      import { modulePresent } from '@embroider/macros';
+      export default function() {
+        return modulePresent('not-a-real-dep');
+      }
+      `);
+      assert.ok(!/modulePresent/.test(code), `modulePresent should not be in the output: ${code}`);
+      assert.ok(!/@embroider\/macros/.test(code), `entire import statement should not be in the output: ${code}`);
+    });
+
+    test('non call error', function(assert) {
+      assert.throws(() => {
+        transform(`
+          import { modulePresent } from '@embroider/macros';
+          let x = modulePresent;
+        `);
+      }, /You can only use modulePresent as a function call/);
+    });
+
+    test('args length error', function(assert) {
+      assert.throws(() => {
+        transform(`
+          import { modulePresent } from '@embroider/macros';
+          modulePresent('foo', 'bar');
+        `);
+      }, /modulePresent takes exactly one argument, you passed 2/);
+    });
+
+    test('non literal arg error', function(assert) {
+      assert.throws(() => {
+        transform(`
+          import { modulePresent } from '@embroider/macros';
+          let name = 'qunit';
+          modulePresent(name);
+        `);
+      }, /the argument to modulePresent must be a string literal/);
     });
   });
 }
 
 const options7: Options7 = {
+  filename: join(__dirname, 'sample.js'),
   presets: [],
   plugins: [babelPluginConfig()]
 };
 
 const options6: Options6 = {
+  filename: join(__dirname, 'sample.js'),
   presets: [],
   plugins: [babelPluginConfig()]
 };

982bbb4c932242689615dbb06b78a5b4a573e582
Edward Faulkner <ef@alum.mit.edu>
2019-02-26 14:12:41 -0500

    test infrastructure for babel6 and 7 with proper types

--- packages/macros/tests/module-present-test.ts
+++ packages/macros/tests/module-present-test.ts
@@ -1,8 +1,26 @@
 import 'qunit';
+import { transform as transform6, TransformOptions as Options6 } from 'babel-core';
+import { transform as transform7, TransformOptions as Options7 } from '@babel/core';
+
 const { test } = QUnit;
 
-QUnit.module('module-present', function(hooks) {
-  test('hello', function(assert) {
-    assert.ok('yes');
+function createTests(transform: (code: string) => string, babelVersion: number) {
+  QUnit.module(`modulePresent (babel ${babelVersion})`, function() {
+    test('hello', function(assert) {
+      assert.equal(transform(`console.log('hello');`), `console.log('hello');`);
+    });
   });
-});
+}
+
+const options7: Options7 = {
+  presets: [],
+  plugins: []
+};
+
+const options6: Options6 = {
+  presets: [],
+  plugins: []
+};
+
+createTests(function(code: string){ return transform6(code, options6).code!; }, 6);
+createTests(function(code: string){ return transform7(code, options7)!.code!; }, 7);

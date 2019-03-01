2483edecd77011e6e0e3b8d8ee97c00338c7d83d
Edward Faulkner <ef@alum.mit.edu>
2019-03-01 08:56:15 -0500

    make test output prettier

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,31 +1,62 @@
 #!/bin/bash
 
-./scripts/parallel --tag <<EOF
-yarn node-test
-cd test-packages/macro-tests && CLASSIC=true yarn test
-cd test-packages/macro-tests && yarn test
-cd test-packages/macro-sample-addon && CLASSIC=true yarn test
+# To add a new test suite, put it into this switch statement and *also* into the
+# list in allSuites below.
+function main {
+  case "$1" in
+  "node")
+    yarn node-test
+    ;;
+  "macro-classic")
+    cd test-packages/macro-tests && CLASSIC=true yarn test
+    ;;
+  "macro")
+    cd test-packages/macro-tests && yarn test
+    ;;
+  "macro-addon")
+    cd test-packages/macro-sample-addon && yarn test
+    ;;
+  "macro-addon-classic")
+    cd test-packages/macro-sample-addon && CLASSIC=true yarn test
+    ;;
+  *)
+    allSuites
+    ;;
+  esac
+}
+
+function allSuites {
+  ./scripts/parallel --tag $0 <<EOF
+  node
+  macro-classic
+  macro
+  macro-addon
+  macro-addon-classic
 EOF
+}
 
+function prettyStatus {
+  if [ -t 1 ] ; then
+    # inside a terminal, so use color
+    RED='\033[0;31m'
+    GREEN='\033[0;32m'
+    NC='\033[0m' # No Color
+
+    if [ $1 -eq 0 ]; then
+      printf "${GREEN}All tests passed${NC}\n"
+    else
+      printf "${RED}Some tests failed${NC}\n"
+    fi
+  else
+    if [ $1 -eq 0 ]; then
+      printf "All tests passed\n"
+    else
+      printf "Some tests failed\n"
+    fi
+  fi
+}
+
+main $1
 status=$?
-
-if [ -t 1 ] ; then
-  # inside a terminal, so use color
-  RED='\033[0;31m'
-  GREEN='\033[0;32m'
-  NC='\033[0m' # No Color
-
-  if [ $status -eq 0 ]; then
-    printf "${GREEN}All tests passed${NC}\n"
-  else
-    printf "${RED}Some tests failed${NC}\n"
-  fi
-else
-  if [ $status -eq 0 ]; then
-    printf "All tests passed\n"
-  else
-    printf "Some tests failed\n"
-  fi
-fi
-
+prettyStatus $status
 exit $status;

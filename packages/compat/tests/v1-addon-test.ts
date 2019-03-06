c7f7db97f10010b6e368f4d1e857acd0b8e7aab3
Edward Faulkner <ef@alum.mit.edu>
2019-03-06 13:23:04 -0500

    experimenting with full ember-cli builds in tests

--- packages/compat/tests/v1-addon-test.ts
+++ packages/compat/tests/v1-addon-test.ts
@@ -1,38 +1,27 @@
-import Project from 'fixturify-project';
+import {
+  emberBuild,
+  emberProject,
+  addAddon
+} from './helpers';
 import 'qunit';
 
 const { test } = QUnit;
 
 QUnit.module('v1-addon', function() {
   test('simple addon with one component', function(assert) {
-    let app = new Project('my-app');
-    app.files = {
-      'ember-cli-build.js': `
-        const EmberApp = require('ember-cli/lib/broccoli/ember-app');
-        module.exports = function(defaults) {
-          let app = new EmberApp(defaults, {
-          });
-          return app.toTree();
-        }
-      `
-    };
-    let addon = app.addDependency('my-addon');
-    addon.files = {
-      'index.js': `
-      module.exports = {
-        name: require('./package').name,
-      };
-      `,
-      addon: {
-        templates: {
-          components: {
-            'hello-world.hbs': '<div>Hello world</div>'
-          }
+    let app = emberProject({
+      workspaceDir: '/tmp/x'
+    });
+    let addon = addAddon(app, 'my-addon');
+    addon.files.addon = {
+      templates: {
+        components: {
+          'hello-world.hbs': '<div>hello world</div>'
         }
       }
     };
-    addon.pkg.keywords = ['ember-addon'];
     app.writeSync();
+    emberBuild(app.baseDir, { STAGE1_ONLY: 'true' });
     assert.ok(true);
   });
 });

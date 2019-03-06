dd9ca825885d5d2d8dfe90c2a98ccf529d9caedb
Edward Faulkner <ef@alum.mit.edu>
2019-03-06 13:27:26 -0500

    more realistic addon component

--- packages/compat/tests/v1-addon-test.ts
+++ packages/compat/tests/v1-addon-test.ts
@@ -8,19 +8,32 @@ import 'qunit';
 const { test } = QUnit;
 
 QUnit.module('v1-addon', function() {
-  test('simple addon with one component', function(assert) {
+  test('full app build', function(assert) {
     let app = emberProject({
       workspaceDir: '/tmp/x'
     });
     let addon = addAddon(app, 'my-addon');
     addon.files.addon = {
+      components: {
+        'hello-world.js': `
+          import Component from '@ember/component';
+          import layout from '../templates/components/hello-world';
+          export default Component.extend({
+            layout
+          });
+        `
+      },
       templates: {
         components: {
           'hello-world.hbs': '<div>hello world</div>'
         }
       }
     };
-    app.writeSync();
+    addon.files.app = {
+      components: {
+        'hello-world.js': `export { default } from 'my-addon/components/hello-world'`
+      }
+    };
     emberBuild(app.baseDir, { STAGE1_ONLY: 'true' });
     assert.ok(true);
   });

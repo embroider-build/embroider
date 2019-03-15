10f0e2af9b4e26a623e99e3835428b345ce74219
Edward Faulkner <ef@alum.mit.edu>
2019-03-15 02:01:48 -0400

    drop usage of macroIf here
    It's not needed and just makes the test harder to read.
    
    (Also it's making master not be green on CI, because I realize we have a bug in macroIf when you use it alongside babel presets.)

--- test-packages/static-app/tests/acceptance/components-example-test.js
+++ test-packages/static-app/tests/acceptance/components-example-test.js
@@ -1,7 +1,7 @@
 import { module, test } from 'qunit';
 import { visit } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
-import { getOwnConfig, macroIf } from '@embroider/macros';
+import { getOwnConfig } from '@embroider/macros';
 
 module('Acceptance | components-example', function(hooks) {
   setupApplicationTest(hooks);
@@ -9,15 +9,19 @@ module('Acceptance | components-example', function(hooks) {
   test('static components', async function(assert) {
     await visit('/components-example');
 
-    assert.ok(document.querySelector('.md-button'), 'found paper-button');
+    let button = document.querySelector('.md-button');
+    assert.ok(button, 'found paper-button');
+    if (button) {
+      assert.equal(getComputedStyle(button)['background-color'], "rgb(63, 81, 181)", "paper-button has its CSS");
+    }
 
     let components = [...document.querySelectorAll("[data-component-name]")].map(elt => elt.dataset.componentName);
     assert.ok(components.includes('paper-button'), 'expected to find paper-button');
 
-    macroIf(getOwnConfig().isClassic, () => {
+    if (getOwnConfig().isClassic) {
       assert.ok(components.includes('paper-dialog'), 'expected to find paper-dialog in classic build');
-    }, () => {
+    } else {
       assert.ok(!components.includes('paper-dialog'), 'expected not to find paper-dialog in embroider build');
-    });
+    }
   });
 });

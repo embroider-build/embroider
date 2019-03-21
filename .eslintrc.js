9dffd2bbf5af828c94706656c6eb8aa6c13264fd
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-03-20 17:48:42 -0700

    ESLint: Enable `no-debugger` rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
   plugins: ['prettier', '@typescript-eslint'],
   extends: ['prettier', 'prettier/@typescript-eslint'],
   rules: {
+    'no-debugger': 'error',
     'no-new-wrappers': 'error',
     'no-var': 'error',
 

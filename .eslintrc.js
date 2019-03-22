9815f753e220cb64a7c762261ba0a9a9ec0af57d
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-03-22 07:27:53 -0700

    ESLint: Enable `no-unused-labels` rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     eqeqeq: ['error', 'smart'],
     'no-debugger': 'error',
     'no-new-wrappers': 'error',
+    'no-unused-labels': 'error',
     'no-var': 'error',
 
     '@typescript-eslint/class-name-casing': 'error',

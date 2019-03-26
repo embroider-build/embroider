e92ef66837e38441bf295997be101830f25102a7
Edward Faulkner <ef@alum.mit.edu>
2019-03-26 13:08:08 -0400

    allow template parallelization

--- packages/webpack/src/webpack-hbs-loader.ts
+++ packages/webpack/src/webpack-hbs-loader.ts
@@ -2,7 +2,11 @@ import { loader } from 'webpack';
 import { getOptions } from 'loader-utils';
 
 export default function hbsLoader(this: loader.LoaderContext, templateContent: string) {
-  let { templateCompiler } = getOptions(this);
+  let { templateCompilerFile } = getOptions(this);
+
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  let templateCompiler = require(templateCompilerFile).compile;
+
   try {
     return templateCompiler(this.resourcePath, templateContent);
   } catch (error) {

d4f67af06fe7fb3c584c9b44da68a8f1845db67d
Gaston Silva Echegaray <gsilvae@riotgames.com>
2019-04-13 00:44:44 -0700

    wip

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,17 @@ module.exports = {
     'no-unused-labels': 'error',
     'no-var': 'error',
 
-    'import/no-extraneous-dependencies': ['error', { devDependencies: ['packages/*/tests/**/*.ts'] }],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          '.eslintrc.js',
+          '**/ember-cli-build.js',
+          '**/config/ember-try.js',
+          'packages/*/tests/**/*.[jt]s',
+        ],
+      },
+    ],
 
     '@typescript-eslint/class-name-casing': 'error',
     '@typescript-eslint/no-angle-bracket-type-assertion': 'error',
@@ -45,6 +55,37 @@ module.exports = {
       rules: {
         'import/no-extraneous-dependencies': 'off',
       },
+    },
+    // node files
+    {
+      files: [
+        '**/.eslintrc.js',
+        '**/.template-lintrc.js',
+        '**/ember-cli-build.js',
+        '**/index.js',
+        '**/testem.js',
+        '**/blueprints/*/index.js',
+        '**/config/**/*.js',
+        '**/tests/dummy/config/**/*.js',
+      ],
+      excludedFiles: [
+        'packages/*/addon/**',
+        'packages/*/addon-test-support/**',
+        'packages/*/app/**',
+        '**/tests/dummy/app/**',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015,
+      },
+      env: {
+        browser: false,
+        node: true,
+      },
+      plugins: ['node'],
+      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
+        // add your custom rules and overrides for node files here
+      }),
     },
   ],
 };

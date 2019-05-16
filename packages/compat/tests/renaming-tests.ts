2fe4782c8e993d5be0032c2d4279ae6ff785d81d
Jen Weber <j@jenweber.me>
2019-05-16 14:48:42 -0400

    Added test coverage for renaming modules features

--- packages/compat/tests/renaming-tests.ts
+++ packages/compat/tests/renaming-tests.ts
@@ -19,6 +19,12 @@ QUnit.module('renaming tests', function(origHooks) {
       'import-capitalize.js': `
         import capitalize from "lodash/capitalize";
         `,
+      'import-own-thing.js': `
+        import ownThing from "emits-multiple-packages/own-thing";
+    `,
+      'import-somebody-elses.js': `
+        import environment from "somebody-elses-package/environment";
+    `,
     };
 
     let addon = app.addAddon(
@@ -30,6 +36,25 @@ QUnit.module('renaming tests', function(origHooks) {
     addon.files.addon = {
       'index.js': `// lodash index`,
       'capitalize.js': `// lodash capitalize`,
+    };
+
+    addon = app.addAddon(
+      'emits-multiple-packages',
+      `
+      treeForAddon(tree) {
+        // doesn't call super so we can escape our namespace
+        return tree;
+      }
+      `
+    );
+
+    addon.files.addon = {
+      'emits-multiple-packages': {
+        'own-thing.js': '// own thing',
+      },
+      'somebody-elses-package': {
+        'environment.js': '// somebody elses environment',
+      },
     };
 
     build = await BuildResult.build(app, {
@@ -49,9 +74,23 @@ QUnit.module('renaming tests', function(origHooks) {
   test('whole package renaming works for top-level module', function(assert) {
     let assertFile = assert.file('components/import-lodash.js').transform(build.transpile);
     assertFile.matches(/import lodash from ["']ember-lodash["']/);
+    assert.file('node_modules/ember-lodash/index.js').matches(/lodash index/);
   });
   test('whole package renaming works for interior module', function(assert) {
     let assertFile = assert.file('components/import-capitalize.js').transform(build.transpile);
     assertFile.matches(/import capitalize from ["']ember-lodash\/capitalize["']/);
+    assert.file('node_modules/ember-lodash/capitalize.js').matches(/lodash capitalize/);
+  });
+  test("modules in own namespace don't get renamed", function(assert) {
+    let assertFile = assert.file('components/import-own-thing.js').transform(build.transpile);
+    assertFile.matches(/import ownThing from ["']emits-multiple-packages\/own-thing["']/);
+    assert.file('node_modules/emits-multiple-packages/own-thing.js').matches(/own thing/);
+  });
+  test('modules outside our namespace do get renamed', function(assert) {
+    let assertFile = assert.file('components/import-somebody-elses.js').transform(build.transpile);
+    assertFile.matches(/import environment from ["']emits-multiple-packages\/somebody-elses-package\/environment["']/);
+    assert
+      .file('node_modules/emits-multiple-packages/somebody-elses-package/environment.js')
+      .matches(/somebody elses environment/);
   });
 });

5746048752cb425ed2906c27fc1ba62c65858af4
Edward Faulkner <ef@alum.mit.edu>
2019-05-17 21:50:44 -0400

    improve brevity of some auto generated relative paths

--- packages/core/src/paths.ts
+++ packages/core/src/paths.ts
@@ -1,4 +1,4 @@
-import { relative } from 'path';
+import { relative, isAbsolute } from 'path';
 
 // by "explicit", I mean that we want "./local/thing" instead of "local/thing"
 // because
@@ -10,6 +10,13 @@ export function explicitRelative(fromDir: string, toFile: string) {
   let result = relative(fromDir, toFile);
   if (!result.startsWith('/') && !result.startsWith('.')) {
     result = './' + result;
+  }
+  if (isAbsolute(toFile) && result.endsWith(toFile)) {
+    // this prevents silly "relative" paths like
+    // "../../../../../Users/you/projects/your/stuff" when we could have just
+    // said "/Users/you/projects/your/stuff". The silly path isn't incorrect,
+    // but it's unnecessarily verbose.
+    return toFile;
   }
   return result;
 }

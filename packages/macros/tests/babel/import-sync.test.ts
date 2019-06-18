51166c22877a1213f74b9b9d032ed2c70e33809e
Edward Faulkner <ef@alum.mit.edu>
2019-06-17 23:10:05 -0400

    importSync macro
    This replaces the previous `import { require } from '@embroider/core'` macro that was internal-only.
    
    `importSync` is intended to be a public macro.
    
    Ember itself really already needs synchronous dynamic imports to exists, so we're not really imposing any extra burden on ourselves to make this a public macro. And making it public helps with #150.

---
+++ packages/macros/tests/babel/import-sync.test.ts
@@ -0,0 +1,34 @@
+import { allBabelVersions } from './helpers';
+
+describe('importSync', function() {
+  allBabelVersions(function createTests(transform: (code: string) => string) {
+    test('importSync becomes require', () => {
+      let code = transform(`
+      import { importSync } from '@embroider/macros';
+      importSync('foo');
+      `);
+      expect(code).toMatch(/require\(['"]foo['"]\)/);
+      expect(code).not.toMatch(/window/);
+    });
+    test('aliased importSync becomes require', () => {
+      let code = transform(`
+      import { importSync as i } from '@embroider/macros';
+      i('foo');
+      `);
+      expect(code).toMatch(/require\(['"]foo['"]\)/);
+      expect(code).not.toMatch(/window/);
+    });
+    test('import of importSync itself gets removed', () => {
+      let code = transform(`
+      import { importSync } from '@embroider/macros';
+      `);
+      expect(code).toEqual('');
+    });
+    test('require becomes window.require', () => {
+      let code = transform(`
+      require('foo');
+      `);
+      expect(code).toMatch(/window\.require\(['"]foo['"]\)/);
+    });
+  });
+});

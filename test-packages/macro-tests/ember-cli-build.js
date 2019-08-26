4e1a5faf6c4fa1c2763b26e1c999fbf9f99e3945
Edward Faulkner <ef@alum.mit.edu>
2019-08-26 16:54:45 -0400

    removing example from macro-tests
    I replaced this with a better test.

--- test-packages/macro-tests/ember-cli-build.js
+++ test-packages/macro-tests/ember-cli-build.js
@@ -33,6 +33,7 @@ module.exports = function(defaults) {
     outputFile: 'apple.js'
   })
 
+
   app.import('vendor/four.js', { outputFile: 'ordered.js' });
   app.import('vendor/two.js', { outputFile: 'ordered.js' });
   app.import('vendor/three.js', { outputFile: 'ordered.js' });
@@ -42,34 +43,6 @@ module.exports = function(defaults) {
   app.import('vendor/prepend/two.js', { prepend: true });
   app.import('vendor/prepend/three.js', { prepend: true });
   app.import('vendor/prepend/four.js', { prepend: true });
-
-  const HIGHCHARTS_BASE_PATH = 'node_modules/highcharts';
-  const highchartsPath = `${HIGHCHARTS_BASE_PATH}/highcharts.src.js`;
-  const highchartsAccessibilityPath = `${HIGHCHARTS_BASE_PATH}/modules/accessibility.src.js`;
-
-  app.import(highchartsPath, { outputFile: 'assets/highcharts/highcharts.js' });
-  app.import(highchartsAccessibilityPath, { outputFile: 'assets/highcharts/modules/accessibility.js' });
-
-  const HIGHLIGHTJS_BASE_PATH = 'node_modules/highlight.js';
-  app.import(`${HIGHLIGHTJS_BASE_PATH}/lib/highlight.js`, {
-    outputFile: 'assets/highlight.js/highlight.js',
-    using: [
-      {
-        transformation: 'amd',
-        as: 'highlight',
-      },
-    ],
-  });
-
-  app.import(`${HIGHLIGHTJS_BASE_PATH}/lib/languages/json.js`, {
-    outputFile: 'assets/highlight.js/languages/json.js',
-    using: [
-      {
-        transformation: 'cjs',
-        as: 'highlight-json',
-      },
-    ],
-  });
 
   if (process.env.CLASSIC) {
     return app.toTree();

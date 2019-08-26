ec8aa34bb8883e17b41b8a5d0e80a61fba749db7
Edward Faulkner <ef@alum.mit.edu>
2019-08-26 16:53:06 -0400

    adding test coverage

--- packages/compat/tests/app-import-test.ts
+++ packages/compat/tests/app-import-test.ts
@@ -1,16 +1,17 @@
 import 'qunit';
 import { Project, BuildResult, installFileAssertions } from '@embroider/test-support';
-
 import { throwOnWarnings } from '@embroider/core';
+import { join } from 'path';
 
 QUnit.module('app.import tests', function(origHooks) {
   let { hooks, test } = installFileAssertions(origHooks);
   let build: BuildResult;
+  let app: Project;
 
   throwOnWarnings(hooks);
 
   hooks.before(async function(assert) {
-    let app = Project.emberNew();
+    app = Project.emberNew();
 
     let addon = app.addAddon(
       'my-addon',
@@ -18,11 +19,16 @@ QUnit.module('app.import tests', function(origHooks) {
       included() {
         this._super.included.apply(this, arguments);
         this.import('vendor/some-font.ttf', { destDir: 'fonts' });
+        this.import('node_modules/third-party/third-party.js', { outputFile: 'assets/tp.js' });
       }
     `
     );
     addon.files.vendor = {
       'some-font.ttf': `some font`,
+    };
+
+    addon.addDependency('third-party', '1.2.3').files = {
+      'third-party.js': '// third party',
     };
 
     build = await BuildResult.build(app, {
@@ -43,5 +49,16 @@ QUnit.module('app.import tests', function(origHooks) {
     let assertFile = assert.file('node_modules/@embroider/synthesized-vendor/package.json').json();
     assertFile.get(['ember-addon', 'public-assets', './vendor/some-font.ttf']).equals('fonts/some-font.ttf');
     assert.file('node_modules/@embroider/synthesized-vendor/vendor/some-font.ttf').exists();
+  });
+
+  test('handle non-transformed node_module with explicit outputFile', function(assert) {
+    let assertFile = assert.file('node_modules/@embroider/synthesized-vendor/package.json').json();
+    assertFile
+      .get([
+        'ember-addon',
+        'public-assets',
+        join(app.baseDir, 'node_modules', 'my-addon', 'node_modules', 'third-party', 'third-party.js'),
+      ])
+      .equals('assets/tp.js');
   });
 });

a434a6835c14a8c8e53a1c717cde2db31b9f94f1
Edward Faulkner <ef@alum.mit.edu>
2019-10-11 15:54:55 -0400

    wiring up eager engine

--- test-packages/eager-engine/index.js
+++ test-packages/eager-engine/index.js
@@ -1,5 +1,11 @@
 'use strict';
 
-module.exports = {
-  name: require('./package').name
-};
+const EngineAddon = require('ember-engines/lib/engine-addon');
+
+module.exports = EngineAddon.extend({
+  name: require('./package').name,
+  // eslint-disable-next-line ember/avoid-leaking-state-in-ember-objects
+  lazyLoading: {
+    enabled: false,
+  },
+});

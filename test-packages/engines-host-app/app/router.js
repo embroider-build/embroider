5b41480852b09c648ff99c9f79fab23e59d71629
Edward Faulkner <ef@alum.mit.edu>
2019-10-11 20:52:59 -0400

    exercising eager-engine and engines-host-app

--- test-packages/engines-host-app/app/router.js
+++ test-packages/engines-host-app/app/router.js
@@ -6,6 +6,9 @@ const Router = EmberRouter.extend({
   rootURL: config.rootURL,
 });
 
-Router.map(function() {});
+Router.map(function() {
+  this.route('use-eager-engine');
+  this.route('style-check');
+});
 
 export default Router;
---
+++ test-packages/engines-host-app/tests/acceptance/basics-test.js
@@ -0,0 +1,21 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | basics', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('host-app', async function(assert) {
+    await visit('/');
+    assert.equal(currentURL(), '/');
+    assert.dom('[data-test-duplicated-helper]').containsText('from-engines-host-app');
+  });
+
+  test('eager-engine', async function(assert) {
+    await visit('/use-eager-engine');
+    assert.equal(currentURL(), '/use-eager-engine');
+    assert.dom('[data-test-eager-engine-main] > h1').containsText('Eager engine');
+    assert.dom('[data-test-truth-helpers-ok]').exists();
+    assert.dom('[data-test-duplicated-helper]').containsText('from-eager-engine-helper');
+  });
+});

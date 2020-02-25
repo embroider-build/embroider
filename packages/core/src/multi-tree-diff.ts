e99f515c142032d19aa1deef1f056c2a619b401e
Edward Faulkner <ef@alum.mit.edu>
2020-02-25 13:25:05 -0500

    enhancing multi-tree-diff to support file merges

--- packages/core/src/multi-tree-diff.ts
+++ packages/core/src/multi-tree-diff.ts
@@ -1,4 +1,5 @@
 import FSTree, { Operation, Entry } from 'fs-tree-diff';
+import lodashIsEqual from 'lodash/isEqual';
 
 export interface InputTree {
   walk(): Entry[];
@@ -6,45 +7,112 @@ export interface InputTree {
 }
 
 // tells you which of your inTrees (by index) resulted in the given output file
-export type Sources = Map<string, number>;
+export class Sources {
+  constructor(
+    private combinedEntries: ReturnType<MultiTreeDiff['combinedEntries']>,
+    private combinedOwners: MultiTreeDiff['combinedOwners']
+  ) {}
+  get(relativePath: string): number[] {
+    return this.combinedOwners.get(this.combinedEntries.get(relativePath)!)!;
+  }
+}
+
+// this is how you control what happens when multiple trees try to output the
+// same path. Your merger function is told which trees are trying to collide,
+// and you say which ones will be allowed to influence the output. More than one
+// winner is allowed because sometimes you will be merging their contents.
+export type Merger = (treeIndices: number[]) => number[];
 
 export default class MultiTreeDiff {
   private prevEntries: Entry[][] | undefined;
   private prevCombined: FSTree = new FSTree();
+
+  // tracks which input Entry is owned by which input tree
   private owners: WeakMap<Entry, number> = new WeakMap();
 
-  constructor(private inTrees: InputTree[]) {}
+  // tracks which output Entry is owned by which set of input trees. This is
+  // different from `owners` because merging is possible.
+  private combinedOwners: WeakMap<Entry, number[]> = new WeakMap();
 
-  update(): { ops: Operation[]; sources: Sources } {
-    let combinedEntries: Entry[] = [];
-    let sources: Map<string, number> = new Map();
+  constructor(private inTrees: InputTree[], private merger: Merger) {}
 
-    let entries = this.inTrees.map((tree, index) => {
+  private allEntries(): Entry[][] {
+    let result = this.inTrees.map((tree, index) => {
       if (!tree.mayChange && this.prevEntries && this.prevEntries[index]) {
         return this.prevEntries[index];
       }
       return tree.walk();
     });
-    this.prevEntries = entries;
+    this.prevEntries = result;
+    return result;
+  }
 
+  private candidates(entries: Entry[][]): Map<string, Entry[]> {
+    let result: Map<string, Entry[]> = new Map();
     for (let [treeIndex, treeEntries] of entries.entries()) {
       for (let entry of treeEntries) {
-        sources.set(entry.relativePath, treeIndex);
+        let list = result.get(entry.relativePath);
+        if (!list) {
+          list = [];
+          result.set(entry.relativePath, list);
+        }
+        list.push(entry);
         this.owners.set(entry, treeIndex);
       }
-      combinedEntries = combinedEntries.concat(treeEntries);
-      treeIndex++;
     }
+    return result;
+  }
 
-    // FSTree requires the entries to be sorted and uniq. We achieve uniqueness
-    // by only keeping the winner for each relativePath.
-    combinedEntries = combinedEntries.filter(entry => this.owners.get(entry) === sources.get(entry.relativePath));
-    combinedEntries.sort(compareByRelativePath);
+  private combinedEntries(candidates: Map<string, Entry[]>): Map<string, Entry> {
+    let result: Map<string, Entry> = new Map();
+    for (let [relativePath, entries] of candidates.entries()) {
+      if (entries.length === 1) {
+        let [entry] = entries;
+        // no collision, simple case.
+        result.set(relativePath, entry);
+        this.combinedOwners.set(entry, [this.owners.get(entry)!]);
+      } else {
+        // collision, apply merge logic
+        let winners = this.merger(entries.map(e => this.owners.get(e)!));
+        if (winners.length === 1) {
+          // single winner, no merging
+          let winner = entries.find(e => this.owners.get(e) === winners[0])!;
+          result.set(relativePath, winner);
+          this.combinedOwners.set(winner, winners);
+        } else {
+          // multiple winners, must synthesize a combined entry
+          let winningEntries = entries.filter(e => winners.includes(this.owners.get(e)!));
+          let combinedEntry: Entry = {
+            relativePath,
+            size: winningEntries.reduce((accum, entry) => {
+              return accum + (entry.size || 0);
+            }, 0),
+            mtime: winningEntries.reduce((accum: undefined | number | Date, entry) => {
+              return latest(accum, entry.mtime);
+            }, undefined),
+            isDirectory() {
+              return false;
+            },
+          };
+          result.set(relativePath, combinedEntry);
+          this.combinedOwners.set(combinedEntry, winners);
+        }
+      }
+    }
+    return result;
+  }
 
-    let newFSTree = FSTree.fromEntries(combinedEntries);
-    let ops = this.prevCombined.calculatePatch(newFSTree, isEqual(this.owners));
+  update(): { ops: Operation[]; sources: Sources } {
+    let combinedEntries = this.combinedEntries(this.candidates(this.allEntries()));
+
+    // FSTree requires the entries to be sorted and uniq. We already have
+    // uniqueness because we're taking them out of a map. And here we do the
+    // sort.
+    let combinedEntriesList = [...combinedEntries.values()].sort(compareByRelativePath);
+    let newFSTree = FSTree.fromEntries(combinedEntriesList);
+    let ops = this.prevCombined.calculatePatch(newFSTree, isEqual(this.combinedOwners));
     this.prevCombined = newFSTree;
-    return { ops, sources };
+    return { ops, sources: new Sources(combinedEntries, this.combinedOwners) };
   }
 }
 
@@ -60,8 +128,24 @@ function compareByRelativePath(entryA: Entry, entryB: Entry) {
   return 0;
 }
 
-function isEqual(owners: WeakMap<Entry, number>) {
+function isEqual(owners: WeakMap<Entry, number[]>) {
   return function(a: Entry, b: Entry) {
-    return FSTree.defaultIsEqual(a, b) && owners.get(a) === owners.get(b);
+    return FSTree.defaultIsEqual(a, b) && lodashIsEqual(owners.get(a), owners.get(b));
   };
+}
+
+function latest(a: number | Date | undefined, b: number | Date | undefined): number | undefined | Date {
+  if (a == null) {
+    return b;
+  }
+  if (b == null) {
+    return a;
+  }
+  if (a instanceof Date) {
+    a = a.getTime();
+  }
+  if (b instanceof Date) {
+    b = b.getTime();
+  }
+  return Math.max(a, b);
 }
--- packages/core/tests/multi-tree-diff.test.ts
+++ packages/core/tests/multi-tree-diff.test.ts
@@ -3,13 +3,21 @@ import { Entry, Patch } from 'fs-tree-diff';
 import { join, sep } from 'path';
 import cloneDeep from 'lodash/cloneDeep';
 
+function lastOneWins(treeIds: number[]) {
+  return treeIds.slice(-1);
+}
+
+function lastTwoMerge(treeIds: number[]) {
+  return treeIds.slice(-2);
+}
+
 describe('tracked-merge-dirs', () => {
   test('it combines files from all inDirs', () => {
     let a = new MockTree(['alpha', 'tomster']);
     let b = new MockTree({ beta: ['x'] });
     let c = new MockTree(['charlie']);
 
-    let t = new MultiTreeDiff([a, b, c]);
+    let t = new MultiTreeDiff([a, b, c], lastOneWins);
     let { ops, sources } = t.update();
 
     expect(fileOps(ops)).toEqual([
@@ -20,25 +28,25 @@ describe('tracked-merge-dirs', () => {
       ['create', 'tomster'],
     ]);
 
-    expect(sources.get('alpha')).toBe(0);
-    expect(sources.get('beta')).toBe(1);
-    expect(sources.get(`beta${sep}x`)).toBe(1);
-    expect(sources.get('charlie')).toBe(2);
-    expect(sources.get('tomster')).toBe(0);
+    expect(sources.get('alpha')).toEqual([0]);
+    expect(sources.get('beta')).toEqual([1]);
+    expect(sources.get(`beta${sep}x`)).toEqual([1]);
+    expect(sources.get('charlie')).toEqual([2]);
+    expect(sources.get('tomster')).toEqual([0]);
   });
 
   test('it prioritizes files from later dirs', () => {
     let a = new MockTree(['alpha', 'tomster']);
     let c = new MockTree(['charlie', 'alpha']);
 
-    let t = new MultiTreeDiff([a, c]);
+    let t = new MultiTreeDiff([a, c], lastOneWins);
     let { ops, sources } = t.update();
 
     expect(fileOps(ops)).toEqual([['create', 'alpha'], ['create', 'charlie'], ['create', 'tomster']]);
 
-    expect(sources.get('alpha')).toBe(1);
-    expect(sources.get('charlie')).toBe(1);
-    expect(sources.get('tomster')).toBe(0);
+    expect(sources.get('alpha')).toEqual([1]);
+    expect(sources.get('charlie')).toEqual([1]);
+    expect(sources.get('tomster')).toEqual([0]);
   });
 
   test('it emits nothing when stable', () => {
@@ -46,7 +54,7 @@ describe('tracked-merge-dirs', () => {
     let b = new MockTree({ beta: ['x'] });
     let c = new MockTree(['charlie']);
 
-    let t = new MultiTreeDiff([a, b, c]);
+    let t = new MultiTreeDiff([a, b, c], lastOneWins);
     t.update();
     let { ops } = t.update();
     expect(fileOps(ops)).toEqual([]);
@@ -57,38 +65,38 @@ describe('tracked-merge-dirs', () => {
     let b = new MockTree({ beta: ['x'] });
     let c = new MockTree(['charlie']);
 
-    let t = new MultiTreeDiff([a, b, c]);
+    let t = new MultiTreeDiff([a, b, c], lastOneWins);
     t.update();
     dirty(a.entries[0]);
     let { ops, sources } = t.update();
     expect(fileOps(ops)).toEqual([['change', 'alpha']]);
-    expect(sources.get('alpha')).toBe(0);
+    expect(sources.get('alpha')).toEqual([0]);
   });
 
   test('it falls back to earlier source at deletion', () => {
     let a = new MockTree(['alpha', 'tomster']);
     let c = new MockTree(['charlie', 'alpha']);
 
-    let t = new MultiTreeDiff([a, c]);
+    let t = new MultiTreeDiff([a, c], lastOneWins);
 
     let result = t.update();
-    expect(result.sources.get('alpha')).toBe(1);
+    expect(result.sources.get('alpha')).toEqual([1]);
 
     c.entries.splice(0, 1);
 
     result = t.update();
     expect(fileOps(result.ops)).toEqual([['change', 'alpha']]);
-    expect(result.sources.get('alpha')).toBe(0);
+    expect(result.sources.get('alpha')).toEqual([0]);
   });
 
   test('it switches to later source at creation', () => {
     let a = new MockTree(['alpha', 'tomster']);
     let b = new MockTree(['charlie']);
 
-    let t = new MultiTreeDiff([a, b]);
+    let t = new MultiTreeDiff([a, b], lastOneWins);
 
     let result = t.update();
-    expect(result.sources.get('alpha')).toBe(0);
+    expect(result.sources.get('alpha')).toEqual([0]);
 
     b.entries.push({
       relativePath: 'alpha',
@@ -100,14 +108,14 @@ describe('tracked-merge-dirs', () => {
 
     result = t.update();
     expect(fileOps(result.ops)).toEqual([['change', 'alpha']]);
-    expect(result.sources.get('alpha')).toBe(1);
+    expect(result.sources.get('alpha')).toEqual([1]);
   });
 
   test('it hides changes in occluded files', () => {
     let a = new MockTree(['alpha']);
     let b = new MockTree(['alpha']);
 
-    let t = new MultiTreeDiff([a, b]);
+    let t = new MultiTreeDiff([a, b], lastOneWins);
     t.update();
     dirty(a.entries[0]);
     let { ops } = t.update();
@@ -117,11 +125,71 @@ describe('tracked-merge-dirs', () => {
   test('it respects mayChange', () => {
     let a = new MockTree(['alpha', 'tomster']);
     a.mayChange = false;
-    let t = new MultiTreeDiff([a]);
+    let t = new MultiTreeDiff([a], lastOneWins);
     t.update();
     dirty(a.entries[0]);
     let { ops } = t.update();
     expect(fileOps(ops)).toEqual([]);
+  });
+
+  test('it can merge files', () => {
+    let a = new MockTree(['alpha', 'tomster']);
+    let c = new MockTree(['charlie', 'alpha']);
+
+    let t = new MultiTreeDiff([a, c], lastTwoMerge);
+    let { ops, sources } = t.update();
+
+    expect(fileOps(ops)).toEqual([['create', 'alpha'], ['create', 'charlie'], ['create', 'tomster']]);
+
+    expect(sources.get('alpha')).toEqual([0, 1]);
+    expect(sources.get('charlie')).toEqual([1]);
+    expect(sources.get('tomster')).toEqual([0]);
+  });
+
+  test('it updates merged file when one side changes', () => {
+    let a = new MockTree(['alpha', 'tomster']);
+    let c = new MockTree(['charlie', 'alpha']);
+
+    let t = new MultiTreeDiff([a, c], lastTwoMerge);
+    t.update();
+
+    dirty(a.entries[0]);
+    let { ops, sources } = t.update();
+    expect(fileOps(ops)).toEqual([['change', 'alpha']]);
+    expect(sources.get('alpha')).toEqual([0, 1]);
+  });
+
+  test('it updates merged file when one side is deleted', () => {
+    let a = new MockTree(['alpha', 'tomster']);
+    let c = new MockTree(['charlie', 'alpha']);
+
+    let t = new MultiTreeDiff([a, c], lastTwoMerge);
+    t.update();
+
+    a.entries.splice(0, 1);
+    let { ops, sources } = t.update();
+    expect(fileOps(ops)).toEqual([['change', 'alpha']]);
+    expect(sources.get('alpha')).toEqual([1]);
+  });
+
+  test('it introduces merge at creation', () => {
+    let a = new MockTree(['alpha', 'tomster']);
+    let c = new MockTree(['charlie']);
+
+    let t = new MultiTreeDiff([a, c], lastTwoMerge);
+    let { ops, sources } = t.update();
+    expect(sources.get('alpha')).toEqual([0]);
+
+    c.entries.push({
+      relativePath: 'alpha',
+      mode: 33188,
+      size: 1000,
+      mtime: Date.now(),
+      isDirectory: () => false,
+    });
+    ({ ops, sources } = t.update());
+    expect(fileOps(ops)).toEqual([['change', 'alpha']]);
+    expect(sources.get('alpha')).toEqual([0, 1]);
   });
 });
 

36435d1d46534b3893ff23899d98b525cede6354
sparshithNR <nairbali@usc.edu>
2020-04-22 16:36:31 -0700

    Adding lazy engine CSS Style tests

--- test-packages/engines-host-app/tests/acceptance/basics-test.js
+++ test-packages/engines-host-app/tests/acceptance/basics-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { visit, currentURL } from '@ember/test-helpers';
-import { setupApplicationTest, skip } from 'ember-qunit';
+import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | basics', function(hooks) {
   setupApplicationTest(hooks);
@@ -18,13 +18,48 @@ module('Acceptance | basics', function(hooks) {
     await visit('/');
     let entriesBefore = Object.entries(window.require.entries).length;
     assert.notOk(window.require.entries['lazy-engine/_app_/helpers/duplicated-helper']);
+
+    // do the style check before we visit the lazy engine
+    await visit('/style-check');
+    assert.dom('.shared-style-target').exists();
+    assert.equal(
+      getComputedStyle(document.querySelector('.shared-style-target'))['border-left-color'],
+      'rgb(0, 0, 255)',
+      'eager-engine styles are present'
+    );
+
+    // verify lazy engine styles are not present
+    assert.equal(
+      getComputedStyle(document.querySelector('.shared-style-target'))['border-right-color'],
+      'rgb(0, 0, 0)',
+      'lazy-engine styles are not present'
+    );
+
+    // load lazy engine
     await visit('/use-lazy-engine');
+
+    // lazy engine JS loaded
     let entriesAfter = Object.entries(window.require.entries).length;
     assert.ok(window.require.entries['lazy-engine/_app_/helpers/duplicated-helper']);
     assert.ok(entriesAfter > entriesBefore);
     assert.equal(currentURL(), '/use-lazy-engine');
     assert.dom('[data-test-lazy-engine-main] > h1').containsText('Lazy engine');
     assert.dom('[data-test-duplicated-helper]').containsText('from-lazy-engine');
+
+    // lazy engine styles are loaded
+    await visit('/style-check');
+
+    assert.equal(
+      getComputedStyle(document.querySelector('.shared-style-target'))['border-left-color'],
+      'rgb(0, 0, 255)',
+      'eager-engine styles are still present'
+    );
+
+    assert.equal(
+      getComputedStyle(document.querySelector('.shared-style-target'))['border-right-color'],
+      'rgb(255, 0, 0)',
+      'now lazy-engine styles are present'
+    );
   });
 
   test('eager-engine', async function(assert) {
@@ -33,44 +68,5 @@ module('Acceptance | basics', function(hooks) {
     assert.dom('[data-test-eager-engine-main] > h1').containsText('Eager engine');
     assert.dom('[data-test-truth-helpers-ok]').exists();
     assert.dom('[data-test-duplicated-helper]').containsText('from-eager-engine-helper');
-  });
-
-  test('styles', async function(assert) {
-    await visit('/style-check');
-    assert.dom('.shared-style-target').exists();
-
-    assert.equal(
-      getComputedStyle(document.querySelector('.shared-style-target'))['border-left-width'],
-      '2px',
-      'eager-engine styles are present'
-    );
-
-    // TODO: uncomment this after implement lazy styles. See skipped test below
-    // that I left as a reminder.
-    //
-    // assert.equal(
-    //   getComputedStyle(document.querySelector('.shared-style-target'))['border-right-width'],
-    //   '0px',
-    //   'lazy-engine styles are not present'
-    // );
-
-    await visit('/use-lazy-engine');
-    await visit('/style-check');
-
-    assert.equal(
-      getComputedStyle(document.querySelector('.shared-style-target'))['border-left-width'],
-      '2px',
-      'eager-engine styles are still present'
-    );
-
-    assert.equal(
-      getComputedStyle(document.querySelector('.shared-style-target'))['border-right-width'],
-      '2px',
-      'now lazy-engine styles are present'
-    );
-  });
-
-  skip('lazy styles are not present until after lazy engine loads', function() {
-    // See commented assertion in previous test.
   });
 });

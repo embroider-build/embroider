c15ee958aee2a4f6b44883e2cce449257bb978c2
Travis Hoover <thoov7@gmail.com>
2020-04-30 10:38:55 -0700

    Reuse engines-host-app test

--- test-packages/router-with-engines-in-classical.test.js
+++
@@ -1,9 +0,0 @@
-const execa = require('execa');
-
-test('router-with-engines-in-classical', async () => {
-  jest.setTimeout(120000);
-
-  await execa('yarn', ['test'], {
-    cwd: `${__dirname}/router-with-engines-in-classical`,
-  });
-});

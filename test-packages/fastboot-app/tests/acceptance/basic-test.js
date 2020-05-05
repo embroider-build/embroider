780fd913b6cfd612398769cd322c4fbf27636ca3
Edward Faulkner <ef@alum.mit.edu>
2020-05-05 04:16:06 -0400

    improve factoring of in-browser tests

--- test-packages/fastboot-app/tests/acceptance/basic-test.js
+++ test-packages/fastboot-app/tests/acceptance/basic-test.js
@@ -1,16 +1,43 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
-import { setupApplicationTest } from 'ember-qunit';
+import {
+  visit,
+  setupApplicationContext,
+  teardownApplicationContext,
+  setupContext,
+  teardownContext,
+} from '@ember/test-helpers';
 
 module('Acceptance | runtime basics', function(hooks) {
-  setupApplicationTest(hooks);
-  test('visiting /', async function(assert) {
+  let opts = { waitForSettled: true };
+
+  hooks.before(async function() {
+    await setupContext(this, opts);
+    await setupApplicationContext(this);
     await visit('/');
-    assert.equal(currentURL(), '/');
+  });
+
+  hooks.after(async function() {
+    await teardownApplicationContext(this, opts);
+    await teardownContext(this, opts);
+  });
+
+  test('content is rendered', function(assert) {
     assert.dom('[data-test="hello"]').containsText('Hello from fastboot-app');
+  });
+
+  test('found browser implementation of in-app module', function(assert) {
     assert.dom('[data-test="example"]').containsText('This is the browser implementation');
+  });
+
+  test('found browser implementation of addon service', function(assert) {
     assert.dom('[data-test="addon-example"]').containsText('Browser AddonExampleService');
+  });
+
+  test('found no fastboot-only service from the app', function(assert) {
     assert.dom('[data-test="check-service"]').containsText('No service present');
+  });
+
+  test('found no fastboot-only file from the addon', function(assert) {
     assert.dom('[data-test="check-addon-file"]').containsText('No addon file value');
   });
 });

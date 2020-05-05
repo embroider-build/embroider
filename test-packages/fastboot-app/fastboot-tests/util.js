2dd47effeaec79ca73a56c2e90c3f25217f241c3
Edward Faulkner <ef@alum.mit.edu>
2020-05-05 04:28:02 -0400

    improving fastboot basic tests

--- test-packages/fastboot-app/fastboot-tests/util.js
+++ test-packages/fastboot-app/fastboot-tests/util.js
@@ -2,10 +2,8 @@
 
 const FastBoot = require('fastboot');
 const { execFileSync } = require('child_process');
-const jsdom = require("jsdom");
+const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
-
-
 
 module.exports = function setup(hooks) {
   let fastboot;
@@ -15,7 +13,7 @@ module.exports = function setup(hooks) {
       expectStatus = 200;
     }
     let visitOpts = {
-      request: { headers: { host: 'localhost:4200' } }
+      request: { headers: { host: 'localhost:4200' } },
     };
     let page = await fastboot.visit(url, visitOpts);
     assert.equal(page.statusCode, expectStatus, `status code for ${url}`);
@@ -24,15 +22,16 @@ module.exports = function setup(hooks) {
     return dom.window.document;
   }
 
-  hooks.before(async function() {
+  hooks.before(async function(assert) {
     if (!process.env.REUSE_FASTBOOT_BUILD) {
       execFileSync('node', ['./node_modules/.bin/ember', 'build']);
       process.env.REUSE_FASTBOOT_BUILD = 'true';
     }
     fastboot = new FastBoot({
       distPath: 'dist',
-      resilient: false
+      resilient: false,
     });
+    this.visit = visit.bind(this, assert);
   });
   hooks.beforeEach(function(assert) {
     this.visit = visit.bind(this, assert);

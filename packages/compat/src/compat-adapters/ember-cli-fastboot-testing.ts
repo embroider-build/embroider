38e393bd3c18b2487c640500de9e9b76601189bd
Edward Faulkner <ef@alum.mit.edu>
2020-05-28 13:32:50 -0400

    use side-effect of v2Tree instead
    to avoid typescript issue

--- packages/compat/src/compat-adapters/ember-cli-fastboot-testing.ts
+++ packages/compat/src/compat-adapters/ember-cli-fastboot-testing.ts
@@ -1,8 +1,8 @@
 import V1Addon from '../v1-addon';
 
 export default class EmberCliFastbootTesting extends V1Addon {
-  constructor() {
-    super(...arguments);
+  get v2Tree() {
+    let tree = super.v2Tree;
     let originalOutputReady = this.addonInstance.outputReady;
     let projectRoot = this.addonInstance.project.root;
     this.addonInstance.outputReady = function() {
@@ -10,5 +10,6 @@ export default class EmberCliFastbootTesting extends V1Addon {
         directory: `${projectRoot}/dist`,
       });
     };
+    return tree;
   }
 }

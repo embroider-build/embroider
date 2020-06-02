76211eb888d83284c4351ec6fd735de45f0a260c
Edward Faulkner <ef@alum.mit.edu>
2020-06-01 22:21:13 -0400

    adding fastboot test coverage to engines app

---
+++ test-packages/engines-host-app/fastboot-tests/engine-fastboot-tests.js
@@ -0,0 +1,25 @@
+/* eslint-env node */
+
+const { module: Qmodule, test } = require('qunit');
+const setup = require('./util');
+
+Qmodule('fastboot basics', function(hooks) {
+  setup(hooks);
+
+  test('host-app', async function(assert) {
+    let doc = await this.visit('/');
+    assert.equal(doc.querySelector('[data-test-duplicated-helper]').textContent.trim(), 'from-engines-host-app');
+  });
+
+  test('lazy-engine', async function(assert) {
+    let doc = await this.visit('/use-lazy-engine');
+    assert.equal(doc.querySelector('[data-test-lazy-engine-main] > h1').textContent.trim(), 'Lazy engine');
+    assert.equal(doc.querySelector('[data-duplicated-helper]').textContent.trim(), 'from-lazy-engine');
+  });
+
+  test('eager-engine', async function(assert) {
+    let doc = await this.visit('/use-eager-engine');
+    assert.equal(doc.querySelector('[data-test-eager-engine-main] > h1').textContent.trim(), 'Eager engine');
+    assert.equal(doc.querySelector('[data-duplicated-helper]').textContent.trim(), 'from-eager-engine-helper');
+  });
+});

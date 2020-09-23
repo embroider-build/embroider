514b762129774e46f9d342f392472563fbc71513
Travis Hoover <thoov7@gmail.com>
2020-09-23 11:35:45 -0700

    adjusting the test

--- test-packages/static-app/tests/unit/missing-imports-test.js
+++ test-packages/static-app/tests/unit/missing-imports-test.js
@@ -2,15 +2,11 @@ import { module, test } from 'qunit';
 import { importSync } from '@embroider/macros';
 
 module('Unit | missing-imports', function() {
-  test('it works', async function(assert) {
-    assert.expect(2);
+  test('it works', function(assert) {
+    assert.expect(1);
 
     assert.throws(() => {
       importSync('missingModule');
     }, /missing module/);
-
-    // assert.throws(async () => {
-    //   await import('missingModule');
-    // }, /missing module/);
   });
 });

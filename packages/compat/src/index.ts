6d525e7a363c057b0c4b9a53619d036bda6317d2
Edward Faulkner <ef@alum.mit.edu>
2020-10-01 13:20:20 -0400

    adding test-setup package

--- packages/compat/src/index.ts
+++ packages/compat/src/index.ts
@@ -1,7 +1,7 @@
 export { default as App } from './compat-app';
 export { default as Addons } from './compat-addons';
 export { default as PrebuiltAddons } from './prebuilt-addons';
-export { default as Options } from './options';
+export { default as Options, recommendedOptions } from './options';
 export { default as V1Addon } from './v1-addon';
 export { default as compatBuild } from './default-pipeline';
 export { PackageRules, ModuleRules } from './dependency-rules';

fee2c2e8c13e26378c412ce12e3f1282be649638
Edward Faulkner <ef@alum.mit.edu>
2020-10-16 13:04:21 -0400

    run new addon's test suite

---
+++ test-packages/addon.test.js
@@ -0,0 +1,9 @@
+const execa = require('execa');
+
+test('macro', async () => {
+  jest.setTimeout(120000);
+
+  await execa('yarn', ['test'], {
+    cwd: `${__dirname}/../packages/addon`,
+  });
+});

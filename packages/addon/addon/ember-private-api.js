c5451255ae7d8bc55a78837000c9f4a377d8f711
Edward Faulkner <ef@alum.mit.edu>
2020-10-16 17:30:25 -0400

    support ember 3.16

--- packages/addon/addon/ember-private-api.js
+++ packages/addon/addon/ember-private-api.js
@@ -3,8 +3,22 @@ const { isCurriedComponentDefinition, CurriedComponentDefinition } = Ember.__loa
 
 export { isCurriedComponentDefinition };
 
-export function lookupCurriedComponentDefinition(name, owner) {
+function runtimeResolver(owner) {
   let resolver = owner.lookup('renderer:-dom')._runtimeResolver;
+  if (resolver) {
+    return resolver;
+  }
+
+  let entry = Object.entries(owner.__container__.cache).find(e => e[0].startsWith('template-compiler:main-'));
+  if (entry) {
+    return entry[1].resolver.resolver;
+  }
+
+  throw new Error(`@embroider/addon couldn't locate the runtime resolver on this ember version`);
+}
+
+export function lookupCurriedComponentDefinition(name, owner) {
+  let resolver = runtimeResolver(owner);
   let handle = resolver.lookupComponentHandle(name, { owner });
   if (handle != null) {
     return new CurriedComponentDefinition(resolver.resolve(handle), null);

79136af7175d2276593cbe7108c1e7fe951c9609
Edward Faulkner <ef@alum.mit.edu>
2020-10-17 00:01:04 -0400

    test suite config

--- test-packages/addon.test.js
+++ test-packages/addon.test.js
@@ -1,7 +1,8 @@
 const execa = require('execa');
 
 test('macro', async () => {
-  jest.setTimeout(120000);
+  // this runs the entire ember-try matrix, so a long timeout is appropriate
+  jest.setTimeout(600000);
 
   await execa('yarn', ['test'], {
     cwd: `${__dirname}/../packages/addon`,

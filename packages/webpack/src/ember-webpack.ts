b10baf5705e3a2413ba31bca5399dfe819f32333
simonihmig <simon.ihmig@kaliber5.de>
2020-11-19 19:17:50 +0100

    Exclude testem.js from fingerprinting

--- packages/webpack/src/ember-webpack.ts
+++ packages/webpack/src/ember-webpack.ts
@@ -208,7 +208,7 @@ const Webpack: Packager<Options> = class Webpack implements PackagerInstance {
   }
 
   private async writeScript(script: string, written: Set<string>, variant: Variant) {
-    if (!variant.optimizeForProduction) {
+    if (!variant.optimizeForProduction || script.match(/testem/)) {
       this.copyThrough(script);
       return script;
     }

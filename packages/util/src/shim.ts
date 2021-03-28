725c5268df9a543f948c0d24bf8ab7bd8b0058d0
Edward Faulkner <ef@alum.mit.edu>
2021-03-28 11:44:47 -0400

    expand app-js

--- packages/util/src/shim.ts
+++ packages/util/src/shim.ts
@@ -1,20 +1,46 @@
 import { resolve } from 'path';
 import { readFileSync } from 'fs';
+import type { AddonMeta } from '@embroider/shared-internals';
+import Funnel from 'broccoli-funnel';
 
 export interface ShimOptions {
   testApp?: string;
+}
+
+function addonMeta(pkgJSON: any): AddonMeta {
+  let meta = pkgJSON['ember-addon'];
+  if (meta?.version !== 2 || meta?.type !== 'addon') {
+    throw new Error(`did not find valid v2 addon metadata in ${pkgJSON.name}`);
+  }
+  return meta as AddonMeta;
 }
 
 export function addonV1Shim(directory: string, options: ShimOptions = {}) {
   let pkg = JSON.parse(
     readFileSync(resolve(directory, './package.json'), 'utf8')
   );
+  let meta = addonMeta(pkg);
   return {
     name: pkg.name,
     treeForApp(this: AddonInstance) {
-      let appJS = pkg['ember-addon']?.['app-js'];
-      if (appJS) {
-        return this.treeGenerator(resolve(directory, appJS));
+      let maybeAppJS = meta['app-js'];
+      if (maybeAppJS) {
+        const appJS = maybeAppJS;
+        return new Funnel(this.treeGenerator(directory), {
+          files: Object.values(appJS),
+          getDestinationPath(relativePath: string): string {
+            for (let [exteriorName, interiorName] of Object.entries(appJS)) {
+              if (relativePath === interiorName) {
+                return exteriorName;
+              }
+            }
+            throw new Error(
+              `bug in addonV1Shim, no match for ${relativePath} in ${JSON.stringify(
+                appJS
+              )}`
+            );
+          },
+        });
       }
     },
     treeForAddon() {

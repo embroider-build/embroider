808807df447719e2200a0ec005b17747d4396dea
Edward Faulkner <ef@alum.mit.edu>
2021-04-22 23:56:49 -0400

    register v2 addons with ember-auto-import

--- packages/addon-shim/addon-main.js
+++ packages/addon-shim/addon-main.js
@@ -2,4 +2,11 @@
 
 module.exports = {
   name: require('./package').name,
+
+  included(...args) {
+    this._super.included.apply(this, args);
+    this.addons
+      .find((a) => a.name === 'ember-auto-import')
+      .registerV2Addon(this.parent.name, this.parent.pkg.root);
+  },
 };

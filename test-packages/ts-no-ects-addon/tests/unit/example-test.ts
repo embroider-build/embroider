139bf26cdcbcd84706663a914dc9ff397951612e
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-07-11 11:19:16 -0400

    Confirmation that tests in typescript are transpiled and ran

---
+++ test-packages/ts-no-ects-addon/tests/unit/example-test.ts
@@ -0,0 +1,10 @@
+import { module, test } from 'qunit';
+
+module('Example Module', function () {
+  test('Example Test', function (assert) {
+    assert.ok(
+      true,
+      'This test file should be transpiled by embroider, even without ember-cli-typescript'
+    );
+  });
+});

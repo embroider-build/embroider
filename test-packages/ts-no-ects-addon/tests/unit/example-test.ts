8cbae5e9a64ddafa41630edaed5367f6517ac312
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-07-11 13:23:41 -0400

    Reproduction: typescript *not* transpiled just by adding 'interface'

--- test-packages/ts-no-ects-addon/tests/unit/example-test.ts
+++ test-packages/ts-no-ects-addon/tests/unit/example-test.ts
@@ -1,9 +1,14 @@
 import { module, test } from 'qunit';
 
+interface Args {
+  example: string;
+}
+
 module('Example Module', function () {
   test('Example Test', function (assert) {
+    let value: Args = { example: 'example' };
     assert.ok(
-      true,
+      value,
       'This test file should be transpiled by embroider, even without ember-cli-typescript'
     );
   });

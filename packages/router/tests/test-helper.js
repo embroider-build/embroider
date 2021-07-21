d5508e5447a77077e918e85bc18296d5f454ca6d
Stefan Penner <stefan.penner@gmail.com>
2021-07-21 14:50:58 -0600

    [fixes #903] Upgrade ember-qunit to address ember-canary compatibility issue currently breaking CI

--- packages/router/tests/test-helper.js
+++ packages/router/tests/test-helper.js
@@ -1,7 +1,11 @@
+import * as QUnit from 'qunit'
 import Application from '../app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
+import { setup } from 'qunit-dom';
+
+setup(QUnit.assert);
 
 setApplication(Application.create(config.APP));
 

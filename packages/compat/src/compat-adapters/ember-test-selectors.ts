e31f67b338178c6504a254f198d4622cd09d9559
Josemar Luedke <josemarluedke@gmail.com>
2021-07-23 08:51:32 -0700

    Fix ember-test-selectors compat adapter for v6

--- packages/compat/src/compat-adapters/ember-test-selectors.ts
+++ packages/compat/src/compat-adapters/ember-test-selectors.ts
@@ -1,9 +1,10 @@
+import semver from 'semver';
 import V1Addon from '../v1-addon';
 import { forceIncludeModule } from '../compat-utils';
 
 export default class extends V1Addon {
   get packageMeta() {
-    if (this.addonInstance._stripTestSelectors) {
+    if (this.addonInstance._stripTestSelectors || semver.satisfies(this.packageJSON.version, '>=6.0.0')) {
       return super.packageMeta;
     } else {
       return forceIncludeModule(super.packageMeta, './utils/bind-data-test-attributes');

f6d0a8bddeb485e8c04bd2bef49e9d5bd60eed9c
Travis Hoover <thoov7@gmail.com>
2021-07-28 13:07:39 -0700

    adding test case

---
+++ tests/fixtures/preprocess-addon/index.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const Filter = require('broccoli-persistent-filter');
+const funnel = require('broccoli-funnel');
+const path = require('path');
+
+class Awk extends Filter {
+  constructor(inputNode, searchReplaceObj) {
+    super(inputNode, {});
+    this.searchReplaceObj = searchReplaceObj;
+  }
+
+  processString(content) {
+    let modifiedContent = content;
+
+    Object.entries(this.searchReplaceObj).forEach(([search, replace]) => {
+      modifiedContent = modifiedContent.replace(search, replace);
+    });
+
+    return modifiedContent;
+  }
+}
+
+module.exports = {
+  name: require('./package').name,
+
+  setupPreprocessorRegistry(type, registry) {
+    registry.add('css', {
+      name: this.name,
+      ext: 'css',
+      toTree: (tree, inputPath, outputPath) => {
+        return funnel(new Awk(tree, { '%%%': inputPath === '/app/styles' ? 'red' : 'blue' }), {
+          getDestinationPath(relativePath) {
+            let relativePathWithPrefix = `/${relativePath}`;
+
+            if (relativePathWithPrefix === `${inputPath}/app.css`) {
+              return path.join(outputPath, 'app-template.css');
+            }
+
+            return path.join(outputPath, relativePathWithPrefix.replace(inputPath, ''));
+          },
+        });
+      },
+    });
+  },
+};

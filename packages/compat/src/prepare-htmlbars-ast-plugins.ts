05ba5c911582273e8cbca01dfaa964ccda108bfa
Eugene ONeill <eoneill@linkedin.com>
2021-08-18 15:53:09 -0700

    mirror cacheKey

--- packages/compat/src/prepare-htmlbars-ast-plugins.ts
+++ packages/compat/src/prepare-htmlbars-ast-plugins.ts
@@ -2,7 +2,7 @@ import { join } from 'path';
 
 export default function prepHtmlbarsAstPluginsForUnwrap(registry: any): void {
   for (let wrapper of registry.load('htmlbars-ast-plugin')) {
-    const { plugin, parallelBabel, baseDir } = wrapper;
+    const { plugin, parallelBabel, baseDir, cacheKey } = wrapper;
     if (plugin) {
       // if the parallelBabel options were set on the wrapper, but not on the plugin, add it
       if (parallelBabel && !plugin._parallelBabel) {
@@ -17,6 +17,11 @@ export default function prepHtmlbarsAstPluginsForUnwrap(registry: any): void {
       // if the baseDir is set on the wrapper, but not on the plugin, add it
       if (baseDir && !plugin.baseDir) {
         plugin.baseDir = baseDir;
+      }
+
+      // if the cacheKey is set on the wrapper, but not on the plugin, add it
+      if (cacheKey && !plugin.cacheKey) {
+        plugin.cacheKey = cacheKey;
       }
     }
   }

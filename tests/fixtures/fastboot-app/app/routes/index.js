b7ba56b172cc4c82e8bcd553b69a801b1485c4b1
Travis Hoover <thoov7@gmail.com>
2021-09-08 11:50:10 -0700

    Cleanup fastboot-app scenario tests

---
+++ tests/fixtures/fastboot-app/app/routes/index.js
@@ -0,0 +1,14 @@
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+
+export default class IndexRoute extends Route {
+  @service
+  fastboot;
+
+  beforeModel() {
+    // This is only to to make sure we can correctly access the request's host, which fails if FastBoot's 'hostWhitelist'
+    // is not correctly set up. This is the case when the changes added to /dist/package.json by FastBoot are not correctly
+    // merged by Embroider. So this serves as a reproduction of https://github.com/embroider-build/embroider/issues/160
+    return this.fastboot.isFastBoot ? this.fastboot.request.host : null;
+  }
+}

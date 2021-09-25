c02160691e51e5bf5cb4d8c4b68bbb88d2a9cf04
Travis Hoover <thoov7@gmail.com>
2021-09-25 11:32:15 -0700

    Convert stage-1 tests to test-scenarios

---
+++ tests/fixtures/blacklisted-addon-build-options/ember-cli-build.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = function (defaults) {
+  let app = new EmberApp(defaults, {
+    // Add options here
+    addons: {
+      blacklist: ['blacklisted-in-repo-addon'],
+    },
+  });
+
+  const { Webpack } = require('@embroider/webpack');
+  return require('@embroider/compat').compatBuild(app, Webpack, {
+    skipBabel: [
+      {
+        package: 'qunit',
+      },
+    ],
+  });
+};

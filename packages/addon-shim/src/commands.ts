b6d0c4c1153594cb16eda1b1168984027226e56e
Edward Faulkner <ef@alum.mit.edu>
2021-09-30 09:43:49 +0200

    add linting option to sync-dev-deps command

--- packages/addon-shim/src/commands.ts
+++ packages/addon-shim/src/commands.ts
@@ -3,25 +3,28 @@
 import { ensureSymlinkSync, readJSONSync, writeJSONSync } from 'fs-extra';
 import { join } from 'path';
 import yargs from 'yargs/yargs';
+import type { Argv } from 'yargs';
+
+function commonArgs(yargs: Argv) {
+  return yargs
+    .option('testAppDir', {
+      type: 'string',
+      description: 'Path to the test app',
+      default: 'test-app',
+    })
+    .option('addonDir', {
+      type: 'string',
+      description: 'Path to your addon',
+      default: process.cwd(),
+    });
+}
 
 yargs(process.argv.slice(2))
   .scriptName('addon-shim')
   .command(
     'link-test-app',
     'Ensures that a test app (that lives a subdir under an addon) has access to the addon and all appropriate deps',
-    (yargs) => {
-      return yargs
-        .option('testAppDir', {
-          type: 'string',
-          description: 'Path to the test app',
-          default: 'test-app',
-        })
-        .option('addonDir', {
-          type: 'string',
-          description: 'Path to your addon',
-          default: process.cwd(),
-        });
-    },
+    (yargs) => commonArgs(yargs),
     function (opts) {
       let { testAppDir, addonDir } = opts;
       ensureSymlinkSync(
@@ -43,22 +46,18 @@ yargs(process.argv.slice(2))
     'sync-dev-deps',
     `Synchronizes a test app's devDependencies into the parent addon's devDependencies`,
     (yargs) => {
-      return yargs
-        .option('testAppDir', {
-          type: 'string',
-          description: 'Path to the test app',
-          default: 'test-app',
-        })
-        .option('addonDir', {
-          type: 'string',
-          description: 'Path to your addon',
-          default: process.cwd(),
-        });
+      return commonArgs(yargs).option('lint', {
+        type: 'boolean',
+        description:
+          'Instead of modifying package.json, print what would have been modified and exit with a failure if any changes are required.',
+        default: false,
+      });
     },
     function (opts) {
-      let { testAppDir, addonDir } = opts;
+      let { testAppDir, addonDir, lint } = opts;
       let addonPkg = readJSONSync(join(addonDir, 'package.json'));
       let testPkg = readJSONSync(join(testAppDir, 'package.json'));
+      let foundDifferences = false;
       let devDeps: { [name: string]: string } = Object.assign(
         {},
         addonPkg.devDependencies
@@ -66,12 +65,29 @@ yargs(process.argv.slice(2))
       for (let [name, range] of Object.entries(
         testPkg.devDependencies as { [name: string]: string }
       )) {
-        if (name !== addonPkg.name) {
-          devDeps[name] = range;
+        if (name === addonPkg.name) {
+          continue;
+        }
+        if (devDeps[name] !== range) {
+          foundDifferences = true;
+          if (lint) {
+            console.error(
+              `test app depends on ${name} ${range} but that is not present in addon's devDependencies package.json`
+            );
+          } else {
+            devDeps[name] = range;
+          }
         }
       }
-      addonPkg.devDependencies = devDeps;
-      writeJSONSync(join(addonDir, 'package.json'), addonPkg, { spaces: 2 });
+      if (!foundDifferences) {
+        return;
+      }
+      if (lint) {
+        process.exit(-1);
+      } else {
+        addonPkg.devDependencies = devDeps;
+        writeJSONSync(join(addonDir, 'package.json'), addonPkg, { spaces: 2 });
+      }
     }
   )
   .demandCommand()

deeedef7bc81add74308630491205add426db697
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-01-19 22:40:06 -0500

    remove accidentally invalid test

--- packages/macros/tests/babel/dependency-satisfies.test.ts
+++ packages/macros/tests/babel/dependency-satisfies.test.ts
@@ -147,19 +147,6 @@ describe(`dependencySatisfies`, function () {
         expect(runDefault(code)).toBe(true);
       });
 
-      test(`it considers a project's star-releases as allowed`, () => {
-        project.addDependency('foo', '*');
-        let code = transform(
-          `
-          import { dependencySatisfies } from '@embroider/macros';
-          export default function() {
-            return dependencySatisfies('foo', '>= 3.27.0-alpha.1 || >= 3.27.0-beta.1');
-          }
-        `
-        );
-        expect(runDefault(code)).toBe(true);
-      });
-
       test('monorepo resolutions resolve correctly', () => {
         project.addDependency('@embroider/util', '1.2.3');
         let code = transform(`

c04d4d2dd434bf1dab21d68d2e130170aac18ba4
Edward Faulkner <edward@eaf4.com>
2022-02-07 23:09:41 -0500

    test support for simulated watching

--- types/broccoli/index.d.ts
+++ types/broccoli/index.d.ts
@@ -1,8 +1,14 @@
 declare module 'broccoli' {
+  import type { SourceNodeInfo } from 'broccoli-node-api';
+
   export class Builder {
     constructor(tree: unknown);
     build(): Promise<void>;
     cleanup(): Promise<void>;
     outputPath: string;
+    readonly watchedSourceNodeWrappers: {
+      nodeInfo: SourceNodeInfo;
+      revise(): void;
+    }[];
   }
 }

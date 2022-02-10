6cb2733ab6dcfa2ca0c526bf1287ee2f413ded4d
Edward Faulkner <edward@eaf4.com>
2022-02-10 02:10:08 -0500

    update ember-bootstrap dep and workaround a missing peerdep

--- tests/scenarios/static-app-test.ts
+++ tests/scenarios/static-app-test.ts
@@ -1,13 +1,14 @@
 import { appScenarios } from './scenarios';
-import { PreparedApp } from 'scenario-tester';
+import { PreparedApp, Project } from 'scenario-tester';
 import QUnit from 'qunit';
 import merge from 'lodash/merge';
+import { dirname } from 'path';
 const { module: Qmodule, test } = QUnit;
 
 appScenarios
   .map('static-app', project => {
     project.linkDevDependency('bootstrap', { baseDir: __dirname });
-    project.linkDevDependency('ember-bootstrap', { baseDir: __dirname });
+    project.addDevDependency(emberBootstrap());
     project.linkDevDependency('@embroider/macros', { baseDir: __dirname });
     project.linkDevDependency('ember-composable-helpers', { baseDir: __dirname });
 
@@ -321,3 +322,12 @@ appScenarios
       });
     });
   });
+
+function emberBootstrap() {
+  // https://github.com/kaliber5/ember-bootstrap/pull/1750
+  let modifiers = Project.fromDir(dirname(require.resolve('@ember/render-modifiers')), { linkDeps: true });
+  modifiers.removeDependency('ember-source');
+  let eb = Project.fromDir(dirname(require.resolve('ember-bootstrap')), { linkDeps: true });
+  eb.addDependency(modifiers);
+  return eb;
+}

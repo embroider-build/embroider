5671ce5e30e815ef1b41b2503c2ba153129ba0a6
Matthew Edwards <medwards@linkedin.com>
2022-04-29 13:14:02 -0700

    Add babel plugin for preprocessing templates with ast transforms

---
+++ packages/addon-dev/src/template-transform-plugin.ts
@@ -0,0 +1,81 @@
+import type * as Babel from '@babel/core';
+import type { types as t } from '@babel/core';
+import type { NodePath } from '@babel/traverse';
+import { preprocess, print } from '@glimmer/syntax';
+
+export interface Options {
+  astTransforms?: Array<string | Function>;
+}
+
+interface State {
+  opts: Options;
+  localName: string | undefined;
+}
+
+export default function main(babel: typeof Babel) {
+  let t = babel.types;
+
+  return {
+    visitor: {
+      ImportDeclaration(path: NodePath<t.ImportDeclaration>, state: State) {
+        if (path.node.source.value !== 'ember-cli-htmlbars') {
+          return;
+        }
+
+        const specifier = path.node.specifiers.find(
+          (s) =>
+            t.isImportSpecifier(s) &&
+            t.isIdentifier(s.imported) &&
+            s.imported.name === 'hbs'
+        );
+
+        if (!specifier) {
+          return;
+        }
+
+        state.localName = specifier.local.name;
+      },
+      CallExpression(path: NodePath<t.CallExpression>, state: State) {
+        const localName = state.localName;
+
+        if (!t.isIdentifier(path.node.callee)) {
+          return;
+        }
+
+        const callee = path.node.callee;
+
+        if (!localName || callee.name !== localName) {
+          return;
+        }
+
+        let template = (path.node.arguments[0] as t.StringLiteral).value;
+
+        let options = {
+          astTransforms: [],
+          ...state.opts,
+        };
+
+        const astTransforms = options.astTransforms.map((maybeFunc) => {
+          return typeof maybeFunc === 'string'
+            ? require(maybeFunc) // If it's a string attempt to resolve the path to a module.
+            : maybeFunc;
+        });
+
+        if (astTransforms.length < 1) {
+          return;
+        }
+
+        const ast = preprocess(template, {
+          plugins: {
+            ast: [...astTransforms],
+          },
+        });
+
+        const augmentedTemplate = print(ast);
+
+        // Create a new stringLiteral with the augmentedTemplate
+        path.node.arguments[0] = t.stringLiteral(augmentedTemplate);
+      },
+    },
+  };
+}
---
+++ packages/addon-dev/tests/template-transform-plugin.test.ts
@@ -0,0 +1,139 @@
+import { allBabelVersions } from '@embroider/test-support';
+import { Options } from '../src/template-transform-plugin';
+import { hbsToJS } from '@embroider/shared-internals';
+import { AST } from '@glimmer/syntax';
+import { join } from 'path';
+import tmp from 'tmp';
+import { writeFileSync } from 'fs-extra';
+
+describe('template-transform-plugin', () => {
+  jest.setTimeout(120000);
+
+  const templateTransformBabelPlugin = join(
+    __dirname,
+    '../src/template-transform-plugin.js'
+  );
+
+  let plugins: any = [];
+
+  function reverseTransform() {
+    return {
+      name: 'reverse-transform',
+      visitor: {
+        ElementNode(node: AST.ElementNode) {
+          node.tag = node.tag.split('').reverse().join('');
+        },
+      },
+    };
+  }
+
+  function setupPlugins(options?: Options) {
+    plugins = [[templateTransformBabelPlugin, options]];
+  }
+
+  allBabelVersions({
+    babelConfig() {
+      return {
+        plugins,
+      };
+    },
+    createTests(transform) {
+      afterEach(function () {
+        plugins = undefined;
+        // options = undefined;
+      });
+
+      test('no-op', () => {
+        setupPlugins();
+        const code = hbsToJS('Hello {{@phrase}}');
+        let output = transform(code);
+        expect(output).toMatch(
+          /import { hbs } from ['"]ember-cli-htmlbars['"];/
+        );
+        expect(output).toMatch(
+          /export default hbs\(['"]Hello {{@phrase}}['"]\);/
+        );
+      });
+
+      test('options.astTransforms empty array', () => {
+        setupPlugins({
+          astTransforms: [],
+        });
+        const code = hbsToJS('Hello {{@phrase}}');
+        let output = transform(code);
+        expect(output).toMatch(
+          /import { hbs } from ['"]ember-cli-htmlbars['"];/
+        );
+        expect(output).toMatch(
+          /export default hbs\(['"]Hello {{@phrase}}['"]\);/
+        );
+      });
+      test('options.astTransforms function', () => {
+        setupPlugins({
+          astTransforms: [reverseTransform],
+        });
+
+        const code = hbsToJS('<span>{{@phrase}}</span>');
+        let output = transform(code);
+        expect(output).toMatch(
+          /import { hbs } from ['"]ember-cli-htmlbars['"];/
+        );
+        expect(output).toMatch(
+          /export default hbs\(['"]\<naps\>{{@phrase}}\<\/naps\>['"]\);/
+        );
+      });
+
+      test('options.astTransforms path', () => {
+        const someFile = tmp.fileSync();
+
+        const contents = `module.exports = function reverseTransform() {
+  return {
+    name: 'reverse-transform',
+    visitor: {
+      ElementNode(node) {
+        node.tag = node.tag.split('').reverse().join('');
+      },
+    },
+  };
+}`;
+
+        writeFileSync(someFile.name, contents, 'utf8');
+
+        setupPlugins({
+          astTransforms: [someFile.name],
+        });
+
+        const code = hbsToJS('<span>{{@phrase}}</span>');
+
+        let output = transform(code);
+
+        expect(output).toMatch(
+          /import { hbs } from ['"]ember-cli-htmlbars['"];/
+        );
+        expect(output).toMatch(
+          /export default hbs\(['"]\<naps\>{{@phrase}}\<\/naps\>['"]\);/
+        );
+
+        someFile.removeCallback();
+      });
+
+      test('ember-cli-htmlbars alias import name', () => {
+        setupPlugins({
+          astTransforms: [reverseTransform],
+        });
+
+        const code = `import { hbs as render } from 'ember-cli-htmlbars';
+export default render('<span>{{@phrase}}</span>');`;
+
+        let output = transform(code);
+
+        expect(output).toMatch(
+          /import { hbs as render } from ['"]ember-cli-htmlbars['"];/
+        );
+        expect(output).toMatch(
+          /export default render\(['"]\<naps\>{{@phrase}}\<\/naps\>['"]\);/
+        );
+      });
+    },
+  });
+});

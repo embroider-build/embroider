08b58a33017f534a5ff74f426c15f657ede51b1c
Matthew Edwards <medwards@linkedin.com>
2022-06-01 10:43:42 -0700

    Add documentation

---
+++ packages/addon-dev/sample-babel.config.js
@@ -0,0 +1,21 @@
+// Some addons need to transform their templates before they have a portable format.
+// In "classic" builds this was done by the application.In embroider it should be
+// done during the addon build.
+const someAstTransformPlugin = require('./some-ast-transform-plugin');
+
+module.exports = {
+  plugins: [
+    '@embroider/addon-dev/template-colocation-plugin',
+    [
+      '@embroider/addon-dev/template-transform-plugin',
+      {
+        astTransforms: [
+          someAstTransformPlugin,
+          './path/to/another-template-transform-plugin',
+        ],
+      },
+    ],
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    '@babel/plugin-proposal-class-properties',
+  ],
+};

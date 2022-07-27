e28d90176c83fd8e89c5dfcb4b7bfbd871843530
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-07-26 21:12:02 -0400

    Resolve ts extensions by default Add ts-app-template

--- packages/compat/src/compat-app.ts
+++ packages/compat/src/compat-app.ts
@@ -233,23 +233,11 @@ class CompatAppAdapter implements AppAdapter<TreeNames> {
     // webpack's default is ['.wasm', '.mjs', '.js', '.json']. Keeping that
     // subset in that order is sensible, since many third-party libraries will
     // expect it to work that way.
-    let extensions = ['.wasm', '.mjs', '.js', '.json', '.hbs'];
-
-    // for now, this is hard-coded. If we see ember-cli-typescript, ts files are
-    // resolvable. Once we implement a preprocessor-registration build hook,
-    // this logic can be pushed down first into `@embroider/compat` (which can
-    // generate the appropriate hooks when it upcompiles ember-cli-typescript),
-    // and then later into ember-cli-typescript itself (which can ship a v2
-    // version with the hook).
     //
-    // Typescript is a slightly weird example of a preprocessor because it gets
-    // implemented in babel, so all we realy need to do is make the extension
-    // resolvable and there's no other "loader" or anything to apply.
-    if (this.activeAddonChildren().find(pkg => pkg.name === 'ember-cli-typescript')) {
-      extensions.unshift('.ts');
-    }
-
-    return extensions;
+    // For TS, we defer to ember-cli-babel, and the setting for
+    // "enableTypescriptTransform" can be set with and without
+    // ember-cli-typescript
+    return ['.ts', '.wasm', '.mjs', '.js', '.json', '.hbs'];
   }
 
   private *emberEntrypoints(htmlTreePath: string): IterableIterator<Asset> {

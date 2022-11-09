98b67325ba3768040f9e647f3798da703281f77b
Edward Faulkner <edward@eaf4.com>
2022-11-09 08:46:54 -0500

    reording for ease of reading

--- packages/compat/tests/resolver.test.ts
+++ packages/compat/tests/resolver.test.ts
@@ -14,65 +14,6 @@ import type { AST, ASTPluginEnvironment } from '@glimmer/syntax';
 const compilerPath = emberTemplateCompilerPath();
 const compilerChecksum = `mock-compiler-checksum${Math.random()}`;
 
-function emberHolyFuturisticNamespacingBatmanTransform(env: ASTPluginEnvironment) {
-  let sigil = '$';
-  let b = env.syntax.builders;
-
-  function rewriteOrWrapComponentParam(node: AST.MustacheStatement | AST.SubExpression | AST.BlockStatement) {
-    if (!node.params.length) {
-      return;
-    }
-
-    let firstParam = node.params[0];
-    if (firstParam.type !== 'StringLiteral') {
-      // note: does not support dynamic / runtime strings
-      return;
-    }
-
-    node.params[0] = b.string(firstParam.original.replace(sigil, '@'));
-  }
-
-  return {
-    name: 'ember-holy-futuristic-template-namespacing-batman:namespacing-transform',
-
-    visitor: {
-      PathExpression(node: AST.PathExpression) {
-        if (node.parts.length > 1 || !node.original.includes(sigil)) {
-          return;
-        }
-
-        return b.path(node.original.replace(sigil, '@'), node.loc);
-      },
-      ElementNode(node: AST.ElementNode) {
-        if (node.tag.indexOf(sigil) > -1) {
-          node.tag = node.tag.replace(sigil, '@');
-        }
-      },
-      MustacheStatement(node: AST.MustacheStatement) {
-        if (node.path.type === 'PathExpression' && node.path.original === 'component') {
-          // we don't care about non-component expressions
-          return;
-        }
-        rewriteOrWrapComponentParam(node);
-      },
-      SubExpression(node: AST.SubExpression) {
-        if (node.path.type === 'PathExpression' && node.path.original !== 'component') {
-          // we don't care about non-component expressions
-          return;
-        }
-        rewriteOrWrapComponentParam(node);
-      },
-      BlockStatement(node: AST.BlockStatement) {
-        if (node.path.type === 'PathExpression' && node.path.original !== 'component') {
-          // we don't care about blocks not using component
-          return;
-        }
-        rewriteOrWrapComponentParam(node);
-      },
-    },
-  };
-}
-
 describe('compat-resolver', function () {
   let appDir: string;
 
@@ -1925,3 +1866,62 @@ describe('compat-resolver', function () {
     );
   });
 });
+
+function emberHolyFuturisticNamespacingBatmanTransform(env: ASTPluginEnvironment) {
+  let sigil = '$';
+  let b = env.syntax.builders;
+
+  function rewriteOrWrapComponentParam(node: AST.MustacheStatement | AST.SubExpression | AST.BlockStatement) {
+    if (!node.params.length) {
+      return;
+    }
+
+    let firstParam = node.params[0];
+    if (firstParam.type !== 'StringLiteral') {
+      // note: does not support dynamic / runtime strings
+      return;
+    }
+
+    node.params[0] = b.string(firstParam.original.replace(sigil, '@'));
+  }
+
+  return {
+    name: 'ember-holy-futuristic-template-namespacing-batman:namespacing-transform',
+
+    visitor: {
+      PathExpression(node: AST.PathExpression) {
+        if (node.parts.length > 1 || !node.original.includes(sigil)) {
+          return;
+        }
+
+        return b.path(node.original.replace(sigil, '@'), node.loc);
+      },
+      ElementNode(node: AST.ElementNode) {
+        if (node.tag.indexOf(sigil) > -1) {
+          node.tag = node.tag.replace(sigil, '@');
+        }
+      },
+      MustacheStatement(node: AST.MustacheStatement) {
+        if (node.path.type === 'PathExpression' && node.path.original === 'component') {
+          // we don't care about non-component expressions
+          return;
+        }
+        rewriteOrWrapComponentParam(node);
+      },
+      SubExpression(node: AST.SubExpression) {
+        if (node.path.type === 'PathExpression' && node.path.original !== 'component') {
+          // we don't care about non-component expressions
+          return;
+        }
+        rewriteOrWrapComponentParam(node);
+      },
+      BlockStatement(node: AST.BlockStatement) {
+        if (node.path.type === 'PathExpression' && node.path.original !== 'component') {
+          // we don't care about blocks not using component
+          return;
+        }
+        rewriteOrWrapComponentParam(node);
+      },
+    },
+  };
+}

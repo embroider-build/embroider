bafee9cfc7f149d41bb73717dfe164f799c4fec9
Chris Krycho <hello@chriskrycho.com>
2022-12-02 11:26:23 -0700

    Export `EnsureSafeComponentHelper` type definition

--- packages/util/index.d.ts
+++ packages/util/index.d.ts
@@ -1,6 +1,17 @@
 import { ComponentLike } from '@glint/template';
+import Helper from '@ember/component/helper';
 
 export function ensureSafeComponent<C extends string | ComponentLike<S>, S>(
   component: C,
   thingWithOwner: unknown
 ): C extends string ? ComponentLike<unknown> : C;
+
+export class EnsureSafeComponentHelper<
+  C extends string | ComponentLike<S>,
+  S
+> extends Helper<{
+  Args: {
+    Positional: [component: C];
+  };
+  Return: C extends string ? ComponentLike<unknown> : C;
+}> {}

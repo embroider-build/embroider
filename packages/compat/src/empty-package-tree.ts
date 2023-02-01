3915334c6061cbd2aa55ba74a58434b24835c89f
Chris Manson <chris@manson.ie>
2023-02-01 16:13:17 +0000

    add a comment to the empty package key generator

--- packages/compat/src/empty-package-tree.ts
+++ packages/compat/src/empty-package-tree.ts
@@ -15,6 +15,7 @@ export default class extends Plugin {
   build() {
     if (!this.built) {
       writeJSONSync(join(this.outputPath, 'package.json'), {
+        '//': 'This empty package was created by embroider. If you are having issues resolving this package and you have followed your dependency tree to this file you could be experiencing an issue with your dependencies. See https://github.com/embroider-build/embroider/blob/main/docs/empty-package-output.md for more information or open an issue in the embroider repo with a recreation of your problem.',
         name: this.name,
       });
     }

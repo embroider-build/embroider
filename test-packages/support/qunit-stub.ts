de529d8bb0f1205bc00b3a114bcd8b5f469ca8b3
Edward Faulkner <edward@eaf4.com>
2023-02-23 16:59:34 -0500

    fixing more spots where scenario-tester hits missing global qunit

--- test-packages/support/qunit-stub.ts
+++ test-packages/support/qunit-stub.ts
@@ -1,7 +1,10 @@
 // our tests import some libraries that extend the global QUnit assertions. When
-// our tests are loaded by QUnit, that works normally. But our suite-setup-util
-// also tries to load all our tests just to list which tests are in there, and
-// it can cause them to blow up on not finding the global qunit.
+// our tests are loaded by QUnit, that works normally.
+//
+// But scenario-tester also has commands that load all our tests just to
+// identify what scenarios are in them, and those won't have the qunit globals.
+//
+// Ultimately, the fault here lies with QUnit for using a globals-based API.
 (globalThis as any).QUnit = {
   assert: {},
 };

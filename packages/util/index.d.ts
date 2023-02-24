112bada939d920d7a8c78ed2dffbf36c34306623
Simon Ihmig <simon.ihmig@gmail.com>
2023-02-24 13:47:59 +0100

    Fix types to not narrow down to `ComponentLike<unknown>`

--- packages/util/index.d.ts
+++ packages/util/index.d.ts
@@ -1,14 +1,13 @@
 import { ComponentLike } from '@glint/template';
 import Helper from '@ember/component/helper';
 
-export function ensureSafeComponent<C extends string | ComponentLike<S>, S>(
+export function ensureSafeComponent<C extends string | ComponentLike<any>>(
   component: C,
   thingWithOwner: unknown
 ): C extends string ? ComponentLike<unknown> : C;
 
 export class EnsureSafeComponentHelper<
-  C extends string | ComponentLike<S>,
-  S
+  C extends string | ComponentLike<any>
 > extends Helper<{
   Args: {
     Positional: [component: C];

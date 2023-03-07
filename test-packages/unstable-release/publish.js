73213f2aafe8d0df1dc484c1b6ca4941652656ab
Edward Faulkner <edward@eaf4.com>
2023-03-07 09:25:55 -0500

    fix publishing

--- test-packages/unstable-release/publish.js
+++ test-packages/unstable-release/publish.js
@@ -1,12 +1,13 @@
 import { execaCommand } from 'execa';
 import { listPublicWorkspaces } from './workspaces.js';
+import { dirname } from 'path';
 
 async function publish() {
   let publicWorkspaces = await listPublicWorkspaces();
 
   for (let workspace of publicWorkspaces) {
     console.info(`Publishing ${workspace}`);
-    await execaCommand('npm publish --tag=unstable --verbose');
+    await execaCommand('npm publish --tag=unstable --verbose', { cwd: dirname(workspace) });
   }
 }
 

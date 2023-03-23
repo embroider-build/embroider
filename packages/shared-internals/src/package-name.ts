ba4c1cb391a778f82b5000ec7977d3d06870106b
Edward Faulkner <edward@eaf4.com>
2023-03-23 16:50:55 -0400

    don't confuse webpack requests with packageNames

--- packages/shared-internals/src/package-name.ts
+++ packages/shared-internals/src/package-name.ts
@@ -1,8 +1,16 @@
 import { isAbsolute } from 'path';
 
 export default function absolutePackageName(specifier: string): string | undefined {
-  if (specifier[0] === '.' || isAbsolute(specifier)) {
-    // Not an absolute specifier
+  if (
+    // relative paths:
+    specifier[0] === '.' ||
+    // webpack-specific microsyntax for internal requests:
+    specifier[0] === '!' ||
+    specifier[0] === '-' ||
+    // absolute paths:
+    isAbsolute(specifier)
+  ) {
+    // Does not refer to a package
     return;
   }
   let parts = specifier.split('/');

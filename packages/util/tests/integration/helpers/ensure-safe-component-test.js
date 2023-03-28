c8886de0fbdd65c936d08c739176b7b8e3e1afcf
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-03-28 15:06:53 -0400

    Remove ensureSafeComponent -- it should not be used in strict mode

--- packages/util/tests/integration/helpers/ensure-safe-component-test.js
+++ packages/util/tests/integration/helpers/ensure-safe-component-test.js
@@ -10,7 +10,6 @@ import { ensureSafeComponent } from '@embroider/util';
 import * as SomeComponentModule from 'dummy/components/some-component';
 import ColocatedExample from 'dummy/components/colocated-example';
 import { setOwner } from '@ember/application';
-import { precompileTemplate } from '@ember/template-compilation';
 
 const SomeComponent = SomeComponentModule.default;
 
@@ -175,35 +174,5 @@ module('Integration | Helper | ensure-safe-component', function (hooks) {
     await settled();
     assert.strictEqual(this.element.textContent.trim(), 'second:goodbye');
     assert.deepEqual(log, ['target instantiated', 'new target instantiated']);
-  });
-
-  module('with strict: true', function () {
-    test('reference to class property-as-component is allowed', async function (assert) {
-      const Thing = setComponentTemplate(hbs`thing`, templateOnlyComponent());
-
-      this.inner = ensureSafeComponent(
-        setComponentTemplate(
-          precompileTemplate(
-            `{{#let (component (ensureSafeComponent this.thing)) as |Thing|}}
-                <Thing />
-             {{/let}}`,
-            {
-              strictMode: true,
-              scope: () => ({
-                ensureSafeComponent,
-              }),
-            }
-          ),
-          class extends Component {
-            thing = Thing;
-          }
-        ),
-        this
-      );
-      await render(hbs`
-        <this.inner @name={{component "some-component"}} />
-      `);
-      assert.strictEqual(this.element.textContent.trim(), 'thing');
-    });
   });
 });

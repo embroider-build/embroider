1aa035a6d46f205ab88bdce832b8356eac6a2f1c
void_malex <void-mAlex@users.noreply.github.com>
2023-04-01 20:50:46 +0100

    only rehome a request if it would resolve in a different package this stops infinite loops from v2 addons that misdeclare exports fields reports correct misconfigured export from within stage3 bundler

--- packages/core/src/module-resolver.ts
+++ packages/core/src/module-resolver.ts
@@ -710,7 +710,12 @@ export class Resolver {
       // this is the easy case -- a package that uses exports can safely resolve
       // its own name, so it's enough to let it resolve the (self-targeting)
       // specifier from its own package root.
-      return request.rehome(resolve(pkg.root, 'package.json'));
+      const resolvedPkgJson = resolve(pkg.root, 'package.json');
+      if (request.fromFile === resolvedPkgJson) {
+        return request;
+      } else {
+        return request.rehome(resolvedPkgJson);
+      }
     } else {
       // otherwise we need to just assume that internal naming is simple
       return request.alias(request.specifier.replace(pkg.name, '.')).rehome(resolve(pkg.root, 'package.json'));

f8942698f203c85aa0bbde061e46e9f0f4dfe8d0
Edward Faulkner <edward@eaf4.com>
2023-05-10 11:42:05 -0400

    ember-cli-update ts-app-template v4.4.0...v4.12.1

--- tests/ts-app-template/ember-cli-build.js
+++ tests/ts-app-template/ember-cli-build.js
@@ -4,7 +4,7 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 const { maybeEmbroider } = require('@embroider/test-setup');
 
 module.exports = function (defaults) {
-  let app = new EmberApp(defaults, {
+  const app = new EmberApp(defaults, {
     // Add options here
     'ember-cli-babel': {
       enableTypeScriptTransform: true,

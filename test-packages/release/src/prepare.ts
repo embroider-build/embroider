c296d0bc853455c43393c65eb701021711429548
Edward Faulkner <edward@eaf4.com>
2023-05-11 14:27:19 -0400

    generating and parsing lerna-changelog results

--- test-packages/release/src/prepare.ts
+++ test-packages/release/src/prepare.ts
@@ -1,6 +1,24 @@
-import { execa } from 'execa';
+import execa from 'execa';
+import fs from 'node:fs';
+import { parseChangeLog } from './change-parser';
 
-async function main() {}
+async function buildChangelog(): Promise<string> {
+  let result = await execa('pnpm', ['lerna-changelog', '--next-version', 'Embroider Monorepo Release']);
+  return result.stdout;
+}
+
+async function main() {
+  let changelog = fs.readFileSync('/tmp/changelog', 'utf8');
+  let parsed;
+  try {
+    parsed = parseChangeLog(changelog);
+  } catch (err) {
+    console.error(err);
+    console.error(`the full changelog that failed to parse was:\n${changelog}`);
+    process.exit(-1);
+  }
+  console.log(JSON.stringify(parsed, null, 2));
+}
 
 main().then(
   () => {

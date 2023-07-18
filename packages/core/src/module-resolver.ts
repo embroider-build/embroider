9a86b97e1a78689454615a66f887d9b648db4bd9
Edward Faulkner <edward@eaf4.com>
2023-07-18 13:15:24 -0700

    better error message

--- packages/core/src/module-resolver.ts
+++ packages/core/src/module-resolver.ts
@@ -951,7 +951,7 @@ export class Resolver {
       }
       if (!entry) {
         throw new Error(
-          `A module tried to resolve "${request.specifier}" and didn't find it.
+          `A module tried to resolve "${request.specifier}" and didn't find it (${label}).
 
  - Maybe a dependency declaration is missing? 
  - Remember that v1 addons can only import non-Ember-addon NPM dependencies if they include ember-auto-import in their dependencies.

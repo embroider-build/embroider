624b0b3083bf08725a902930c01200be1f728a64
Markus Sanin <markus.sanin@zeppelin-group.com>
2023-07-28 08:17:54 +0200

    Add semverRange <=4.11.0 for ember-data debug

--- packages/compat/src/compat-adapters/@ember-data/debug.ts
+++ packages/compat/src/compat-adapters/@ember-data/debug.ts
@@ -1,5 +1,6 @@
 import { AddonMeta } from '@embroider/core';
 import V1Addon from '../../v1-addon';
+import semver from 'semver';
 
 export default class EmberDataDebug extends V1Addon {
   get packageMeta(): Partial<AddonMeta> {
@@ -10,5 +11,9 @@ export default class EmberDataDebug extends V1Addon {
     meta.externals = [...(meta.externals ?? []), '@ember-data/store'];
 
     return meta;
+  }
+
+  static shouldApplyAdapter(addonInstance: any) {
+    return semver.lt(addonInstance.pkg.version, '4.11.1');
   }
 }

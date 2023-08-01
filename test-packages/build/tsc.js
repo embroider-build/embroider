ce048a41bc87374fd6abaa85980b8572162996ac
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-08-01 17:42:35 -0400

    Progress

--- test-packages/build/tsc.js
+++ test-packages/build/tsc.js
@@ -2,26 +2,25 @@ import { execaCommand } from 'execa';
 import path from 'node:path';
 import url from 'node:url';
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
-const repoRoot = path.join(__dirname, '../../../');
+const repoRoot = path.join(__dirname, '../../');
 
 export async function buildDeclarations() {
   let callerDir = process.cwd();
 
   await execaCommand(
-    `
+    `pnpm tsc
     --declaration
-    --declarationDir
     --emitDeclarationOnly
-    --rootDir ${callerDir}/src/index.ts
-    --declarationDir ${callerDir}/dist/types
+    --files "${callerDir}/src/**/*"
+    --declarationDir "${callerDir}/dist/types"
     --skipLibCheck
     --experimentalDecorators
     --esModuleInterop
     --target esnext
     --moduleResolution nodenext
     --typeRoots ./node_modules/@types/,./types
-  `,
-    { cwd: repoRoot }
+  `.replaceAll(/\n/g, ''),
+    { cwd: repoRoot, stdio: 'inherit' }
   );
 }
 

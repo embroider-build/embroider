fdf7d4c83cdc01857992b4e70efd4fe29bd5802e
Godfrey Chan <godfreykfc@gmail.com>
2023-10-08 12:31:15 -0700

    Emit a pseudo import for the co-located template
    As discussed in #1619, tools like babel caches the ouput based on
    the source content of the input files. For component javascript
    files, whether there is a co-located template file is an extra bit
    of information that doesn't show up in the source file, but that
    information does get used in producing the output. This causes the
    caches to not invalidate when a co-located tempalte file is added
    or deleted.
    
    This fixes the problem by ensuring we include that information in
    the input source file. For now, it is just an inert comment, but
    we can actually adjust our babel plugin to rely on this information
    rather than doing its own filesystem probing again, which should
    have some performance benefit.

--- packages/compat/src/synthesize-template-only-components.ts
+++ packages/compat/src/synthesize-template-only-components.ts
@@ -1,16 +1,27 @@
 import Plugin from 'broccoli-plugin';
 import type { Node } from 'broccoli-node-api';
 import { join, basename } from 'path';
-import walkSync from 'walk-sync';
-import { removeSync, outputFileSync, pathExistsSync } from 'fs-extra';
+import walkSync, { type Entry } from 'walk-sync';
+import { removeSync, outputFileSync, pathExistsSync, readFileSync } from 'fs-extra';
 
 const source = `import templateOnlyComponent from '@ember/component/template-only';
 export default templateOnlyComponent();`;
 
 const jsExtensions = ['.js', '.ts', '.mjs', '.mts'];
 
+type Emitted = { type: 'template-only-component' } | { type: 'template-import'; mtime: number };
+
+type TemplateOnly = { template: Entry; javascript: undefined };
+type JavaScriptOnly = { template: undefined; javascript: Entry };
+type Colocated = { template: Entry; javascript: Entry };
+type ComponentFiles = TemplateOnly | JavaScriptOnly | Colocated;
+
+function importTemplate(files: { template: Entry }): string {
+  return `/* import __COLOCATED_TEMPLATE__ from './${basename(files.template.relativePath)}'; */\n`;
+}
+
 export default class SynthesizeTemplateOnlyComponents extends Plugin {
-  private emitted = new Set() as Set<string>;
+  private emitted = new Map() as Map<string, Emitted>;
   private allowedPaths: string[];
   private templateExtensions: string[];
 
@@ -25,16 +36,18 @@ export default class SynthesizeTemplateOnlyComponents extends Plugin {
   }
 
   async build() {
-    let unneeded = new Set(this.emitted);
+    let unneeded = new Set(this.emitted.keys());
     for (let dir of this.allowedPaths) {
-      let { needed, seen } = this.crawl(join(this.inputPaths[0], dir));
-      for (let file of needed) {
-        let fullName = join(this.outputPath, dir, file);
+      let entries = this.crawl(join(this.inputPaths[0], dir));
+      for (let [name, files] of entries) {
+        let fullName = join(this.outputPath, dir, name);
         unneeded.delete(fullName);
-        if (seen.has(file)) {
-          this.remove(fullName);
+        if (files.javascript && files.template) {
+          this.addTemplateImport(fullName, files);
+        } else if (files.template) {
+          this.addTemplateOnlyComponent(fullName, files);
         } else {
-          this.add(fullName);
+          this.remove(fullName);
         }
       }
     }
@@ -42,15 +55,31 @@ export default class SynthesizeTemplateOnlyComponents extends Plugin {
       this.remove(fullName);
     }
   }
-  private add(filename: string) {
-    if (!this.emitted.has(filename)) {
+
+  private addTemplateOnlyComponent(filename: string, files: TemplateOnly) {
+    if (this.emitted.get(filename)?.type !== 'template-only-component') {
       // special case: ember-cli doesn't allow template-only components named
       // "template.hbs" because there are too many people doing a "pods-like"
       // layout that happens to match that pattern.🤮
       if (basename(filename) !== 'template') {
-        outputFileSync(filename + '.js', source, 'utf8');
+        outputFileSync(filename + '.js', importTemplate(files) + source, 'utf8');
       }
-      this.emitted.add(filename);
+
+      this.emitted.set(filename, { type: 'template-only-component' });
+    }
+  }
+
+  private addTemplateImport(filename: string, files: Colocated) {
+    const emitted = this.emitted.get(filename);
+    const mtime = files.javascript.mtime;
+
+    if (!(emitted?.type === 'template-import' && emitted.mtime === mtime)) {
+      const inputSource = readFileSync(files.javascript.fullPath, { encoding: 'utf8' });
+
+      // If we are ok with appending instead, copy + append maybe more efficient?
+      outputFileSync(filename + '.js', importTemplate(files) + inputSource, 'utf8');
+
+      this.emitted.set(filename, { type: 'template-import', mtime });
     }
   }
 
@@ -61,23 +90,27 @@ export default class SynthesizeTemplateOnlyComponents extends Plugin {
     }
   }
 
-  private crawl(dir: string) {
-    const needed = new Set<string>();
-    const seen = new Set<string>();
+  private crawl(dir: string): Map<string, ComponentFiles> {
+    const entries = new Map<string, ComponentFiles>();
+
     if (pathExistsSync(dir)) {
-      for (let file of walkSync(dir, { directories: false })) {
-        for (const templateExtension of this.templateExtensions) {
-          if (file.endsWith(templateExtension)) {
-            needed.add(file.slice(0, -1 * templateExtension.length));
-          } else {
-            const jsExtension = jsExtensions.find(ext => file.endsWith(ext));
-            if (jsExtension) {
-              seen.add(file.slice(0, -1 * jsExtension.length));
-            }
-          }
+      for (let entry of walkSync.entries(dir, { directories: false })) {
+        const templateExtension = this.templateExtensions.find(ext => entry.relativePath.endsWith(ext));
+        if (templateExtension) {
+          const key = entry.relativePath.slice(0, -1 * templateExtension.length);
+          entries.set(key, { template: entry, javascript: entries.get(key)?.javascript });
+          continue;
+        }
+
+        const jsExtension = jsExtensions.find(ext => entry.relativePath.endsWith(ext));
+        if (jsExtension) {
+          const key = entry.relativePath.slice(0, -1 * jsExtension.length);
+          entries.set(key, { template: entries.get(key)?.template, javascript: entry });
+          continue;
         }
       }
     }
-    return { needed, seen };
+
+    return entries;
   }
 }

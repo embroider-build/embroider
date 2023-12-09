7f160ac522e64b0c51f994a31d003cb012e31cf6
patrickpircher <pa.trickjlp@gmail.com>
2023-12-10 00:26:35 +0100

    re-add addons, otherwise I get duplicates for glimmer-vm, which does not like it

---
+++ packages/vite/src/addons.ts
@@ -0,0 +1,20 @@
+import { ResolverLoader, packageName } from '@embroider/core';
+
+export function addons(root: string): string[] {
+  let rloader = new ResolverLoader(root);
+  let { options } = rloader.resolver;
+  let names = new Set<string>();
+  for (let from of Object.keys(options.renameModules)) {
+    let pName = packageName(from);
+    if (pName) {
+      names.add(pName);
+    }
+  }
+  for (let from of Object.keys(options.renamePackages)) {
+    names.add(from);
+  }
+  for (let name of options.engines.map(e => e.activeAddons).flat().map(a => a.name)) {
+    names.add(name);
+  }
+  return [...names];
+}

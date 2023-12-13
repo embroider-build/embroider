fb2191ae7ef971d2ec5be751cc2d2bddb6dc0930
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-12-13 14:16:41 -0500

    Make separate scenario
    Test file must end with -test

--- tests/scenarios/v2-addon-dev-typescript-test.ts
+++ tests/scenarios/v2-addon-dev-typescript-test.ts
@@ -12,11 +12,10 @@ appScenarios
   .map('v2-addon-dev-typescript', async project => {
     let addon = baseV2Addon();
     addon.pkg.name = 'v2-addon';
-    addon.pkg.type = 'module';
     addon.pkg.files = ['dist'];
     addon.pkg.exports = {
       './*': './dist/*.js',
-      './addon-main.cjs': './addon-main.cjs',
+      './addon-main.js': './addon-main.js',
       './package.json': './package.json',
     };
     addon.pkg.scripts = {

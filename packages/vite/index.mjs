90e4682c753674db1331768ed4405ebd71bde919
Chris Manson <mansona@users.noreply.github.com>
2023-12-14 17:04:49 +0000

    remove missing import from vite index

--- packages/vite/index.mjs
+++ packages/vite/index.mjs
@@ -3,5 +3,4 @@ export * from './src/esbuild-resolver.js';
 export * from './src/hbs.js';
 export * from './src/scripts.js';
 export * from './src/template-tag.js';
-export * from './src/addons.js';
 export * from './src/optimize-deps.js';

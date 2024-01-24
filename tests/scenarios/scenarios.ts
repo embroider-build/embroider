6ee43fd7f3b7b6243f6533a5dca231171230e914
Patrick Pircher <patricklx@users.noreply.github.com>
2024-01-24 16:00:37 +0100

    remove canary

--- tests/scenarios/scenarios.ts
+++ tests/scenarios/scenarios.ts
@@ -21,14 +21,6 @@ async function release(project: Project) {
   project.linkDevDependency('ember-qunit', { baseDir: __dirname, resolveName: 'ember-qunit-7' });
 }
 
-async function canary(project: Project) {
-  project.linkDevDependency('ember-source', { baseDir: __dirname, resolveName: 'ember-source-canary' });
-  project.linkDevDependency('ember-cli', { baseDir: __dirname, resolveName: 'ember-cli-beta' });
-  project.linkDevDependency('ember-data', { baseDir: __dirname, resolveName: 'ember-data-latest' });
-  project.linkDevDependency('@ember/test-helpers', { baseDir: __dirname, resolveName: '@ember/test-helpers-3' });
-  project.linkDevDependency('ember-qunit', { baseDir: __dirname, resolveName: 'ember-qunit-7' });
-}
-
 export function supportMatrix(scenarios: Scenarios) {
   return scenarios.expand({
     lts_3_28,

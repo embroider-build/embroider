91deb61d4326aba41071b6e56cfe38ff36724fd4
Chris Manson <chris@manson.ie>
2024-02-06 15:15:19 +0000

    use Vite for all tests

---
+++ tests/app-template/babel.config.cjs
@@ -0,0 +1,4 @@
+// eslint-disable-next-line n/no-missing-require
+let config = require("./node_modules/.embroider/rewritten-app/_babel_config_");
+
+module.exports = config;
---
+++ tests/ts-app-template/babel.config.cjs
@@ -0,0 +1,6 @@
+// eslint-disable-next-line n/no-missing-require
+let config = require('./node_modules/.embroider/rewritten-app/_babel_config_');
+
+debugger;
+
+module.exports = config;

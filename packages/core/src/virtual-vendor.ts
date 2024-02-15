09f1566221600c0e7e4581e3feafd09adbb2fe73
Marine Dunstetter <bluecut.official@gmail.com>
2024-02-15 12:28:38 +0100

    feat(virtual vendor): in virtual-vendor, remove appFiles usage as we only use the Engine part of it

--- packages/core/src/virtual-vendor.ts
+++ packages/core/src/virtual-vendor.ts
@@ -1,11 +1,9 @@
-import { type Package, extensionsPattern } from '@embroider/shared-internals';
+import { type Package } from '@embroider/shared-internals';
 import type { V2AddonPackage } from '@embroider/shared-internals/src/package';
 import { readFileSync } from 'fs';
 import { sortBy } from 'lodash';
-import { join } from 'path';
 import resolve from 'resolve';
-import walkSync from 'walk-sync';
-import { AppFiles } from './app-files';
+import type { Engine } from './app-files';
 import type { Resolver } from './module-resolver';
 import type { VirtualContentResult } from './virtual-content';
 
@@ -22,26 +20,19 @@ export function renderVendor(filename: string, resolver: Resolver): VirtualConte
 }
 
 function getVendor(owner: Package, resolver: Resolver): string {
-  let engine = resolver.owningEngine(owner);
-  let hasFastboot = Boolean(resolver.options.engines[0]!.activeAddons.find(a => a.name === 'ember-cli-fastboot'));
-  let appFiles = new AppFiles(
-    {
-      package: owner,
-      addons: new Map(
-        engine.activeAddons.map(addon => [
-          resolver.packageCache.get(addon.root) as V2AddonPackage,
-          addon.canResolveFromFile,
-        ])
-      ),
-      isApp: true,
-      modulePrefix: resolver.options.modulePrefix,
-      appRelativePath: 'NOT_USED_DELETE_ME',
-    },
-    getAppFiles(owner.root),
-    hasFastboot ? getFastbootFiles(owner.root) : new Set(),
-    extensionsPattern(resolver.options.resolvableExtensions),
-    resolver.options.podModulePrefix
-  );
+  let engineConfig = resolver.owningEngine(owner);
+  let engine: Engine = {
+    package: owner,
+    addons: new Map(
+      engineConfig.activeAddons.map(addon => [
+        resolver.packageCache.get(addon.root) as V2AddonPackage,
+        addon.canResolveFromFile,
+      ])
+    ),
+    isApp: true,
+    modulePrefix: resolver.options.modulePrefix,
+    appRelativePath: 'NOT_USED_DELETE_ME',
+  };
 
   // TODO - From where do we get this dynamically? in compat-app-builder:
   // let emberENV = this.configTree.readConfig().EmberENV;
@@ -54,23 +45,10 @@ function getVendor(owner: Package, resolver: Resolver): string {
     _TEMPLATE_ONLY_GLIMMER_COMPONENTS: true,
   };
 
-  return generateVendor(appFiles, emberENV);
+  return generateVendor(engine, emberENV);
 }
 
-function getAppFiles(appRoot: string): Set<string> {
-  const files: string[] = walkSync(appRoot, {
-    ignore: ['_babel_config_.js', '_babel_filter_.js', 'app.js', 'assets', 'testem.js', 'node_modules'],
-  });
-  return new Set(files);
-}
-
-function getFastbootFiles(appRoot: string): Set<string> {
-  const appDirPath = join(appRoot, '_fastboot_');
-  const files: string[] = walkSync(appDirPath);
-  return new Set(files);
-}
-
-function generateVendor(engine: AppFiles, emberENV?: unknown): string {
+function generateVendor(engine: Engine, emberENV?: unknown): string {
   // Add addons implicit-scripts
   let vendor: string[] = impliedAddonVendors(engine).map((sourcePath: string): string => {
     let source = readFileSync(sourcePath);
@@ -86,7 +64,7 @@ function generateVendor(engine: AppFiles, emberENV?: unknown): string {
   return vendor.join('') as string;
 }
 
-function impliedAddonVendors({ engine }: AppFiles): string[] {
+function impliedAddonVendors(engine: Engine): string[] {
   let result: Array<string> = [];
   for (let addon of sortBy(Array.from(engine.addons.keys()), pkg => {
     switch (pkg.name) {

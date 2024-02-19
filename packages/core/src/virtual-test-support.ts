7025345c5ab6aa32c982c2945ae2b9b82e389122
Marine Dunstetter <bluecut.official@gmail.com>
2024-02-19 18:53:58 +0100

    feat(virtual test-support.js): implement the generation of the virtual content

--- packages/core/src/virtual-test-support.ts
+++ packages/core/src/virtual-test-support.ts
@@ -1,4 +1,9 @@
-import { type Package } from '@embroider/shared-internals';
+import type { Package } from '@embroider/shared-internals';
+import type { V2AddonPackage } from '@embroider/shared-internals/src/package';
+import { readFileSync } from 'fs';
+import { sortBy } from 'lodash';
+import resolve from 'resolve';
+import type { Engine } from './app-files';
 import type { Resolver } from './module-resolver';
 import type { VirtualContentResult } from './virtual-content';
 
@@ -15,11 +20,69 @@ export function renderImplicitTestScripts(filename: string, resolver: Resolver):
 }
 
 function getTestSupport(owner: Package, resolver: Resolver): string {
-  console.log(owner);
-  console.log(resolver);
-  return `
+  let engineConfig = resolver.owningEngine(owner);
+  let engine: Engine = {
+    package: owner,
+    addons: new Map(
+      engineConfig.activeAddons.map(addon => [
+        resolver.packageCache.get(addon.root) as V2AddonPackage,
+        addon.canResolveFromFile,
+      ])
+    ),
+    isApp: true,
+    modulePrefix: resolver.options.modulePrefix,
+    appRelativePath: 'NOT_USED_DELETE_ME',
+  };
+
+  return generateTestSupport(engine);
+}
+
+function generateTestSupport(engine: Engine): string {
+  // Add classic addons test-support
+  let result: string[] = impliedAddonTestSupport(engine);
+  let hasEmbroiderMacrosTestSupport = result.find(sourcePath =>
+    sourcePath.endsWith('embroider-macros-test-support.js')
+  );
+  result = result.map((sourcePath: string): string => {
+    let source = readFileSync(sourcePath);
+    return `${source}`;
+  });
+
+  // Add _testing_suffix_.js
+  result.push(`
 var runningTests=true;
 if (typeof Testem !== 'undefined' && (typeof QUnit !== 'undefined' || typeof Mocha !== 'undefined')) {
   Testem.hookIntoTestFramework();
-}`;
+}`);
+
+  // whether or not anybody was actually using @embroider/macros explicitly
+  // as an addon, we ensure its test-support file is always present.
+  if (!hasEmbroiderMacrosTestSupport) {
+    result.unshift(`${readFileSync(require.resolve('@embroider/macros/src/vendor/embroider-macros-test-support'))}`);
+  }
+
+  return result.join('') as string;
+}
+
+function impliedAddonTestSupport(engine: Engine): string[] {
+  let result: Array<string> = [];
+  for (let addon of sortBy(Array.from(engine.addons.keys()), pkg => {
+    switch (pkg.name) {
+      case 'loader.js':
+        return 0;
+      case 'ember-source':
+        return 10;
+      default:
+        return 1000;
+    }
+  })) {
+    let implicitScripts = addon.meta['implicit-test-scripts'];
+    if (implicitScripts) {
+      let options = { basedir: addon.root };
+      for (let mod of implicitScripts) {
+        result.push(resolve.sync(mod, options));
+      }
+    }
+  }
+  return result;
 }

57db617f424c1d9fb575f610589da88e01253802
Marine Dunstetter <bluecut.official@gmail.com>
2024-02-26 16:49:19 +0100

    POC: install demo-content-for in the vite-app to have a classic addon with content-for replacements

--- tests/vite-app/ember-cli-build.js
+++ tests/vite-app/ember-cli-build.js
@@ -1,12 +1,26 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
-const { prebuild } = require('@embroider/compat');
+// const { prebuild } = require('@embroider/compat');
+const { compatBuild } = require('@embroider/compat');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     // Add options here
   });
 
-  return prebuild(app);
+  return compatBuild(app, undefined, {
+    staticAddonTrees: true,
+    staticAddonTestSupportTrees: true,
+    staticComponents: true,
+    staticHelpers: true,
+    staticModifiers: true,
+    staticEmberSource: true,
+    availableContentForTypes: ['fox'],
+    amdCompatibility: {
+      es: [],
+    },
+  });
+
+  // return prebuild(app);
 };

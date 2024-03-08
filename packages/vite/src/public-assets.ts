8de15f18b649b07a324f771790a13ea3dc8685c7
Marine Dunstetter <bluecut.official@gmail.com>
2024-03-08 17:37:48 +0100

    feat(addons public-assets): modify the middleware to handle any public-assets coming from addons

--- packages/vite/src/public-assets.ts
+++ packages/vite/src/public-assets.ts
@@ -5,30 +5,33 @@ import send from 'send';
 import type { Plugin } from 'vite';
 
 export function publicAssets(): Plugin {
-  let resolverLoader = new ResolverLoader(process.cwd());
+  const resolverLoader = new ResolverLoader(process.cwd());
+  const excludedUrls = ['/@fs', '/@id/embroider_virtual:', '/@vite/client'];
   return {
     name: 'embroider-public-assets',
 
     configureServer(server) {
       server.middlewares.use((req, res, next) => {
-        // TODO: How to handle only public-assets?
-        if (req.originalUrl === '/ember-welcome-page/images/construction.png') {
-          let appActiveAddons = resolverLoader.resolver.options.engines[0].activeAddons;
-          let ownerAddon = appActiveAddons.find(({ name }) => name === 'ember-welcome-page');
-          if (ownerAddon) {
-            let ownerAddonConfig = readFileSync(`${ownerAddon.root}/package.json`);
-            let ownerAddonAssets = JSON.parse(ownerAddonConfig.toString())['ember-addon']['public-assets'];
-            let pathInAddon = Object.keys(ownerAddonAssets).find(
-              leftPath => ownerAddonAssets[leftPath] === req.originalUrl
-            );
-            if (pathInAddon) {
-              console.log(join(ownerAddon.root, pathInAddon));
-              // @ts-ignore (TODO: how to handle the types properly?)
-              send(req, join(ownerAddon.root, pathInAddon)).pipe(res);
-            }
-          }
-        }
-        next();
+        // TODO: Is there another way to handle the minimum amount of requests (without forcing the user to list the addons)?
+        if (!req.originalUrl || req.originalUrl === '/' || excludedUrls.find(url => req.originalUrl.includes(url)))
+          return next();
+
+        // TODO: Is it the right thing to look at?
+        let appActiveAddons = resolverLoader.resolver.options.engines[0].activeAddons;
+        let maybeAddonName = req.originalUrl.split('/')[1];
+        let ownerAddon = appActiveAddons.find(({ name }) => name === maybeAddonName);
+        if (!ownerAddon) return next();
+
+        let ownerAddonConfig = readFileSync(`${ownerAddon.root}/package.json`);
+        let ownerAddonAssets = JSON.parse(ownerAddonConfig.toString())['ember-addon']['public-assets'];
+        let pathInAddon = Object.keys(ownerAddonAssets).find(leftPath =>
+          // public-assets can contain "./my-url" for "/my-url" request, so we can't use === operator
+          ownerAddonAssets[leftPath].includes(req.originalUrl)
+        );
+        if (!pathInAddon) return next();
+
+        // @ts-ignore TODO: how to handle the types properly?
+        send(req, join(ownerAddon.root, pathInAddon)).pipe(res);
       });
     },
   };

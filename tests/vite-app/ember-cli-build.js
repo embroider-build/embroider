5ab779524727167232b36ce8e418e911502d497d
Chris Manson <chris@manson.ie>
2024-03-12 13:21:11 +0000

    make maybeEmbroider work with vite

--- tests/vite-app/ember-cli-build.js
+++ tests/vite-app/ember-cli-build.js
@@ -3,10 +3,12 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 const { prebuild } = require('@embroider/compat');
 
+const { maybeEmbroider } = require('@embroider/test-setup');
+
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     // Add options here
   });
 
-  return prebuild(app);
+  return maybeEmbroider(app);
 };

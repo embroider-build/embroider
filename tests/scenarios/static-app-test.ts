faf1ffed60c55569d4d47ca74de923e3698202f0
Edward Faulkner <edward@eaf4.com>
2024-03-28 14:36:49 -0400

    force build tests in static-app

--- tests/scenarios/static-app-test.ts
+++ tests/scenarios/static-app-test.ts
@@ -306,6 +306,7 @@ appScenarios
 
         module.exports = function (defaults) {
           let app = new EmberApp(defaults, {
+            tests: process.env.FORCE_BUILD_TESTS ? true : undefined,
             'ember-bootstrap': {
               bootstrapVersion: 4,
               importBootstrapCSS: true

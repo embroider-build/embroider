8319cc7f5c49323dc8adc5211faf4e85178b46f4
Marine Dunstetter <bluecut.official@gmail.com>
2024-04-04 11:36:41 +0200

    Make the addon-template use vite

--- tests/fixtures/macro-sample-addon/ember-cli-build.js
+++ tests/fixtures/macro-sample-addon/ember-cli-build.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+const { maybeEmbroider } = require('@embroider/test-setup');
 
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
@@ -14,9 +15,11 @@ module.exports = function(defaults) {
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
 
-  if (process.env.CLASSIC) {
-    return app.toTree();
-  }
-  const Webpack = require('@embroider/webpack').Webpack;
-  return require('@embroider/compat').compatBuild(app, Webpack);
+  return maybeEmbroider(app, {
+    skipBabel: [
+      {
+        package: 'qunit',
+      },
+    ],
+  });
 };

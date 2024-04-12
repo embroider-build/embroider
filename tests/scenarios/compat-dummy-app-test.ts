963900176509cdb90846c0ac27caec0d7623e1d4
Marine Dunstetter <bluecut.official@gmail.com>
2024-04-12 18:05:16 +0200

    test(compat-dummy-app): add testing a public asset provided by the addon to make sure the publicDir config doesn't interfeer with addons

--- tests/scenarios/compat-dummy-app-test.ts
+++ tests/scenarios/compat-dummy-app-test.ts
@@ -21,6 +21,9 @@ dummyAppScenarios
           'example.hbs': `hello`,
         },
       },
+      public: {
+        'from-addon.txt': 'a public asset provided by the classic addon',
+      },
       tests: {
         dummy: {
           public: {
@@ -73,25 +76,31 @@ dummyAppScenarios
         app = await scenario.prepare();
       });
 
-      test('contains public assets from dummy app after a build', async function (assert) {
+      test('contains public assets from both addon and dummy app after a build', async function (assert) {
         await app.execute(`pnpm vite build`);
         expectFile = expectRewrittenFilesAt(resolve(app.dir, 'tests/dummy'), {
           qunit: assert,
         });
+        expectFile('../../node_modules/.embroider/rewritten-app/addon-template/from-addon.txt').exists();
         expectFile('../../node_modules/.embroider/rewritten-app/robots.txt').exists();
-        expectFile('../../node_modules/.embroider/rewritten-app/package.json')
+        let assets = expectFile('../../node_modules/.embroider/rewritten-app/package.json')
           .json()
-          .get('ember-addon.assets')
-          .includes('robots.txt');
+          .get('ember-addon.assets');
+        assets.includes('robots.txt');
+        assets.includes('./addon-template/from-addon.txt');
       });
 
-      test('contains public assets from dummy app in dev mode', async function (assert) {
+      test('contains public assets from both addon and dummy app in dev mode', async function (assert) {
         const server = CommandWatcher.launch('vite', ['--clearScreen', 'false'], { cwd: app.dir });
         try {
           const [, url] = await server.waitFor(/Local:\s+(https?:\/\/.*)\//g);
           let response = await fetch(`${url}/robots.txt`);
           let text = await response.text();
           assert.strictEqual(text, 'go away bots');
+
+          response = await fetch(`${url}/addon-template/from-addon.txt`);
+          text = await response.text();
+          assert.strictEqual(text, 'a public asset provided by the classic addon');
         } finally {
           await server.shutdown();
         }

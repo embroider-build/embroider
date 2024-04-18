cbe94c8e6bcdff6c3d4683e7b2a9bc5850e9ef8a
Edward Faulkner <edward@eaf4.com>
2024-04-18 10:02:30 -0400

    Merge pull request #1885 from embroider-build/portable-babel-launcher
    Fix pre support in portable babel launcher

--- tests/scenarios/stage1-test.ts
+++ tests/scenarios/stage1-test.ts
@@ -144,7 +144,7 @@ appScenarios
         );
 
         fileContents.matches(
-          /hbs\(["']<div class={{embroider-sample-transforms-result}}>Extra<\/div>["']\)/,
+          /precompileTemplate\(["']<div class={{embroider-sample-transforms-result}}>Extra<\/div>["']\)/,
           'called template is still hbs and custom transforms have run'
         );
 
@@ -225,7 +225,7 @@ appScenarios
         );
 
         file.matches(
-          /hbs\(["']<div class={{embroider-sample-transforms-result}}>Extra<\/div>["']\)/,
+          /precompileTemplate\(["']<div class={{embroider-sample-transforms-result}}>Extra<\/div>["']\)/,
           'called template is still hbs and custom transforms have run'
         );
 

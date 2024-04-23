8a8af2bda28d0655d07f77fa80b102ad77e74d04
Marine Dunstetter <bluecut.official@gmail.com>
2024-04-23 14:26:08 +0200

    test(vendor.js): remove the override of ember-cli-build

--- tests/scenarios/compat-addon-classic-features-test.ts
+++ tests/scenarios/compat-addon-classic-features-test.ts
@@ -1,4 +1,3 @@
-import { merge } from 'lodash';
 import QUnit from 'qunit';
 import type { PreparedApp } from 'scenario-tester';
 import fetch from 'node-fetch';
@@ -9,33 +8,7 @@ import CommandWatcher from './helpers/command-watcher';
 const { module: Qmodule, test } = QUnit;
 
 appScenarios
-  .map('compat-addon-classic-features-virtual-scripts', project => {
-    merge(project.files, {
-      'ember-cli-build.js': `
-        'use strict';
-
-        const EmberApp = require('ember-cli/lib/broccoli/ember-app');
-        const { maybeEmbroider } = require('@embroider/test-setup');
-
-        module.exports = function (defaults) {
-          let app = new EmberApp(defaults, {
-            ...(process.env.FORCE_BUILD_TESTS ? {
-              tests: true,
-            } : undefined),
-          });
-
-          return maybeEmbroider(app, {
-            availableContentForTypes: ['custom'],
-            skipBabel: [
-              {
-                package: 'qunit',
-              },
-            ],
-          });
-        };
-      `,
-    });
-  })
+  .map('compat-addon-classic-features-virtual-scripts', () => {})
   .forEachScenario(scenario => {
     Qmodule(`${scenario.name}`, function (hooks) {
       let app: PreparedApp;

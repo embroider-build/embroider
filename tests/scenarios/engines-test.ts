85deb6794375a15f51b722418edb2a1a91176b47
Chris Manson <chris@manson.ie>
2024-05-14 16:34:30 +0100

    skip fastboot and engines tests for now

--- tests/scenarios/engines-test.ts
+++ tests/scenarios/engines-test.ts
@@ -84,6 +84,7 @@ engineScenarios
   .skip('lts_3_28-engines') // this skip should be removed before https://github.com/embroider-build/embroider/pull/1435 is merged
   .skip('lts_4_4-engines') // this skip should be removed before https://github.com/embroider-build/embroider/pull/1435 is merged
   .skip('release-engines') // this skip should be removed before https://github.com/embroider-build/embroider/pull/1435 is merged
+  .skip('lts_5_8-engines') // this shouldn't be run
   .skip('canary-engines') // this shouldn't be run
   .map('without-fastboot', () => {})
   .forEachScenario(scenario => {
@@ -141,6 +142,7 @@ engineScenarios
   .skip('lts_3_28-engines') // fails due to https://github.com/emberjs/ember.js/pull/20461
   .skip('lts_4_4-engines') // fails due to https://github.com/emberjs/ember.js/pull/20461
   .skip('release-engines') // fails due to https://github.com/emberjs/ember.js/pull/20461
+  .skip('lts_5_8-engines')
   .skip('canary-engines') // this shouldn't be run
   .map('with-fastboot', app => {
     app.linkDependency('ember-cli-fastboot', { baseDir: __dirname });

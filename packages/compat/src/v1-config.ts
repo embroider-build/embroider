2589bb6d4f4bdfd7889ad6d845bbd5988b9cbefa
Marine Dunstetter <bluecut.official@gmail.com>
2024-05-24 14:38:51 +0200

    feat(new config module): export a new config module in the app/folder

--- packages/compat/src/v1-config.ts
+++ packages/compat/src/v1-config.ts
@@ -1,7 +1,7 @@
 import Plugin from 'broccoli-plugin';
 import type { Node } from 'broccoli-node-api';
 import { join } from 'path';
-import { readFileSync, outputFileSync } from 'fs-extra';
+import { readFileSync } from 'fs-extra';
 
 export interface ConfigContents {
   modulePrefix: string;
@@ -25,51 +25,4 @@ export class V1Config extends Plugin {
     }
     return this.lastConfig;
   }
-}
-
-export class WriteV1Config extends Plugin {
-  private lastContents: string | undefined;
-  constructor(private inputTree: V1Config, private storeConfigInMeta: boolean, private testInputTree?: V1Config) {
-    super([inputTree, testInputTree as V1Config].filter(Boolean), {
-      persistentOutput: true,
-      needsCache: false,
-    });
-  }
-  build() {
-    let filename = join(this.outputPath, 'config/environment.js');
-    let contents;
-    if (this.storeConfigInMeta) {
-      contents = metaLoader();
-    } else {
-      if (this.testInputTree) {
-        contents = `
-        import { isTesting } from '@embroider/macros';
-        let env;
-        if (isTesting()) {
-          env = ${JSON.stringify(this.testInputTree.readConfig())};
-        } else {
-          env = ${JSON.stringify(this.inputTree.readConfig())};
-        }
-        export default env;
-        `;
-      } else {
-        contents = `export default ${JSON.stringify(this.inputTree.readConfig())};`;
-      }
-    }
-    if (!this.lastContents || this.lastContents !== contents) {
-      outputFileSync(filename, contents);
-    }
-    this.lastContents = contents;
-  }
-}
-
-function metaLoader() {
-  // Supporting config content as JS Module.
-  // Wrapping the content with immediate invoked function as
-  // replaced content for config-module was meant to support AMD module.
-  return `
-    export default (function() {
-      {{content-for 'config-module'}}
-    })().default;
-  `;
 }

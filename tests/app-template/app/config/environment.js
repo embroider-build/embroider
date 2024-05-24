7f821c90fb5c316c3a9e54f7e02b9015e7213b97
Marine Dunstetter <bluecut.official@gmail.com>
2024-05-24 14:40:23 +0200

    test(new config module): add the new config module in the test templates, and create a test that consumes it

---
+++ tests/app-template/app/config/environment.js
@@ -0,0 +1,33 @@
+import { macroCondition, isTesting } from '@embroider/macros';
+
+const ENV = {
+  modulePrefix: 'app-template',
+  rootURL: '/',
+  locationType: 'history',
+  EmberENV: {
+    EXTEND_PROTOTYPES: false,
+    FEATURES: {
+      // Here you can enable experimental features on an ember canary build
+      // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
+    },
+  },
+
+  APP: {
+    // Here you can pass flags/options to your application instance
+    // when it is created
+  },
+};
+
+if (macroCondition(isTesting())) {
+  // Testem prefers this...
+  ENV.locationType = 'none';
+
+  // keep test console output quieter
+  ENV.APP.LOG_ACTIVE_GENERATION = false;
+  ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+  ENV.APP.rootElement = '#ember-testing';
+  ENV.APP.autoboot = false;
+}
+
+export default ENV;
--- tests/scenarios/app-config-environment-test.ts
+++ tests/scenarios/app-config-environment-test.ts
@@ -1,5 +1,7 @@
 import merge from 'lodash/merge';
+import CommandWatcher from './helpers/command-watcher';
 import { appScenarios } from './scenarios';
+import fetch from 'node-fetch';
 import type { PreparedApp } from 'scenario-tester';
 import QUnit from 'qunit';
 
@@ -17,50 +19,126 @@ appScenarios
 
         module.exports = function (defaults) {
           let app = new EmberApp(defaults, {
-            tests: true,
-            storeConfigInMeta: false,
+            ...(process.env.FORCE_BUILD_TESTS ? {
+              tests: true,
+            } : undefined),
           });
           return maybeEmbroider(app, {});
         };
       `,
       config: {
-        'environment.js': `module.exports = function(environment) {
-          // DEFAULT config/environment.js
-          let ENV = {
+        'environment.js': `
+          module.exports = function(environment) {
+            // DEFAULT config/environment.js
+            let ENV = {
+              modulePrefix: 'app-template',
+              environment,
+              rootURL: '/',
+              locationType: 'history',
+              EmberENV: {
+                EXTEND_PROTOTYPES: false,
+                FEATURES: {},
+              },
+              APP: {},
+              'legacy-config-format': 'production',
+            };
+
+            if (environment === 'development') {
+              ENV['legacy-config-format'] = 'development';
+            };
+
+            if (environment === 'test') {
+              ENV.locationType = 'none';
+              ENV.APP.LOG_ACTIVE_GENERATION = false;
+              ENV.APP.LOG_VIEW_LOOKUPS = false;
+              ENV.APP.rootElement = '#ember-testing';
+              ENV.APP.autoboot = false;
+
+              ENV['legacy-config-format'] = 'test';
+            };
+
+            return ENV;
+          };
+        `,
+      },
+      app: {
+        config: {
+          'environment.js': `
+          import { macroCondition, isDevelopingApp, isTesting } from '@embroider/macros';
+
+          const ENV = {
             modulePrefix: 'app-template',
-            environment,
             rootURL: '/',
             locationType: 'history',
             EmberENV: {
               EXTEND_PROTOTYPES: false,
-              FEATURES: {},
+              FEATURES: {
+                // Here you can enable experimental features on an ember canary build
+                // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
+              },
             },
-            APP: {},
+
+            APP: {
+              // Here you can pass flags/options to your application instance
+              // when it is created
+            },
+
+            // CUSTOM
+            'new-config-format': 'production',
           };
 
-          if (environment === 'test') {
+          if (macroCondition(isDevelopingApp())) {
+            // CUSTOM
+            ENV['new-config-format'] = 'development';
+          }
+
+          if (macroCondition(isTesting())) {
+            // Testem prefers this...
             ENV.locationType = 'none';
+
+            // keep test console output quieter
             ENV.APP.LOG_ACTIVE_GENERATION = false;
             ENV.APP.LOG_VIEW_LOOKUPS = false;
+
             ENV.APP.rootElement = '#ember-testing';
             ENV.APP.autoboot = false;
 
             // CUSTOM
-            ENV.someCustomField = true;
-          };
+            ENV['new-config-format'] = 'test';
+          }
 
-          return ENV;
-        };`,
+          export default ENV;`,
+        },
+        controllers: {
+          'application.js': `
+          import Controller from '@ember/controller';
+          import config from '../config/environment';
+
+          export default class ApplicationController extends Controller {
+            config = config;
+
+            get configValue() {
+              console.log(this.config);
+              return this.config['new-config-format'];
+            }
+          }
+          `,
+        },
+        templates: {
+          'application.hbs': `
+            <p>new-config-format: {{this.configValue}}</p>
+          `,
+        },
       },
       tests: {
         unit: {
-          'store-config-in-meta-test.js': `
+          'config-test.js': `
             import { module, test } from 'qunit';
             import ENV from 'app-template/config/environment';
 
-            module('Unit | storeConfigInMeta set to false', function (hooks) {
+            module('Unit | it loads the config', function (hooks) {
               test('it has loaded the correct config values', async function (assert) {
-                assert.equal(ENV.someCustomField, true);
+                assert.equal(ENV['new-config-format'], 'test');
               });
             });`,
         },
@@ -74,7 +152,7 @@ appScenarios
         app = await scenario.prepare();
       });
 
-      test(`ember test ran against dev build with custom unit test`, async function (assert) {
+      test(`environement config v2 is imported and correct: test mode`, async function (assert) {
         // here we build the app with environment set to dev so that we can use
         // the build output directory as the input path to an `ember test` run
         // later. This difference in environment is important because it's the
@@ -84,6 +162,18 @@ appScenarios
         assert.equal(devBuildResult.exitCode, 0, devBuildResult.output);
         let testRunResult = await app.execute(`pnpm test:ember --path dist`);
         assert.equal(testRunResult.exitCode, 0, testRunResult.output);
+      });
+
+      test('environement config v2 is imported and correct: dev mode', async function (assert) {
+        const server = CommandWatcher.launch('vite', ['--clearScreen', 'false'], { cwd: app.dir });
+        try {
+          const [, url] = await server.waitFor(/Local:\s+(https?:\/\/.*)\//g);
+          let response = await fetch(`${url}/`);
+          let text = await response.text();
+          assert.true(text.includes('development'));
+        } finally {
+          await server.shutdown();
+        }
       });
     });
   });

55ac7745cdca4c56a4531f45b6785c5953713a6d
Marine Dunstetter <bluecut.official@gmail.com>
2024-05-30 13:10:02 +0200

    test(new config module): write the new config format for the different test templates

--- tests/addon-template/tests/dummy/app/config/environment.js
+++ tests/addon-template/tests/dummy/app/config/environment.js
@@ -1,19 +1,4 @@
-// from https://github.com/ember-cli/ember-cli/blob/master/lib/broccoli/app-config-from-meta.js
-export default (function() {
-  let config = function() {
-    let prefix = 'dummy';
-    let metaName = prefix + '/config/environment';
-    try {
-      let rawConfig = document.querySelector('meta[name="' + metaName + '"]').getAttribute('content');
-      let config = JSON.parse(decodeURIComponent(rawConfig));
-      let exports = { 'default': config };
-      Object.defineProperty(exports, '__esModule', { value: true });
-      return exports;
-		}
-		catch(err) {
-      throw new Error('Could not read config from meta tag with name "' + metaName + '".');
-    }
-  }()
-  config.default.APP.fromConfigModule = 'hello new world';
-  return config;
-})().default;
+import loadConfigFromMeta from '@embroider/config-meta-loader';
+
+export default loadConfigFromMeta('dummy');
+// config.APP.fromConfigModule = 'hello new world';

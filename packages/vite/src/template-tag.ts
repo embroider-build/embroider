3e8f9562a96cb8a59c346e33b7e7515475a1ae18
patrickpircher <pa.trickjlp@gmail.com>
2024-06-17 16:12:11 +0200

    fix

--- packages/vite/src/template-tag.ts
+++ packages/vite/src/template-tag.ts
@@ -7,41 +7,9 @@ const gjsFilter = createFilter('**/*.{gjs,gts}?(\\?)*');
 export function templateTag({ inline_source_map } = { inline_source_map: false }): Plugin {
   let preprocessor = new Preprocessor();
 
-  function candidates(id: string) {
-    return [id + '.gjs', id + '.gts', id + '/index.gjs', id + '/index.gts'];
-  }
-
   return {
     name: 'embroider-template-tag',
     enforce: 'pre',
-
-    async resolveId(id: string, importer: string | undefined, options) {
-      if (options.custom?.embroider?.isExtensionSearch) {
-        return null;
-      }
-      let resolution = await this.resolve(id, importer, {
-        skipSelf: true,
-      });
-      if (resolution) {
-        return resolution;
-      }
-      for (let candidate of candidates(id)) {
-        resolution = await this.resolve(candidate, importer, {
-          skipSelf: true,
-          custom: {
-            embroider: {
-              isExtensionSearch: true,
-              enableCustomResolver: false,
-            },
-          },
-        });
-        if (resolution) {
-          return {
-            id: resolution.id,
-          };
-        }
-      }
-    },
 
     transform(code: string, id: string) {
       if (!gjsFilter(id)) {

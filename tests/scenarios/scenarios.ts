2489f4b11b256cfdba9e3434901f1c0070f38b18
Chris Manson <chris@manson.ie>
2024-07-04 11:01:18 +0100

    pin ember-data to avoid audit issues

--- tests/scenarios/scenarios.ts
+++ tests/scenarios/scenarios.ts
@@ -54,7 +54,7 @@ async function release(project: Project) {
 async function beta(project: Project) {
   project.linkDevDependency('ember-source', { baseDir: __dirname, resolveName: 'ember-source-beta' });
   project.linkDevDependency('ember-cli', { baseDir: __dirname, resolveName: 'ember-cli-beta' });
-  project.linkDevDependency('ember-data', { baseDir: __dirname, resolveName: 'ember-data-beta' });
+  project.linkDevDependency('ember-data', { baseDir: __dirname, resolveName: 'ember-data-latest' });
   project.linkDevDependency('@ember/test-helpers', { baseDir: __dirname, resolveName: '@ember/test-helpers-3' });
   project.linkDevDependency('ember-qunit', { baseDir: __dirname, resolveName: 'ember-qunit-7' });
   project.linkDevDependency('ember-cli-babel', { baseDir: __dirname, resolveName: 'ember-cli-babel-latest' });

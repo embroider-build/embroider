2c06ddce8605c08608dd44ba0daf228f5f3ed049
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-09-07 11:15:14 -0400

    Simulate the relationship ember-source has with glimmer-vm

--- tests/scenarios/library-exports-conditions-test.ts
+++ tests/scenarios/library-exports-conditions-test.ts
@@ -1,5 +1,5 @@
 import { join } from 'path';
-import { appScenarios } from './scenarios';
+import { appScenarios, baseAddon } from './scenarios';
 import { type PreparedApp, Project } from 'scenario-tester';
 import QUnit from 'qunit';
 import merge from 'lodash/merge';
@@ -11,6 +11,10 @@ const { module: Qmodule, test } = QUnit;
 const nativeESM = {
   prodText: `production - from native-esm`,
   devText: `dev - from native-esm`,
+};
+const nestedNativeESM = {
+  prodText: `production - from nested-native-esm`,
+  devText: `dev - from nested-native-esm`,
 };
 
 appScenarios
@@ -35,8 +39,35 @@ appScenarios
         default: './dist/prod/index.js',
       },
     };
+    let nestedAddon = new Project('nested-native-esm', '0.0.0', {
+      files: {
+        dist: {
+          dev: { 'index.js': `export const location = '${nestedNativeESM.devText}';` },
+          prod: { 'index.js': `export const location = '${nestedNativeESM.prodText}';` },
+        },
+      },
+    });
+    nestedAddon.pkg.files = ['dist'];
+    nestedAddon.pkg.exports = {
+      '.': {
+        development: {
+          default: './dist/dev/index.js',
+        },
+        default: './dist/prod/index.js',
+      },
+    };
 
+    let proxyV1Addon = baseAddon();
+    proxyV1Addon.pkg.name = 'proxy-v1-addon';
+    proxyV1Addon.mergeFiles({
+      addon: {
+        'index.js': `export { location } from 'nested-native-esm';`,
+      },
+    });
+
+    proxyV1Addon.addDependency(nestedAddon);
     project.addDevDependency(addon);
+    project.addDevDependency(proxyV1Addon);
 
     merge(project.files, {
       app: {
@@ -44,8 +75,10 @@ appScenarios
           'application.js': `
 import Route from '@ember/routing/route';
 import * as nativeESM from 'native-esm';
+import * as proxyV1Addon from 'proxy-v1-addon';
 
-console.log({ nativeESM });
+console.log({ nativeESM, proxyV1Addon });
+
 export default class Application extends Route {};
 `,
         },
@@ -55,31 +88,54 @@ export default class Application extends Route {};
   .forEachScenario(scenario => {
     Qmodule(scenario.name, function (hooks) {
       let app: PreparedApp;
+      let filePaths: string[];
 
       hooks.before(async assert => {
         app = await scenario.prepare();
         let result = await app.execute('pnpm build');
         assert.equal(result.exitCode, 0, result.output);
+
+        let files = await globby('**/*.js', { cwd: join(app.dir, 'dist') });
+
+        filePaths = files.map(file => join(app.dir, 'dist', file));
       });
 
-      Qmodule('Consuming app', function () {
-        test(`dist assets contain the prod strings, not the development`, async function (assert) {
-          let files = await globby('**/*.js', { cwd: join(app.dir, 'dist') });
+      Qmodule('dist assets contain the prod strings, not the development', function () {
+        test(`native-esm`, async function (assert) {
           let found = false;
 
-          for (let file of files) {
-            let fullPath = join(app.dir, 'dist', file);
-            let buffer = await readFile(fullPath);
+          for (let file of filePaths) {
+            let buffer = await readFile(file);
             let contents = buffer.toString();
 
             let hasProdText = contents.includes(nativeESM.prodText);
 
-            assert.notOk(contents.includes(nativeESM.devText), `${fullPath} does not contain '${nativeESM.devText}'`);
+            assert.notOk(contents.includes(nativeESM.devText), `${file} does not contain '${nativeESM.devText}'`);
 
             if (hasProdText) found = true;
           }
 
           assert.ok(found, `Found text '${nativeESM.prodText}' within ${app.dir}/dist`);
+        });
+
+        test(`nested-native-esm`, async function (assert) {
+          let found = false;
+
+          for (let file of filePaths) {
+            let buffer = await readFile(file);
+            let contents = buffer.toString();
+
+            let hasProdText = contents.includes(nestedNativeESM.prodText);
+
+            assert.notOk(
+              contents.includes(nestedNativeESM.devText),
+              `${file} does not contain '${nestedNativeESM.devText}'`
+            );
+
+            if (hasProdText) found = true;
+          }
+
+          assert.ok(found, `Found text '${nestedNativeESM.prodText}' within ${app.dir}/dist`);
         });
       });
     });

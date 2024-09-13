6f505317ef598df0ff92c18f7e100aaa89ca242a
patrickpircher <pa.trickjlp@gmail.com>
2024-09-13 13:27:41 +0200

    fail test

--- tests/scenarios/vite-internals-test.ts
+++ tests/scenarios/vite-internals-test.ts
@@ -76,6 +76,12 @@ appScenarios
           `,
           'epsilon.hbs': `<div class="epsilon">Epsilon</div>`,
           'fancy-button.hbs': `<h1>I'm fancy</h1>`,
+          'delta.js': `
+            import Component from '@glimmer/component';
+            export default class extends Component {
+              message = "delta";
+            }
+          `,
         },
         templates: {
           'application.hbs': `
@@ -90,11 +96,17 @@ appScenarios
         lib: {
           'app-lib-one.js': `
             globalThis.appLibOneLoaded = (globalThis.appLibOneLoaded ?? 0) + 1;
-            export default function() { return 'app-lib-one'; }
+            const localObject = {
+              message: 'app-lib-one'
+            };
+            export default function() { return localObject; }
           `,
           'app-lib-two.js': `
             globalThis.appLibTwoLoaded = (globalThis.appLibTwoLoaded ?? 0) + 1;
-            export default function() { return 'app-lib-two'; }
+            const localObject = {
+              message: 'app-lib-two'
+            };
+            export default function() { return localObject; }
           `,
         },
       },
@@ -134,6 +146,11 @@ appScenarios
                   await render(hbs\`<Zeta />\`);
                   assert.dom('.zeta').hasText('Zeta');
                   assert.dom('.epsilon').hasText('Epsilon');
+                });
+
+                 test("paired component between app and addon", async function (assert) {
+                  await render(hbs\`<Delta />\`);
+                  assert.dom('.delta').hasText('delta');
                 });
 
                 test("addon depends on an app's module via relative import", async function (assert) {
@@ -179,6 +196,13 @@ appScenarios
           import appLibTwo from 'app-template/lib/app-lib-two';
           export { appLibOne, appLibTwo };
         `,
+        templates: {
+          components: {
+            'delta.hbs': `
+              <div class="delta">delta</div>
+            `,
+          },
+        },
         components: {
           'beta.js': `
             export { default } from 'v1-example-addon/components/beta';

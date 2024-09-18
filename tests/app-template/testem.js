9a3fcb969deee3506ebde07530ebb5d039855b8f
patrick <pa.trickjlp@gmail.com>
2024-09-18 11:49:17 +0200

    fix intermittent chrome failures

--- tests/app-template/testem.js
+++ tests/app-template/testem.js
@@ -13,6 +13,7 @@ if (typeof module !== 'undefined') {
           // --no-sandbox is needed when running Chrome inside a container
           process.env.CI ? '--no-sandbox' : null,
           '--headless',
+          '--disable-gpu',
           '--disable-dev-shm-usage',
           '--disable-software-rasterizer',
           '--mute-audio',

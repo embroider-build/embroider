d92cda264d192b5c310a50493829ca9f2268e787
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-10-02 18:33:59 -0400

    Remove outdated comment

--- packages/core/types/virtual/index.js
+++ packages/core/types/virtual/index.js
@@ -1,12 +1,3 @@
-/**
- * This file exists to appease both type checking and require.resolve
- * checks for the existance of `@embroider/virtual`.
- *
- * This library has a '.' entrypoint which points to this file for require.resolve (if a consumer needed), as well as every other export.
- *
- * Real types are provided for the non-'.' exports.
- */
-
 throw new Error(
   `@embroider/virtual module loaded at runtime! This is likely a mistake. Make sure that you have all the needed plugins for virtually providing embroider's virtual modules.`
 );

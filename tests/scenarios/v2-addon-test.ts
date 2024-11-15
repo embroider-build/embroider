f3e095ca03f11de490a211c0991819e561155a4b
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-11-15 16:08:49 -0500

    Tell the optimizer to use esntext, and tell the test's vite config to use a newer browser target that supports top-level await

--- tests/scenarios/v2-addon-test.ts
+++ tests/scenarios/v2-addon-test.ts
@@ -153,6 +153,80 @@ appScenarios
           `,
         },
       },
+      // Default + different build.target
+      'vite.config.mjs': `
+          import { defineConfig } from "vite";
+          import {
+            resolver,
+            hbs,
+            scripts,
+            templateTag,
+            optimizeDeps,
+            compatPrebuild,
+            assets,
+            contentFor,
+          } from "@embroider/vite";
+          import { resolve } from "path";
+          import { babel } from "@rollup/plugin-babel";
+
+          export default defineConfig(({ mode }) => {
+            return {
+              cacheDir: resolve("node_modules", ".vite"),
+              resolve: {
+                extensions: [
+                  ".mjs",
+                  ".gjs",
+                  ".js",
+                  ".mts",
+                  ".gts",
+                  ".ts",
+                  ".hbs",
+                  ".hbs.js",
+                  ".json",
+                ],
+              },
+              plugins: [
+                hbs(),
+                templateTag(),
+                scripts(),
+                resolver(),
+                compatPrebuild(),
+                assets(),
+                contentFor(),
+
+                babel({
+                  babelHelpers: "runtime",
+
+                  // this needs .hbs because our hbs() plugin above converts them to
+                  // javascript but the javascript still also needs babel, but we don't want
+                  // to rename them because vite isn't great about knowing how to hot-reload
+                  // them if we resolve them to made-up names.
+                  extensions: [".gjs", ".js", ".hbs", ".ts", ".gts"],
+                }),
+              ],
+              optimizeDeps: optimizeDeps(),
+              server: {
+                port: 4200,
+              },
+              build: {
+                outDir: "dist",
+                target: ['chrome130'],
+                rollupOptions: {
+                  input: {
+                    main: "index.html",
+                    ...(shouldBuildTests(mode)
+                      ? { tests: "tests/index.html" }
+                      : undefined),
+                  },
+                },
+              },
+            };
+          });
+
+          function shouldBuildTests(mode) {
+            return mode !== "production" || process.env.FORCE_BUILD_TESTS;
+          }
+          `,
     });
   })
   .forEachScenario(scenario => {

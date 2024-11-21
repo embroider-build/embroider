83b22b78a6452f288a4f5fe63d088dbd0d572c81
Edward Faulkner <edward@eaf4.com>
2024-11-21 13:34:12 -0500

    up to DOM renderer

--- tests/app-template/app/app-ssr.js
+++ tests/app-template/app/app-ssr.js
@@ -1,3 +1,14 @@
-export function render(url) {
-  return `You visisted ${url}`;
+import Application from '@ember/application';
+import Resolver from 'ember-resolver';
+
+class App extends Application {
+  Resolver = Resolver.withModules({});
+}
+
+export async function render(url) {
+  let result = await App.create({
+    autoboot: false,
+    modulePrefix: 'spike',
+  }).visit(url);
+  return result;
 }

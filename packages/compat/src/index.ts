8f1f042491c66a4f497f59feb96f70df7cce9bce
void_malex <void-mAlex@users.noreply.github.com>
2024-11-26 00:08:06 +0000

    reinstate export of compat package

--- packages/compat/src/index.ts
+++ packages/compat/src/index.ts
@@ -3,4 +3,5 @@ export { default as Addons } from './compat-addons';
 export { default as Options, recommendedOptions } from './options';
 export { default as V1Addon } from './v1-addon';
 export { prebuild, PipelineOptions } from './default-pipeline';
+export { default as templateTagCodemod, TemplateTagCodemodOptions } from './template-tag-codemod';
 export { PackageRules, ModuleRules } from './dependency-rules';

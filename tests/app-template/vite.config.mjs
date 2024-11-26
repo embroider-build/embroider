6c41d7285c06c245d936ef15f159102a562bb246
Chris Manson <chris@manson.ie>
2024-11-26 18:18:09 +0000

    bundle up vite plugins in a nicer way

--- tests/app-template/vite.config.mjs
+++ tests/app-template/vite.config.mjs
@@ -1,70 +1,15 @@
 import { defineConfig } from "vite";
-import {
-  resolver,
-  hbs,
-  scripts,
-  templateTag,
-  optimizeDeps,
-  compatPrebuild,
-  assets,
-  contentFor,
-} from "@embroider/vite";
-import { resolve } from "path";
+import { extensions, classicEmberSupport, ember } from "@embroider/vite";
 import { babel } from "@rollup/plugin-babel";
 
-export default defineConfig(({ mode }) => {
-  return {
-    cacheDir: resolve("node_modules", ".vite"),
-    resolve: {
-      extensions: [
-        ".mjs",
-        ".gjs",
-        ".js",
-        ".mts",
-        ".gts",
-        ".ts",
-        ".hbs",
-        ".hbs.js",
-        ".json",
-      ],
-    },
-    plugins: [
-      hbs(),
-      templateTag(),
-      scripts(),
-      resolver(),
-      compatPrebuild(),
-      assets(),
-      contentFor(),
+export default defineConfig({
+  plugins: [
+    classicEmberSupport(),
+    ember(),
 
-      babel({
-        babelHelpers: "runtime",
-
-        // this needs .hbs because our hbs() plugin above converts them to
-        // javascript but the javascript still also needs babel, but we don't want
-        // to rename them because vite isn't great about knowing how to hot-reload
-        // them if we resolve them to made-up names.
-        extensions: [".gjs", ".js", ".hbs", ".ts", ".gts"],
-      }),
-    ],
-    optimizeDeps: optimizeDeps(),
-    server: {
-      port: 4200,
-    },
-    build: {
-      outDir: "dist",
-      rollupOptions: {
-        input: {
-          main: "index.html",
-          ...(shouldBuildTests(mode)
-            ? { tests: "tests/index.html" }
-            : undefined),
-        },
-      },
-    },
-  };
+    babel({
+      babelHelpers: "runtime",
+      extensions,
+    }),
+  ],
 });
-
-function shouldBuildTests(mode) {
-  return mode !== "production" || process.env.FORCE_BUILD_TESTS;
-}

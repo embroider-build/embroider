5d69eb68e441239e1567f1715e7c0fe3f0ba0290
Edward Faulkner <edward@eaf4.com>
2024-11-26 09:13:48 -0500

    wip

--- packages/template-tag-codemod/src/steps.ts
+++ packages/template-tag-codemod/src/steps.ts
@@ -4,6 +4,7 @@ import { hbsToJS, ResolverLoader } from '@embroider/core';
 import { transformAsync } from '@babel/core';
 import templateCompilation, { type Options as EtcOptions } from 'babel-plugin-ember-template-compilation';
 import { createRequire } from 'module';
+import extractMeta, { type ExtractMetaOpts, type MetaResult } from './extract-meta.js';
 const require = createRequire(import.meta.url);
 
 export async function ensurePrebuild() {
@@ -27,7 +28,7 @@ const resolver = new ResolverLoader(process.cwd()).resolver;
 
 export async function processRouteTemplate(filename: string) {
   let src = readFileSync(filename, 'utf8');
-  let outSrc = (await transformAsync(hbsToJS(src), {
+  let strictSource = (await transformAsync(hbsToJS(src), {
     filename,
     plugins: [
       [
@@ -50,7 +51,19 @@ export async function processRouteTemplate(filename: string) {
       ],
     ],
   }))!.code!;
-  writeFileSync(filename.replace(/.hbs$/, '.js'), outSrc);
+
+  const meta: ExtractMetaOpts = { result: undefined };
+  await transformAsync(strictSource, {
+    filename,
+    plugins: [[extractMeta, meta]],
+  });
+  if (!meta.result) {
+    throw new Error(`failed to extract metadata while processing ${filename}`);
+  }
+  let outSource: string[] = [];
+  outSource.push(renderScopeImports(meta.result));
+  outSource.push(`<template>${meta.result.templateSource}</template>`);
+  writeFileSync(filename.replace(/.hbs$/, '.gjs'), outSource.join('\n'));
   console.log(`route template: ${filename} `);
 }
 
@@ -58,9 +71,50 @@ export async function processComponentTemplates() {
   for (let filename of globSync('app/components/*.hbs')) {
     console.log(`component template: ${filename} `);
   }
+
+
+async function resolveImports(filename: string, meta: MetaResult): Promise<MetaResult> {
+  resolver.nodeResolve(
 }
 
-export async function todo() {
-  let loader = new ResolverLoader(process.cwd());
-  return loader.resolver;
+function renderScopeImports(result: MetaResult) {
+  let modules = new Map<string, Map<string, string>>();
+  for (let entry of result.scope.values()) {
+    let module = modules.get(entry.module);
+    if (!module) {
+      module = new Map();
+      modules.set(entry.module, module);
+    }
+    module.set(entry.local, entry.imported);
+  }
+  return [...modules]
+    .map(([module, names]) => {
+      let sections: string[] = [];
+      let entries = [...names.entries()];
+
+      let def = entries.find(e => e[1] === 'default');
+      if (def) {
+        sections.push(def[0]);
+      }
+
+      let named = entries.filter(e => e[1] !== 'default');
+      if (named.length > 0) {
+        sections.push(
+          '{' +
+            named
+              .map(([local, imported]) => {
+                if (local === imported) {
+                  return imported;
+                } else {
+                  return `${imported} as ${local}`;
+                }
+              })
+              .join(',') +
+            '}'
+        );
+      }
+
+      return `import ${sections.join(',')} from "${module}";`;
+    })
+    .join('\n');
 }

9cb8e3a2471e850aa05d9f0dcb171e84a4822c08
Chris Manson <chris.manson@mainmatter.com>
2024-12-19 21:06:56 +0000

    use new ember-qunit in minimal app template

--- tests/app-template-minimal/tests/test-helper.js
+++ tests/app-template-minimal/tests/test-helper.js
@@ -3,10 +3,11 @@ import config from 'app-template-minimal/config/environment';
 import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
-import { start as qunitStart } from 'ember-qunit';
+import { start as qunitStart, setupEmberOnerrorValidation } from 'ember-qunit';
 
 export function start() {
   setApplication(Application.create(config.APP));
   setup(QUnit.assert);
-  qunitStart({ loadTests: false });
+  setupEmberOnerrorValidation();
+  qunitStart();
 }

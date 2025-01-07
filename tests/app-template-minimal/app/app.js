92cc2be2c68b317784a2b578eb3b01816c8827af
Edward Faulkner <edward@eaf4.com>
2025-01-07 10:49:34 -0500

    updating to ember 6.3 alpha and fixing resolver paths

--- tests/app-template-minimal/app/app.js
+++ tests/app-template-minimal/app/app.js
@@ -2,12 +2,24 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import config from './config/environment';
 
-const templates = import.meta.glob(`./templates/**/*`, { eager: true });
+const templates = import.meta.glob(`./templates/**/*.gjs`, {
+  eager: true,
+});
+
+// TODO: we need nice API here
+function mangle(templates) {
+  return Object.fromEntries(
+    Object.entries(templates).map(([k, v]) => [
+      k.replace(/\.gjs$/, '').replace(/^\.\//, 'app-template-minimal/'),
+      v,
+    ])
+  );
+}
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
   Resolver = Resolver.withModules({
-    ...templates,
+    ...mangle(templates),
   });
 }

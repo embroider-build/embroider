40aea1c34e54eb8d7db797910064b52a0bdd3757
Chris Manson <chris@manson.ie>
2025-01-21 13:09:05 +0000

    fix api for compatBuild to stop using an any

--- packages/compat/src/default-pipeline.ts
+++ packages/compat/src/default-pipeline.ts
@@ -5,6 +5,7 @@ import type { Variant, EmberAppInstance } from '@embroider/core';
 import type { Node } from 'broccoli-node-api';
 import writeFile from 'broccoli-file-creator';
 import mergeTrees from 'broccoli-merge-trees';
+import type Plugin from 'broccoli-plugin';
 
 export interface PipelineOptions<PackagerOptions> extends Options {
   packagerOptions?: PackagerOptions;
@@ -36,7 +37,7 @@ export function prebuild(emberApp: EmberAppInstance, options?: Options): Node {
   return mergeTrees([embroiderApp.asStage(addons).tree, writeFile('.stage2-output', () => outputPath)]);
 }
 
-export function compatBuild(emberApp: EmberAppInstance, Builder: any, options?: Options): Node {
+export function compatBuild(emberApp: EmberAppInstance, Builder: typeof Plugin, options?: Options): Node {
   if (process.env.EMBROIDER_PREBUILD) {
     return prebuild(emberApp, options);
   }

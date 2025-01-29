21e3d4cdd762e667e7e4531022a1ad81691c9b27
Chris Manson <chris.manson@mainmatter.com>
2025-01-29 10:28:54 +0000

    fix minimal app crashing if global.EmberENV isn't defined

--- tests/app-template-minimal/app/app.js
+++ tests/app-template-minimal/app/app.js
@@ -3,6 +3,17 @@ import Resolver from 'ember-resolver';
 import config from './config/environment';
 import compatModules from '@embroider/virtual/compat-modules';
 
+/**
+ * TODO remove this. Apparently ember-test-helpers depends on this global existing or else it crashes
+ * and apparently we don't introduce this global any more in the pure minimal app.
+ *
+ * Also interesting observation: as an Ember develop you probably won't notice this error when developing locally
+ * since you probably have the Ember Inspector installed and the Ember inspector **introduces** this global by
+ * just being installed 😭 I only figured this out when running testem in dev mode which opens a new instance of
+ * chrome with no extensions installed 🫠
+ */
+globalThis.EmberENV = {};
+
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;

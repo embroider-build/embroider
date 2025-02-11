c40923b485d78892588395e74effd3c4fcceb9b1
Edward Faulkner <edward@eaf4.com>
2025-02-11 12:06:38 -0500

    vite auto-merges this

--- packages/vite/src/classic-ember-support.ts
+++ packages/vite/src/classic-ember-support.ts
@@ -6,7 +6,6 @@ import { contentFor } from './content-for.js';
 import browserslistToEsbuild from 'browserslist-to-esbuild';
 import { existsSync } from 'fs';
 import { join } from 'path';
-import { mergeConfig, type UserConfig } from 'vite';
 import { pathToFileURL } from 'url';
 
 export function classicEmberSupport() {
@@ -18,22 +17,18 @@ export function classicEmberSupport() {
     contentFor(),
     {
       name: 'vite-plugin-ember-browser-targets',
-      async config(userConfig: UserConfig) {
+      async config() {
         const targetsPath = join(process.cwd(), 'config/targets.js');
         if (existsSync(targetsPath)) {
           const targets = await import(pathToFileURL(targetsPath).toString());
           if (targets.default.browsers) {
-            return mergeConfig(
-              {
-                build: {
-                  target: browserslistToEsbuild(targets.browsers),
-                },
+            return {
+              build: {
+                target: browserslistToEsbuild(targets.browsers),
               },
-              userConfig
-            );
+            };
           }
         }
-        return userConfig;
       },
     },
   ];

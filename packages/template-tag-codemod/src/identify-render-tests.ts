86f78fb069a4a5eae56c7dfde5f7d607b66e2561
Edward Faulkner <edward@eaf4.com>
2025-02-28 15:31:48 -0500

    Handle render tests that const-define the template outside the render() call

--- packages/template-tag-codemod/src/identify-render-tests.ts
+++ packages/template-tag-codemod/src/identify-render-tests.ts
@@ -23,48 +23,70 @@ export async function identifyRenderTests(ast: types.File, source: string, filen
 
   let availableBindings = new Map<number, RenderTest['availableBinding']>();
 
+  function handleTemplate(templatePath: NodePath) {
+    let loc = templatePath.node.loc;
+    if (!loc) {
+      throw new Error(`bug: no locations provided by babel`);
+    }
+
+    let target = startOfScope(templatePath);
+    let availableBinding = availableBindings.get(target);
+    if (!availableBinding) {
+      let counter = 0;
+      let identifier = 'self';
+      while (templatePath.scope.getBinding(identifier)) {
+        identifier = `self${counter++}`;
+      }
+
+      let inserted = false;
+      availableBinding = () => {
+        let needsInsertAt: number | null = null;
+        if (!inserted) {
+          needsInsertAt = target;
+          inserted = true;
+        }
+        return {
+          identifier,
+          needsInsertAt,
+        };
+      };
+      availableBindings.set(target, availableBinding);
+    }
+
+    renderTests.push({
+      startIndex: loc.start.index,
+      endIndex: loc.end.index,
+      availableBinding,
+    });
+  }
+
   traverse(ast, {
     CallExpression(path) {
       if (path.get('callee').referencesImport('@ember/test-helpers', 'render')) {
         let [arg0] = path.get('arguments');
         if (isLooseHBS(arg0)) {
-          let loc = arg0.node.loc;
-          if (!loc) {
-            throw new Error(`bug: no locations provided by babel`);
-          }
-
-          let target = startOfScope(path);
-          let availableBinding = availableBindings.get(target);
-          if (!availableBinding) {
-            let counter = 0;
-            let identifier = 'self';
-            while (path.scope.getBinding(identifier)) {
-              identifier = `self${counter++}`;
-            }
-
-            let inserted = false;
-            availableBinding = () => {
-              let needsInsertAt: number | null = null;
-              if (!inserted) {
-                needsInsertAt = target;
-                inserted = true;
-              }
-              return {
-                identifier,
-                needsInsertAt,
-              };
-            };
-            availableBindings.set(target, availableBinding);
-          }
-
-          renderTests.push({
-            startIndex: loc.start.index,
-            endIndex: loc.end.index,
-            availableBinding,
-          });
-        } else {
-          throw fail(arg0.node, `unsupported syntax in rendering test (${arg0.type})`);
+          handleTemplate(arg0);
+          return;
         }
+        if (arg0.isIdentifier()) {
+          let binding = arg0.scope.getBinding(arg0.node.name);
+          let definition = binding?.path;
+          if (definition?.isVariableDeclarator()) {
+            let init = definition.get('init');
+            if (isLooseHBS(init)) {
+              if (binding?.references === 1) {
+                handleTemplate(init as NodePath);
+                return;
+              } else {
+                throw fail(
+                  arg0.node,
+                  `unsupported syntax in rendering test: local variable "${arg0.node.name}" is a template but it's used in multiple places. We can only rewrite it automatically when it's used exactly once in a call to render()`
+                );
+              }
+            }
+          }
+        }
+        throw fail(arg0.node, `unsupported syntax in rendering test (${arg0.type})`);
       }
     },
   });

65688d8c4f703a5cdb39936aa454e3e8fbb13175
Marine Dunstetter <bluecut.official@gmail.com>
2025-04-24 12:51:42 +0200

    inspector support: create the support script in the compat package, import it in app-template for testing

--- tests/app-template/app/app.js
+++ tests/app-template/app/app.js
@@ -3,6 +3,7 @@ import compatModules from '@embroider/virtual/compat-modules';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
+import '@embroider/compat/inspector-support';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;

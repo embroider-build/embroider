dcc2d38a1d3f20c5dc096508bd403cb73d3b2df3
Marine Dunstetter <bluecut.official@gmail.com>
2025-07-16 14:29:59 +0200

    inspector support: make embroider/compat a v2 addon, and provide inspector support

--- packages/vite/src/ember.ts
+++ packages/vite/src/ember.ts
@@ -2,7 +2,6 @@ import { templateTag } from './template-tag.js';
 import { resolver } from './resolver.js';
 import { type UserConfig, type ConfigEnv, type Plugin } from 'vite';
 import { esBuildResolver } from './esbuild-resolver.js';
-import { inspectorSupport } from './inspector-support.js';
 
 export let extensions = ['.mjs', '.gjs', '.js', '.mts', '.gts', '.ts', '.hbs', '.hbs.js', '.json'];
 
@@ -25,7 +24,6 @@ export function ember(params?: {
   return [
     templateTag(),
     resolver(),
-    inspectorSupport(),
     {
       name: 'vite-plugin-ember-config',
       async config(config: UserConfig, env: ConfigEnv) {

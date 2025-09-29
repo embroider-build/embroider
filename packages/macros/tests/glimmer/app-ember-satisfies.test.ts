7f94c8ab35cfb21ea621181b87fb769b5f1ceb8d
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-09-29 10:58:46 -0400

    All glimmer tests pass atm

--- packages/macros/tests/glimmer/app-ember-satisfies.test.ts
+++ packages/macros/tests/glimmer/app-ember-satisfies.test.ts
@@ -1,14 +1,40 @@
 import { Project, templateTests } from './helpers';
 import { join } from 'path';
 
-describe('dependency satisfies', () => {
+describe('app ember dependency satisfies (prerelease)', () => {
+  let project: Project;
+  let filename: string;
+
+  beforeAll(async () => {
+    project = new Project('app');
+    project.addDependency('ember-source', '1.2.0-beta.1');
+    await project.write();
+    filename = join(project.baseDir, 'sample.js');
+  });
+
+  afterAll(() => {
+    project?.dispose();
+  });
+
+  templateTests((originalTransform) => {
+    function transform(text: string): Promise<string> {
+      return originalTransform(text, { filename, appRoot: project.baseDir });
+    }
+
+    test('it considers prereleases (otherwise within the range) as allowed', async () => {
+      let result = await transform(`{{macroAppEmberSatisfies '^1.0.0'}}`);
+      expect(result).toEqual('{{true}}');
+    });
+  });
+});
+
+describe('app ember dependency satisfies (released)', () => {
   let project: Project;
   let filename: string;
 
   beforeAll(async () => {
     project = new Project('app');
     project.addDependency('ember-source', '2.9.1');
-    project.addDependency('foo', '1.1.0-beta.1');
     await project.write();
     filename = join(project.baseDir, 'sample.js');
   });
@@ -50,18 +76,13 @@ describe('dependency satisfies', () => {
     test('args length error', async () => {
       await expect(async () => {
         await transform(`{{macroAppEmberSatisfies 'not-a-real-dep' 'another'}}`);
-      }).rejects.toThrow(/macroAppEmberSatisfies requires one argument, you passed 2/);
+      }).rejects.toThrow(/macroAppEmberSatisfies requires only one argument, you passed 2/);
     });
 
     test('non literal arg error', async () => {
       await expect(async () => {
         await transform(`{{macroAppEmberSatisfies someDep }}`);
       }).rejects.toThrow(/all arguments to macroAppEmberSatisfies must be string literals/);
-    });
-
-    test('it considers prereleases (otherwise within the range) as allowed', async () => {
-      let result = await transform(`{{macroAppEmberSatisfies '^1.0.0'}}`);
-      expect(result).toEqual('{{true}}');
     });
   });
 });

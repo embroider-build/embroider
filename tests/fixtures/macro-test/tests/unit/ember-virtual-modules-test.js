fa859811876f201a190a17d98ba213176b209a57
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-12-29 11:27:38 -0500

    Make test conditional

--- tests/fixtures/macro-test/tests/unit/ember-virtual-modules-test.js
+++ tests/fixtures/macro-test/tests/unit/ember-virtual-modules-test.js
@@ -1,13 +1,31 @@
 import { module, test } from 'qunit';
 
 module('Unit | ember-virtual-modules', function () {
+
   module('@ember/reactive', function () {
-    test('it exists', async function (assert) {
-      const { trackedObject } = await import('@ember/reactive/collections');
+    if (appEmberSatisfies('>= 6.8.0-beta.1')) {
+      test('it exists', async function (assert) {
+        const { trackedObject } = await import('@ember/reactive/collections');
 
-      const data = trackedObject({ foo: 1 });
+        const data = trackedObject({ foo: 1 });
 
-      assert.deepEqual(data, { foo: 1 });
-    });
+        assert.deepEqual(data, { foo: 1 });
+      });
+    } else {
+      test('it does not exist', async function (assert) {
+        try {
+          await import('@ember/reactive/collections');
+        } catch (e) {
+          if (e.includes('Failed to resolve module specifier')) {
+            assert.step('error');
+          } else {
+            throw e;
+          }
+        }
+
+        assert.verifySteps(['error']);
+      });
+    }
   });
+
 });
